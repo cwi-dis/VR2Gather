e25d9619b582becdae8e55bb58dc52e1a3a233ea
fernandoperez <fernandoperez@entropystudio.net>
2019-03-13 11:18:36 +0100

    Readme.

--- Assets/VO/Signals/Scripts/UnitySignals.cs
+++ Assets/VO/Signals/Scripts/UnitySignals.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 using System;
 
 public class UnitySignals : MonoBehaviour {
-    IntPtr  handle;
+
+    public string StreamName = "MyMediaPipeline";
+    public string URL = "http://livesim.dashif.org/livesim/testpic_2s/Manifest.mpd";
+
+    IntPtr handle;
     bool    isPlaying = false;
+
 
     private void Awake() {
         Application.logMessageReceived += HandleLog;
@@ -14,17 +19,20 @@ public class UnitySignals : MonoBehaviour {
     void Start () {
         Environment.SetEnvironmentVariable("SIGNALS_SMD_PATH", Application.dataPath + (Application.isEditor ? "/VO/Signals/Plugins" : "/Plugins"));
         Environment.SetEnvironmentVariable("PATH", Application.dataPath + (Application.isEditor ? "/VO/Signals/Plugins" : "/Plugins"));
-        handle = signals_unity_bridge_pinvoke.sub_create("MyMediaPipeline");
-        if (handle != IntPtr.Zero)
-        {
+
+        handle = signals_unity_bridge_pinvoke.sub_create(StreamName);
+        if (handle != IntPtr.Zero) {
             Debug.Log(">>> sub_create " + handle);
-            isPlaying = signals_unity_bridge_pinvoke.sub_play(handle, "http://livesim.dashif.org/livesim/testpic_2s/Manifest.mpd");
+            isPlaying = signals_unity_bridge_pinvoke.sub_play(handle, URL);
             Debug.Log(">>> sub_play " + isPlaying);
             if (isPlaying) {
                 int count = signals_unity_bridge_pinvoke.sub_get_stream_count(handle);
                 Debug.Log(">>> sub_get_stream_count " + count);
-            }
-        }
+            }else
+                Debug.LogError("SUBD_ERROR: can't open URL " + URL );
+
+        }else
+            Debug.LogError("SUBD_ERROR: can't create streaming: " + StreamName);
     }
 	
 	// Update is called once per frame
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Test Bed Unity Proyect.
+
+This repository will keep a bunch of unit test to check all the components developed within the VRTogether consortium.
+
+It currently uses Unity 2018.2.21f1 and runs on Windows 10.
+
+## Scenes
+
+### CWI/Scenes/PointClouds
+
+Scene to test the correct Point Clouds compoments developed by CWI.
+
+- How to run this test:
+	```
+	Open scene.
+	Press play.
+	```
+
+- Result:
+	```
+	A point cloud from "StreamingAssets/pcl_frame1.ply" file.
+	```
+
+### VO/Signals/Scenes/Signals
+
+Scene to test the correct signals unity bridge compoments developed by Viaccess-Orca.
+
+- How to run this test:
+	```
+	Open scene.
+	Press play.
+	Inspecting Signals object in the hierarchy you can change the stream name and the URL.
+	```
+
+- Result:
+	```
+	Console log showing several values: Streamer pointer value, if it can play the URL and stream count.
+	In case of error, console shows it;
+	```
+
