827ee1f2b59e23fb05110e657aab43aea7759c59
fernandoperez <fernandoperez@entropystudio.net>
2019-03-29 13:10:49 +0100

    Add key space to open TVSet.

---
+++ Assets/Entropy/TVSet/Scripts/AnimationController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+    Animator animator;
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        animator.speed = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if( Input.GetKeyDown(KeyCode.Space))
+            animator.speed = 1;
+    }
+}
