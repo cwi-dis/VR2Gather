097703becccadd1c12bb199d97a3e0930e1b842a
fernandoperez <fernandoperez@entropystudio.net>
2019-04-02 14:31:51 +0200

    SUB Pinvoke commented.

--- Assets/MS/Signals/Plugins/signals-unity-bridge_pinvoke.cs
+++ Assets/MS/Signals/Plugins/signals-unity-bridge_pinvoke.cs
@@ -12,18 +12,22 @@ public class signals_unity_bridge_pinvoke {
     // Creates a new pipeline.
     // name: a display name for log messages. Can be NULL.
     // The returned pipeline must be freed using 'sub_destroy'.
+    // SUB_EXPORT sub_handle* sub_create(const char* name);
     [DllImport("signals-unity-bridge")]
     extern static public IntPtr sub_create([MarshalAs(UnmanagedType.LPStr)]string pipeline);
 
     // Destroys a pipeline. This frees all the resources.
+    // SUB_EXPORT void sub_destroy(sub_handle* h);
     [DllImport("signals-unity-bridge")]
     extern static public void sub_destroy(IntPtr handle);
 
     // Returns the number of compressed streams.
+    // SUB_EXPORT int sub_get_stream_count(sub_handle* h);
     [DllImport("signals-unity-bridge")]
     extern static public int sub_get_stream_count(IntPtr handle);
 
     // Plays a given URL.
+    // SUB_EXPORT bool sub_play(sub_handle* h, const char* URL);
     [DllImport("signals-unity-bridge")]
     extern static public bool sub_play(IntPtr handle, [MarshalAs(UnmanagedType.LPStr)]string name);
 
@@ -31,6 +35,7 @@ public class signals_unity_bridge_pinvoke {
     // Returns: the size of compressed data actually copied,
     // or zero, if no frame was available for this stream.
     // If 'dst' is null, the frame will not be dequeued, but its size will be returned.
+    // SUB_EXPORT size_t sub_grab_frame(sub_handle* h, int streamIndex, uint8_t* dst, size_t dstLen, FrameInfo* info);
     [DllImport("signals-unity-bridge")]
     extern static public int sub_grab_frame(IntPtr handle, int streamIndex, System.IntPtr dst, int dstLen, ref FrameInfo info);
 
