fa71cd56bfe01e5fe7d53d1f56ecf5bdf90caecb
Martc <marc.kami1991@gmail.com>
2019-04-11 17:20:30 +0200

    Connections Experiment v0.1 Need improve few things

---
+++ Assets/i2Cat/Connections/TVMSettings_Hook.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Prototype.NetworkLobby;
+using System.Net;
+using System.Net.Sockets;
+
+public class TVMSettings_Hook : LobbyHook {
+
+    public static string LocalIPAddress()
+    {
+        IPHostEntry host;
+        string localIP = "0.0.0.0";
+        host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (IPAddress ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                localIP = ip.ToString();
+                break;
+            }
+        }
+        return localIP;
+    }
+
+    void Start()
+    {
+        Debug.Log(NetworkManager.singleton.networkAddress);
+        Debug.Log(LocalIPAddress()); 
+    }
+
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+        ShowTVMs tvm = gamePlayer.GetComponent<ShowTVMs>();
+
+        string uri = "amqp://tofis:tofis@";
+        string IP = NetworkManager.singleton.networkAddress;
+        uri += IP;
+        uri += ":5672";
+
+        tvm.connectionURI = uri;
+
+        //base.OnLobbyServerSceneLoadedForPlayer(manager, lobbyPlayer, gamePlayer);
+    }
+
+}
