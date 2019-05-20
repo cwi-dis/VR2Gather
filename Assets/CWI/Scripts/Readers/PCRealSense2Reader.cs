04c844dfee3a1b32511059b43ce468fe0940eead
Jack Jansen <Jack.Jansen@cwi.nl>
2019-05-20 22:34:57 +0200

    Fixed logic error in realsense2 constructor: it doesn't set the obj instance variable

--- Assets/CWI/Scripts/Readers/PCRealSense2Reader.cs
+++ Assets/CWI/Scripts/Readers/PCRealSense2Reader.cs
@@ -7,13 +7,13 @@ public class PCRealSense2Reader : PCSyntheticReader
     public PCRealSense2Reader(string configFilename)
     {
         System.IntPtr errorPtr = System.IntPtr.Zero;
-        var rv = API_cwipc_realsense2.cwipc_realsense2(configFilename, ref errorPtr);
-        Debug.Log("xxxjack realsense2 configFile " + configFilename + ", rv " + rv + "errorPtr " + errorPtr);
-        int maxTile = API_cwipc_util.cwipc_tiledsource_maxtile(rv);
+        obj = API_cwipc_realsense2.cwipc_realsense2(configFilename, ref errorPtr);
+        Debug.Log("xxxjack realsense2 configFile " + configFilename + ", obj " + obj + "errorPtr " + errorPtr);
+        int maxTile = API_cwipc_util.cwipc_tiledsource_maxtile(obj);
         Debug.Log("xxxjack realsense2 maxtile=" + maxTile);
         for (int i=0; i<maxTile; i++)
         {
-            Debug.Log("xxxjack tileinfo " + i + " is " + API_cwipc_util.cwipc_tiledsource_get_tileinfo(rv, i, System.IntPtr.Zero));
+            Debug.Log("xxxjack tileinfo " + i + " is " + API_cwipc_util.cwipc_tiledsource_get_tileinfo(obj, i, System.IntPtr.Zero));
         }
         if (errorPtr != System.IntPtr.Zero)
         {
