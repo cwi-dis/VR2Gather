a64e00ede8ed6b85c389101099dbb2c88d24040a
Jack Jansen <Jack.Jansen@cwi.nl>
2019-05-20 16:55:38 +0200

    Added config file support and some debug prints. Doesn't work yet.

--- Assets/CWI/Scripts/Readers/PCRealSense2Reader.cs
+++ Assets/CWI/Scripts/Readers/PCRealSense2Reader.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 public class PCRealSense2Reader : PCSyntheticReader
 {
     // Start is called before the first frame update
-    public PCRealSense2Reader()
+    public PCRealSense2Reader(string configFilename)
     {
         System.IntPtr errorPtr = System.IntPtr.Zero;
-        var rv = API_cwipc_realsense2.cwipc_realsense2(null, ref errorPtr);
+        var rv = API_cwipc_realsense2.cwipc_realsense2(configFilename, ref errorPtr);
+        Debug.Log("xxxjack realsense2 configFile " + configFilename + ", rv " + rv + "errorPtr " + errorPtr);
+        int maxTile = API_cwipc_util.cwipc_tiledsource_maxtile(rv);
+        Debug.Log("xxxjack realsense2 maxtile=" + maxTile);
+        for (int i=0; i<maxTile; i++)
+        {
+            Debug.Log("xxxjack tileinfo " + i + " is " + API_cwipc_util.cwipc_tiledsource_get_tileinfo(rv, i, System.IntPtr.Zero));
+        }
         if (errorPtr != System.IntPtr.Zero)
         {
             string errorMessage = Marshal.PtrToStringAuto(errorPtr);
             Debug.LogError("cwipc_realsense2 returned error: " + errorMessage);
-        }        
+        }     
     }
 }
