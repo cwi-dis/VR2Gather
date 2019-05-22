2e200ae96ebe829e8aa313c1ab86485cbaa88b69
fernandoperez <fernandoperez@entropystudio.net>
2019-05-22 08:08:06 +0200

    New VoiceChat TestBed.

---
+++ Assets/Entropy/VoiceChat/Scripts/MicroRecorder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicroRecorder : MonoBehaviour
+{
+    string device;
+    int currentMinFreq, currentMaxFreq;
+    AudioClip recorder;
+    // Start is called before the first frame update
+    void Start() {
+        if (Microphone.devices.Length > 0) {
+            device = Microphone.devices[0];
+            Microphone.GetDeviceCaps(device, out currentMinFreq, out currentMaxFreq);
+            recorder = Microphone.Start(device, true, 2, currentMaxFreq);
+            AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.clip = recorder;
+            audioSource.loop = true;
+            audioSource.Play();
+            Debug.Log($"Using {device}:{currentMaxFreq}");
+        }
+        else
+            Debug.LogError("No Micros detected.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Entropy/VoiceChat/Scripts/VoicePlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoicePlayer : MonoBehaviour {
+    AudioSource audioSource;
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update() {
+        
+    }
+}
