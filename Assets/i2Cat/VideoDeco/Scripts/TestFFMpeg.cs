c3c0ebca909b7546a7f628dc8a16eb8af34d2f26
gunderwulde <gunderwulde@gmail.com>
2019-08-12 08:23:22 +0200

    Video decoded to a png file.

--- Assets/i2Cat/VideoDeco/Scripts/TestFFMpeg.cs
+++ Assets/i2Cat/VideoDeco/Scripts/TestFFMpeg.cs
@@ -12,7 +12,7 @@ public unsafe class TestFFMpeg : MonoBehaviour {
     private const long AV_NOPTS_VALUE = 0x800000000000000;
 
     public string infilename;
-    public string outfilename;
+    public string outfilepath;
 
     public bool wDecoder;
     private bool finish = false;
@@ -28,218 +28,55 @@ public unsafe class TestFFMpeg : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        CheckMode2(infilename);
-        return;
-
-        AVOutputFormat* fmt = ffmpeg.av_guess_format(null, infilename, null);
-        Debug.Log($"video_codec {fmt->video_codec} audio_codec {fmt->audio_codec}");
-
-        packet = ffmpeg.av_packet_alloc();
-        codec = ffmpeg.avcodec_find_decoder( AVCodecID.AV_CODEC_ID_H264);
-        if (codec != null) {
-            codec_ctx = ffmpeg.avcodec_alloc_context3(codec);
-            if (codec_ctx != null) {
-                parser = ffmpeg.av_parser_init((int)codec->id);
-                if (parser != null) {
-                    int ret = ffmpeg.avcodec_open2(codec_ctx, codec, null);
-                    frame = ffmpeg.av_frame_alloc();
-                    if (frame != null) Parse();
-                } else Debug.Log("av_parser_init ERROR");
-            } else Debug.Log("avcodec_alloc_context3 ERROR");
-        } else Debug.Log("avcodec_find_decoder ERROR");
-    }
-
-    void Parse() {
-        if (!finish) {
-            try {
-                using (var stream = new FileStream(infilename, FileMode.Open, FileAccess.Read)) {
-                    int numBytesToRead = 4096;
-                    byte[] bytes = new byte[numBytesToRead];
-                    int data_size = stream.Read(bytes, 0, numBytesToRead);
-                    while (data_size > 0) {
-                        int offset = 0;
-                        while (data_size > 0 ) {
-                            ffmpeg.av_init_packet(packet);
-                            byte* data = (byte*)System.Runtime.InteropServices.Marshal.UnsafeAddrOfPinnedArrayElement(bytes, offset);
-                            int bytes_used = ffmpeg.av_parser_parse2(parser, codec_ctx, &packet->data, &packet->size, data, data_size, AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
-                            if (bytes_used < 0) {
-                                Debug.Log("Error parsing");
-                                return;
-                            }
-                            offset += bytes_used;
-                            data_size -= bytes_used;
-                            if (packet->size > 0 )  {
-                                Debug.Log($"bytes_used {bytes_used} size {packet->size}");
-                                //int gotFrame;
-                                int ret2 = ffmpeg.avcodec_send_packet(codec_ctx, packet);
-                                if (ret2 < 0) {
-                                    byte* errbuf = (byte*)Marshal.AllocHGlobal(128);
-                                    ffmpeg.av_strerror(ret2, errbuf, 128);
-                                    string err_txt = Marshal.PtrToStringAnsi((IntPtr)errbuf);
-                                    Debug.Log($"Error sending a packet for decoding {ret2} {err_txt}");
-                                } else {
-                                    while (ret2 >= 0) {
-                                        ret2 = ffmpeg.avcodec_receive_frame(codec_ctx, frame);
-                                        if (ret2 >= 0 && ret2 != ffmpeg.AVERROR(ffmpeg.EAGAIN) && ret2 != ffmpeg.AVERROR_EOF) {
-                                            Debug.Log($"saving frame {codec_ctx->frame_number}");
-                                        }else
-                                            Debug.Log($"ret2 {ret2}" );
-
-                                        //SaveToFile(frame->data[0], frame->linesize[0], frame->width, frame->height);
-                                    }
-                                }
-                            }
-
-                            //DecodeFile(c, frame, pkt, outfilename);
-                            //data_size = stream.Read(bytes, 0, numBytesToRead);
-                        }
-                        data_size = stream.Read(bytes, 0, numBytesToRead);
-                        //Debug.Log($">> read Buffer {data_size}");
-                    }
-                    //int ret = ffmpeg.av_parser_parse2(parser, c, &pkt->data, &pkt->size, data, data_size, AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
-                    //if (ret < 0) {
-                    //    Debug.Log("Error while parsing");
-                    //}
-                    //data += ret;
-                    //data_size -= ret;
-
-                    //if (pkt->size > 0)
-                    //    DecodeFile();
-
-                    //using (FileStream fsNew = new FileStream(outfilename, FileMode.Create, FileAccess.Write)) {
-                    //    fsNew.Write(bytes, 0, numBytesToRead);
-                    //}
-                }
-                finish = true;
-            } catch (FileNotFoundException ioEx) {
-                Debug.Log(ioEx.Message);
-            }
-        }
-    }
-
-    //void DecodeStream(byte* pData, int sz) {
-    //    AVPacket packet;
-    //    ffmpeg.av_init_packet(&packet);
-
-    //    packet.data = pData;
-    //    packet.size = sz;
-    //    int framefinished = 0;
-    //    int nres = ffmpeg.avcodec_decode_video2(c, frame, &framefinished, &packet);
-
-    //    byte[] arr = new byte[frame->height];
-    //    Marshal.Copy((IntPtr)frame->data_0, arr, 0, frame->height);
-
-    //    if (framefinished != 0) {
-    //        try {
-    //            using (var fs = new FileStream(outfilename, FileMode.Create, FileAccess.Write)) {
-    //                fs.Write(arr, 0, arr.Length);
-    //            }
-    //        }
-    //        catch (Exception ex) {
-    //            Console.WriteLine("Exception caught in process: {0}", ex);
-    //        }
-    //    }
-    //}
-
-    AVFrame* DecodeFile() {
-        //AVPacket packet;
-        //ffmpeg.av_init_packet(pkt);
-
-        int ret = ffmpeg.avcodec_send_packet(codec_ctx, packet);
-        if (ret < 0) Debug.Log("Error sending a packet for decoding");
-
-        while (ret >= 0) {
-            ret = ffmpeg.avcodec_receive_frame(codec_ctx, frame);
-            if (ret == ffmpeg.AVERROR(ffmpeg.EAGAIN) || ret == ffmpeg.AVERROR_EOF) return null;
-            else if (ret < 0) Debug.Log("Error during decoding");
-
-            Debug.Log("saving frame " + codec_ctx->frame_number);
-
-            //SaveToFile(frame->data[0], frame->linesize[0], frame->width, frame->height);
-        }
-
-        return frame;
-
-        //pkt->data = pData;
-        //pkt->size = sz;
-        //int framefinished = 0;
-        //int nres = 0;
-
-        //nres = ffmpeg.avcodec_decode_video2(c, frame, &framefinished, pkt);
-
-        //Debug.Log("nres: " + nres);
-        //Debug.Log("Frame_0: " + frame->data[0]->ToString());
-        //Debug.Log("Frame_0: " + frame->data[1]->ToString());
-        //Debug.Log("Frame_0: " + frame->data[2]->ToString());
-        //Debug.Log("Frame_0: " + frame->data[3]->ToString());
-        //Debug.Log("Frame_0: " + frame->data[4]->ToString());
-        //Debug.Log("Frame_0: " + frame->data[5]->ToString());
-        //Debug.Log("Frame_0: " + frame->data[6]->ToString());
-        //Debug.Log("Frame_0: " + frame->data[7]->ToString());
-        //Debug.Log("Height: " + frame->height);
-        //Debug.Log("Width: " + frame->width);
-
-        //byte[] arr = new byte[frame->height];
-        //Marshal.Copy((IntPtr)frame->data_0, arr, 0, frame->height);
-
-        //if (framefinished != 0) {
-        //    // do the yuv magic and call a consumer
-        //    try {
-        //        using (var fs = new FileStream(outfilename, FileMode.Create, FileAccess.Write)) {
-        //            fs.Write(arr, 0, arr.Length);
-        //        }
-        //    }
-        //    catch (Exception ex) {
-        //        Console.WriteLine("Exception caught in process: {0}", ex);
-        //    }
-        //}
-    }
-
-    void SaveToFile(byte* buf, int wrap, int xsize, int ysize) {
-        try {
-            using (var fs = new FileStream(outfilename, FileMode.OpenOrCreate, FileAccess.Write)) {
-
-                byte[] buffer = new byte[ysize];
-                Marshal.Copy((IntPtr)buf, buffer, 0, ysize);
-                fs.Write(buffer, 0, ysize);
-            }
-        } catch (Exception ex) {
-            Debug.Log("Exception caught in process: " + ex);
-        }
+        CheckMode2(infilename, outfilepath);
     }
 
     public struct buffer_data {
         public byte* ptr;
+        public int offset;
         public int size; ///< size left in the buffer
     };
 
     [DllImport("kernel32.dll", EntryPoint = "CopyMemory", SetLastError = false)]
     public static extern void CopyMemory(IntPtr dest, IntPtr src, uint count);
 
-    int read_packet(void* opaque, byte* buf, int buf_size) {
+    int read_function(void* opaque, byte* buf, int buf_size) {
         buffer_data *bd = (buffer_data *)opaque;
-        Debug.Log($"read_pakage: {buf_size}");
-        buf_size = (int)Mathf.Min (buf_size, bd->size);
-
-        /* copy internal buffer data to buf */
-        CopyMemory((IntPtr)bd->ptr, (IntPtr)buf, (uint)buf_size);
-        bd->ptr  += buf_size;
-        bd->size -= buf_size;
+        buf_size = (int)Mathf.Min (buf_size, (bd->size- bd->offset));
+        // Debug.Log($"read_function: {buf_size}");
+        CopyMemory((IntPtr)buf, (IntPtr)(bd->ptr + bd->offset), (uint)buf_size);
+        bd->offset += buf_size;
     return buf_size;
-}
+    }
 
-void CheckMode2(string infilename) {
-        int avio_ctx_buffer_size = 80192;
+    long seek_function(void* opaque, long position, int whence) {
+        buffer_data* bd = (buffer_data*)opaque;
+        //Debug.Log($"seek_function offset {position} whence {whence}");
+        if (whence == ffmpeg.AVSEEK_SIZE) {
+            return bd->size;
+        }
+        bd->offset = (int)position;
+        return position;
+    }
+
+    void CheckMode2(string infilename, string outfilepath) {
+        int avio_ctx_buffer_size = 4096;
         byte* avio_ctx_buffer;
         AVIOContext* avio_ctx;
+        AVCodecContext* video_dec_ctx;
+        AVCodec* video_dec;
         buffer_data bd;
         byte[] bytes = System.IO.File.ReadAllBytes(infilename);
         bd.ptr = (byte*)System.Runtime.InteropServices.Marshal.UnsafeAddrOfPinnedArrayElement(bytes, 0);
         bd.size = bytes.Length;
+        bd.offset = 0;
+        int ret;
 
-        AVFormatContext* fmt_ctx = ffmpeg.avformat_alloc_context();
+        AVFormatContext* fmt_ctx;
+        
+        fmt_ctx = ffmpeg.avformat_alloc_context();
         avio_ctx_buffer = (byte*)ffmpeg.av_malloc((ulong)avio_ctx_buffer_size);
-        avio_ctx = ffmpeg.avio_alloc_context(avio_ctx_buffer, avio_ctx_buffer_size, 0, &bd, (avio_alloc_context_read_packet)read_packet, null, null);
+        avio_ctx = ffmpeg.avio_alloc_context(avio_ctx_buffer, avio_ctx_buffer_size, 0, &bd, (avio_alloc_context_read_packet)read_function, null, (avio_alloc_context_seek)seek_function);
         if (avio_ctx==null) {
             Debug.Log("Could not create avio_ctx");
             return;
@@ -247,7 +84,8 @@ void CheckMode2(string infilename) {
         }
         fmt_ctx->flags |= ffmpeg.AVFMT_FLAG_CUSTOM_IO;
         fmt_ctx->pb = avio_ctx;
-        int ret = ffmpeg.avformat_open_input(&fmt_ctx, "", null, null);
+
+        ret = ffmpeg.avformat_open_input(&fmt_ctx, "", null, null);
         if (ret < 0) {
             byte* errbuf = (byte*)Marshal.AllocHGlobal(128);
             ffmpeg.av_strerror(ret, errbuf, 128);
@@ -255,6 +93,7 @@ void CheckMode2(string infilename) {
             Debug.Log($"Could not open input {ret} {err_txt}");
            return;
         }
+
         ret = ffmpeg.avformat_find_stream_info(fmt_ctx, null);
         if (ret < 0) {
             byte* errbuf = (byte*)Marshal.AllocHGlobal(128);
@@ -264,6 +103,85 @@ void CheckMode2(string infilename) {
             return;
         }
 
-        ffmpeg.av_dump_format(fmt_ctx, 0, infilename, 0);
+        int video_stream_index = ffmpeg.av_find_best_stream(fmt_ctx, AVMediaType.AVMEDIA_TYPE_VIDEO, -1, -1, &video_dec, 0);
+        if (video_stream_index < 0) {
+            Debug.Log($"Cannot find a video stream in the input file");
+            return;
+        }
+        video_dec_ctx = fmt_ctx->streams[video_stream_index]->codec;
+        if ((ret = ffmpeg.avcodec_open2(video_dec_ctx, video_dec, null)) < 0) {
+            Debug.Log("Cannot open video decoder");
+            return;
+        }
+        Debug.Log($">>> width {video_dec_ctx->width} height {video_dec_ctx->height} pix_fmt {video_dec_ctx->pix_fmt}");
+
+        AVCodecParserContext* parser = ffmpeg.av_parser_init((int)video_dec->id);
+        if (parser==null) {
+            Debug.Log($"Cannot av_parser_init");
+            return;
+        }
+
+        ret = ffmpeg.avcodec_open2(video_dec_ctx, video_dec, null);
+        if (ret < 0) {
+            Debug.Log($"Cannot avcodec_open2");
+            return;
+        }
+
+        AVFrame* frame = ffmpeg.av_frame_alloc();
+        AVPacket* packet = ffmpeg.av_packet_alloc();
+        ffmpeg.av_init_packet(packet);
+        packet->data = null;
+        packet->size = 0;
+        
+        AVFrame* frame2 = ffmpeg.av_frame_alloc();
+        frame2->width = video_dec_ctx->width;
+        frame2->height = video_dec_ctx->height;
+        frame2->format = (int)AVPixelFormat.AV_PIX_FMT_RGB24;
+        int num_bytes = ffmpeg.av_image_get_buffer_size(AVPixelFormat.AV_PIX_FMT_RGB24, video_dec_ctx->width, video_dec_ctx->height,1);
+        var _pictureFrameData = ffmpeg.av_malloc((ulong)num_bytes);
+        byte_ptrArray4 tmpDataArray;
+        int_array4 tmpLineSizeArray;
+        ffmpeg.av_image_fill_arrays(ref tmpDataArray, ref tmpLineSizeArray, (byte*)_pictureFrameData, (AVPixelFormat)frame2->format, frame2->width, frame2->height, 1);
+
+        SwsContext* ctx = ffmpeg.sws_getContext(video_dec_ctx->width, video_dec_ctx->height,
+                    AVPixelFormat.AV_PIX_FMT_YUV420P, video_dec_ctx->width, video_dec_ctx->height,
+                    AVPixelFormat.AV_PIX_FMT_RGB24, 0, null, null, null);
+
+
+        Texture2D txt = new Texture2D(frame2->width, frame2->height, TextureFormat.RGB24, false, true );
+        
+        while (ffmpeg.av_read_frame(fmt_ctx, packet) >= 0) {
+            if (packet->stream_index == video_stream_index) {
+                ret = ffmpeg.avcodec_send_packet(video_dec_ctx, packet);
+                if (ret < 0) {
+                    byte* errbuf = (byte*)Marshal.AllocHGlobal(128);
+                    ffmpeg.av_strerror(ret, errbuf, 128);
+                    string err_txt = Marshal.PtrToStringAnsi((IntPtr)errbuf);
+                    Debug.Log($"avcodec_send_packet error({packet->stream_index}): {err_txt}");
+                    return;
+                }
+                while (ret >= 0) {
+                    ret = ffmpeg.avcodec_receive_frame(video_dec_ctx, frame);
+                    if (ret == ffmpeg.AVERROR(ffmpeg.EAGAIN) || ret == ffmpeg.AVERROR_EOF) {
+                        break;
+                    } else if (ret < 0) {
+                        Debug.Log("Error during decoding");
+                        return;
+                    }
+                    Debug.Log($"Frame {video_dec_ctx->frame_number}");
+                    if (video_dec_ctx->frame_number < 50) {
+                        //                        Debug.Log($"Frame {video_dec_ctx->frame_number} linesize {frame->linesize[0]}");
+
+                        ret = ffmpeg.sws_scale(ctx, frame->data, frame->linesize, 0, frame->height, tmpDataArray, tmpLineSizeArray);
+                        txt.LoadRawTextureData((IntPtr)tmpDataArray[0], tmpLineSizeArray[0] * frame->height);
+                        txt.Apply();
+                        System.IO.File.WriteAllBytes($"{outfilepath}/frame_{video_dec_ctx->frame_number}.png", txt.EncodeToPNG());
+                    } else
+                        return;
+                   
+                }
+                ffmpeg.av_packet_unref(packet);
+            }
+        }
     }
 }
