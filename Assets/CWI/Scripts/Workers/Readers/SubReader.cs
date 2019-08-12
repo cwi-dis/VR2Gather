1bfe589afe5c8b02c63c80bd02beac8d30349de9
gunderwulde <gunderwulde@gmail.com>
2019-08-12 09:19:51 +0200

    Throw away first SUB buffer received.

--- Assets/CWI/Scripts/Workers/Readers/SubReader.cs
+++ Assets/CWI/Scripts/Workers/Readers/SubReader.cs
@@ -15,6 +15,7 @@ namespace Workers
         public SUBReader(Config._User._SUBConfig cfg) :base(WorkerType.Init) { 
             url = cfg.url;
             streamNumber = cfg.streamNumber;
+            firstTime = true;
             try {
                 signals_unity_bridge_pinvoke.SetPaths();
                 subHandle = signals_unity_bridge_pinvoke.sub_create("source_from_sub");
@@ -45,31 +46,31 @@ namespace Workers
 
         }
         float latTime = 0;
-
+        bool firstTime = true;
         protected override void Update() {
             base.Update();
             if (token != null) {  // Wait for token
                 int size = signals_unity_bridge_pinvoke.sub_grab_frame(subHandle, streamNumber, System.IntPtr.Zero, 0, ref info); // Get buffer length.
                 if (size != 0)
                 {
-                    if (size > dampedSize)
-                    {
-                        dampedSize = (int)(size * Config.Instance.memoryDamping); // Reserves 30% more.
-                        currentBufferArray = new byte[dampedSize];
-                        currentBuffer = System.Runtime.InteropServices.Marshal.UnsafeAddrOfPinnedArrayElement(currentBufferArray, 0);
-                    }
+                    if (!firstTime) {
+                        if (size > dampedSize) {
+                            dampedSize = (int)(size * Config.Instance.memoryDamping); // Reserves 30% more.
+                            currentBufferArray = new byte[dampedSize];
+                            currentBuffer = System.Runtime.InteropServices.Marshal.UnsafeAddrOfPinnedArrayElement(currentBufferArray, 0);
+                        }
 
-                    int bytesRead = signals_unity_bridge_pinvoke.sub_grab_frame(subHandle, streamNumber, currentBuffer, size, ref info);
-                    if (bytesRead == size)
-                    {
-                        // All ok, yield to the next process
-                        token.currentBuffer = currentBuffer;
-                        token.currentByteArray = currentBufferArray;
-                        token.currentSize = bytesRead;
-                        Next();
-                    }
-                    else
-                        Debug.LogError("PCSUBReader: sub_grab_frame returned " + bytesRead + " bytes after promising " + size);
+                        int bytesRead = signals_unity_bridge_pinvoke.sub_grab_frame(subHandle, streamNumber, currentBuffer, size, ref info);
+                        if (bytesRead == size) {
+                            // All ok, yield to the next process
+                            token.currentBuffer = currentBuffer;
+                            token.currentByteArray = currentBufferArray;
+                            token.currentSize = bytesRead;
+                            Next();
+                        } else
+                            Debug.LogError("PCSUBReader: sub_grab_frame returned " + bytesRead + " bytes after promising " + size);
+                    } else
+                        firstTime = false;
                 }
             }
         }
