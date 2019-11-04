9e6769b23c3f452f04d42bd73ec2f5dd6f971b2c
gunderwulde <gunderwulde@gmail.com>
2019-11-04 12:43:53 +0100

    - Audio degradation fixed.

--- Assets/Entropy/VoiceChat/Workers/Readers/VoiceReader.cs
+++ Assets/Entropy/VoiceChat/Workers/Readers/VoiceReader.cs
@@ -8,10 +8,16 @@ namespace Workers
     {
         MonoBehaviour monoBehaviour;
         Coroutine coroutine;
-        bool bReady;
-        float[] buffer;
+        float[] writeBuffer;
+        float[] circularBuffer;
+        int circularBufferReadPosition;
+        int circularBufferWritePosition;
+        int circularBufferSize;
+
         public VoiceReader(MonoBehaviour monoBehaviour, int bufferLength) : base(WorkerType.Init) {
             this.bufferLength = bufferLength;
+            circularBufferSize = 320 * 100;
+            this.circularBuffer = new float[circularBufferSize];
             this.monoBehaviour = monoBehaviour;
             coroutine = monoBehaviour.StartCoroutine(MicroRecorder());
             Start();
@@ -19,10 +25,19 @@ namespace Workers
 
         protected override void Update() {
             base.Update();
-            if (token != null && bReady) {
-                token.currentFloatArray = buffer;
-                token.currentSize = buffer.Length;
-                bReady = false;
+            int bytesInAudioBuffer = 0;
+            lock (this) {
+                if (circularBufferWritePosition < circularBufferReadPosition) bytesInAudioBuffer = (circularBufferSize - circularBufferReadPosition) + circularBufferWritePosition;
+                else bytesInAudioBuffer = circularBufferWritePosition - circularBufferReadPosition;
+            }
+            if (token != null && bytesInAudioBuffer>= bufferLength) {
+                lock (this) {
+                    System.Array.Copy(circularBuffer, circularBufferReadPosition, writeBuffer, 0, bufferLength);
+                    token.currentFloatArray = writeBuffer;
+                    token.currentSize = bufferLength;
+                    token.latency = NTPTools.GetNTPTime();
+                    circularBufferReadPosition = (circularBufferReadPosition+bufferLength) % circularBufferSize;
+                }
                 Next();
             }
         }
@@ -42,25 +57,30 @@ namespace Workers
                 int currentMinFreq;
                 Microphone.GetDeviceCaps(device, out currentMinFreq, out samples);
                 samples = 16000;//codec.recorderFrequency;1
-                bufferLength = 320;// * 4;//codec.bufferLeght;
+                // bufferLength = 320;// * 4;//codec.bufferLeght;
 
                 recorder = Microphone.Start(device, true, 1, samples);
                 samples = recorder.samples;
-                buffer = new float[bufferLength];
+                float[] readBuffer = new float[bufferLength];
+                writeBuffer = new float[bufferLength];
                 Debug.Log($"Using {device}  Frequency {samples} bufferLength {bufferLength} IsRecording {Microphone.IsRecording(device)}");
 
                 int readPosition = 0;
                 while (true) {
-                    if (token != null && !bReady) {
+                    if (token != null ) {
                         int writePosition = Microphone.GetPosition(device);
                         int available;
                         if (writePosition < readPosition)   available = (samples - readPosition) + writePosition;
                         else                                available = writePosition - readPosition;
-                        if (available > bufferLength) {
-                            recorder.GetData(buffer, readPosition);
-                            token.latency = NTPTools.GetNTPTime();
+                        while(available >= bufferLength) {
+                            recorder.GetData(readBuffer, readPosition);
+                            // Write all data from microphone.
+                            lock (this) {
+                                System.Array.Copy(readBuffer, 0, circularBuffer, circularBufferWritePosition, bufferLength);
+                                circularBufferWritePosition = (circularBufferWritePosition + bufferLength) % circularBufferSize;
+                                available -= bufferLength;
+                            }
                             readPosition = (readPosition + bufferLength) % samples;
-                            bReady = true;
                         }
                     }
                     yield return null;
