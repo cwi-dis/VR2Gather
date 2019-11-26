aa731562660511f03eda5d5af1817269509b2fcd
Marc_i2Cat <marc.kami1991@gmail.com>
2019-11-26 16:09:26 +0100

    Profiler

---
+++ Assets/i2Cat/Profiler/FPSProfiler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class FPSProfiler : BaseProfiler {
+    List<Vector2> data = new List<Vector2>();
+
+    public override void Flush() { 
+        data.Clear();
+    }
+
+    public override void AddFrameValues() {
+        data.Add(new Vector2(Time.frameCount, Time.frameCount / Time.time));
+    }
+
+    public override void GetHeaders(StringBuilder sb) { 
+        sb.Append("FRAME;FPS;"); 
+    }
+
+    public override void GetFramesValues(StringBuilder sb, int frame) {
+        sb.AppendFormat("{0};{1:0.00};", (int)data[frame].x, data[frame].y);
+    }
+}
