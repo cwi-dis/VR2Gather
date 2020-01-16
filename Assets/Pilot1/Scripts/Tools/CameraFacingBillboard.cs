a12e4ca0f24f9c13b5eeef9e617556a8bca49bf8
MarcKami <Marc.kami1991@gmail.com>
2020-01-16 15:32:00 +0100

    LastClean? Working

--- Assets/Pilot1/Scripts/Tools/CameraFacingBillboard.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class CameraFacingBillboard : MonoBehaviour { 
-    void LateUpdate() {
-        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
-    }
-}
