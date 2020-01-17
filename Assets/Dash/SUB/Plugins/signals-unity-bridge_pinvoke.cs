bbb6a2f8c930a52fbfaa42d712ec6142f526e3ff
Jack Jansen <Jack.Jansen@cwi.nl>
2020-01-17 17:00:13 +0100

    SIGNALS_SMD_PATH wasn't set in the environment if it was picked up from the config file. Fixed.

--- Assets/Dash/SUB/Plugins/signals-unity-bridge_pinvoke.cs
+++ Assets/Dash/SUB/Plugins/signals-unity-bridge_pinvoke.cs
@@ -165,6 +165,8 @@ public class sub
             {
                 UnityEngine.Debug.LogError($"Environment variable SIGNALS_SMD_PATH must be set on MacOS");
             }
+            Environment.SetEnvironmentVariable("SIGNALS_SMD_PATH", path);
+            UnityEngine.Debug.Log($"xxxjack: mac-specific: SIGNALS_SMD_PATH=${path}");
             return;
         }
         if (lastMSpathInstalled == module_base) return;
