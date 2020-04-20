bd75e9407e89bcff835535063200836849d13328
FernandoPerez <fernando.perez@themoderncultural.com>
2020-04-20 11:35:08 +0200

    *

--- Assets/TheMo/Scripts/NewMemorySystem/NewMemorySystem.cs
+++ Assets/TheMo/Scripts/NewMemorySystem/NewMemorySystem.cs
@@ -1,25 +1,50 @@
-﻿using System.Collections;
+﻿#define  TEST_LOCAL_PC
+//#define TEST_PC
+//#define TEST_VOICECHAT
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NewMemorySystem : MonoBehaviour
 {
-    Workers.BaseWorker reader;
-    Workers.BaseWorker encoder;
-    Workers.BaseWorker decoder;
-    Workers.BaseWorker dashWriter;
-    Workers.BaseWorker dashReader;
 
-    Workers.BaseWorker preparer;
-    QueueThreadSafe preparerQueue = new QueueThreadSafe();
-    QueueThreadSafe encoderQueue = new QueueThreadSafe();
-    QueueThreadSafe writerQueue = new QueueThreadSafe();
-    QueueThreadSafe decoderQueue = new QueueThreadSafe();
-    MonoBehaviour render;
+    public bool         forceMesh = true;
+    Workers.BaseWorker  reader;
+    Workers.BaseWorker  encoder;
+    Workers.BaseWorker  decoder;
+    Workers.BaseWorker  dashWriter;
+    Workers.BaseWorker  dashReader;
+
+    Workers.BaseWorker  binWriter;
+    Workers.BaseWorker  binReader;
+
+
+    Workers.BaseWorker  preparer;
+    QueueThreadSafe     preparerQueue = new QueueThreadSafe();
+    QueueThreadSafe     encoderQueue = new QueueThreadSafe();
+    QueueThreadSafe     writerQueue = new QueueThreadSafe();
+    QueueThreadSafe     decoderQueue = new QueueThreadSafe();
+    MonoBehaviour       render;
+
+    // rtmp://127.0.0.1:1935/live/signals
     // Start is called before the first frame update
     void Start() {
         Config._User cfg = Config.Instance.Users[0];
-        /*
+#if TEST_LOCAL_PC
+        if (forceMesh) {
+            preparer = new Workers.MeshPreparer(preparerQueue);
+            render = gameObject.AddComponent<Workers.PointMeshRenderer>();
+            ((Workers.PointMeshRenderer)render).preparer = (Workers.MeshPreparer)preparer;
+        } else {
+            preparer = new Workers.BufferPreparer(preparerQueue);
+            render = gameObject.AddComponent<Workers.PointBufferRenderer>();
+            ((Workers.PointBufferRenderer)render).preparer = (Workers.BufferPreparer)preparer;
+        }
+        reader = new Workers.RS2Reader(cfg.PCSelfConfig, preparerQueue);
+#endif
+
+#if TEST_PC
         preparer = new Workers.BufferPreparer(preparerQueue);
         render = gameObject.AddComponent<Workers.PointBufferRenderer>();
         ((Workers.PointBufferRenderer)render).preparer = (Workers.BufferPreparer)preparer;
@@ -34,16 +59,21 @@ public class NewMemorySystem : MonoBehaviour
         dashReader = new Workers.SUBReader(cfg.SUBConfig, "https://vrt-evanescent.viaccess-orca.com/"+ uuid + "/testBed.mpd", decoderQueue);
 
         decoder = new Workers.PCDecoder(decoderQueue, preparerQueue);
-        */
+#endif
 
+#if TEST_VOICECHAT
         string uuid = System.Guid.NewGuid().ToString();
         gameObject.AddComponent<VoiceDashSender>().Init(cfg.PCSelfConfig.AudioBin2Dash, "https://vrt-evanescent.viaccess-orca.com/" + uuid + "/"); //Audio Pipeline
         gameObject.AddComponent<VoiceDashReceiver>().Init(cfg.AudioSUBConfig, "https://vrt-evanescent.viaccess-orca.com/" + uuid + "/audio.mpd"); //Audio Pipeline
+#endif
     }
 
     void OnDestroy() {
         dashWriter?.StopAndWait();
         dashReader?.StopAndWait();
+        binWriter?.StopAndWait();
+        binReader?.StopAndWait();
+
         reader?.StopAndWait();
         encoder?.StopAndWait();
         decoder?.StopAndWait();
