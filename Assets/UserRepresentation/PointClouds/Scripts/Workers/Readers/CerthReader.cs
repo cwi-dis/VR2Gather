e46a1f4390e6831f5e37837d2be64cb20f604539
Jack Jansen <Jack.Jansen@cwi.nl>
2020-04-28 00:47:26 +0200

    RabbitMQ receivers for pointcloud data starting to work

--- Assets/UserRepresentation/PointClouds/Scripts/Workers/Readers/CerthReader.cs
+++ Assets/UserRepresentation/PointClouds/Scripts/Workers/Readers/CerthReader.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
-using PCLDataProviders;
 using Utils;
 
 namespace Workers {
@@ -12,7 +11,6 @@ namespace Workers {
         {
             ConnectionProperties.ConnectionURI = uri;
             ConnectionProperties.ExchangeName = exchange;
-            Debug.Log($"xxxjack MyRabbitMQReceiver: uri={uri}, exchange={exchange}");
         }
     }
 
@@ -22,12 +20,11 @@ namespace Workers {
         QueueThreadSafe outQueue;
         QueueThreadSafe out2Queue;
 
-        private PCLIdataProvider dataProvider;  // Connection to RabbitMQ over which normal RGBD data comes in.
-        private PCLIdataProvider metaDataProvider;  // Connection to RabbitMQ over which metadata comes in.
         bool metaDataReceived = false;          // Set to true once metadata has been received
         GCHandle metaDataHandle;                // Set to unmanaged memory handle where metadata has been stored.
         cwipc.pointcloud mostRecentPc;          // Stores the most recently received pointcloud (if any)
         const int pcl_id = 0;                   // Index of Cert pc constructor (constant for now)
+        const int numWrappers = 1;              // Total number of pc constructors (constant for now)
 
         private RabbitMQReceiver PCLRabbitMQReceiver;
         private RabbitMQReceiver MetaRabbitMQReceiver;
@@ -37,6 +34,9 @@ namespace Workers {
             outQueue = _outQueue;
             out2Queue = _out2Queue;
             voxelSize = cfg.voxelSize;
+
+            // Tell Certh library how many pc constructors we want. pcl_id must be < this.
+            native_pointcloud_receiver_pinvoke.set_number_wrappers(numWrappers);
 
             if (cfg.CerthReaderConfig == null)
             {
@@ -58,6 +58,7 @@ namespace Workers {
                 return;
             }
 
+            Debug.Log($"PCCertReader: receiving PCs from {cfg.CerthReaderConfig.ConnectionURI}");
             PCLRabbitMQReceiver.OnDataReceived += OnNewPCLData;
             PCLRabbitMQReceiver.Enabled = true;
 
@@ -70,67 +71,89 @@ namespace Workers {
         }
 
         public void Stop() {
-            PCLRabbitMQReceiver.OnDataReceived -= OnNewPCLData;
             PCLRabbitMQReceiver.Enabled = false;
-            MetaRabbitMQReceiver.OnDataReceived -= OnNewMetaData;
             MetaRabbitMQReceiver.Enabled = false;
+            PCLRabbitMQReceiver.OnDataReceived -= OnNewPCLData;
+            MetaRabbitMQReceiver.OnDataReceived -= OnNewMetaData;
 
             Debug.Log("PCCerthReader: Stopped.");
         }
 
         // Informing that the metadata were received
         private void OnNewMetaData(object sender, EventArgs<byte[]> e) {
-            lock (e) {
-                if (e.Value != null && !metaDataReceived) {
-                    var buffer = e.Value; // Buffer 's data
-                    metaDataHandle = GCHandle.Alloc(buffer, GCHandleType.Pinned); // GCHandler for the buffer
-                    var pnt = metaDataHandle.AddrOfPinnedObject(); // Buffer 's address
-                    metaDataReceived = native_pointcloud_receiver_pinvoke.received_metadata(pnt, buffer.Length, pcl_id);
+            try
+            {
+                Debug.Log($"xxxjack received metadata");
+                lock (e)
+                {
+                    if (e.Value != null && !metaDataReceived)
+                    {
+                        var buffer = e.Value; // Buffer 's data
+                        metaDataHandle = GCHandle.Alloc(buffer, GCHandleType.Pinned); // GCHandler for the buffer
+                        var pnt = metaDataHandle.AddrOfPinnedObject(); // Buffer 's address
+                        metaDataReceived = native_pointcloud_receiver_pinvoke.received_metadata(pnt, buffer.Length, pcl_id);
+                        if (!metaDataReceived)
+                        {
+                            Debug.LogError("PCCerthReader: metadata received, but native_pointcloud_receiver_pinvoke.received_metadata() returned false");
+                        }
+                    }
                 }
+            } catch (System.Exception exc)
+            {
+                Debug.LogError($"PCCerthReader: OnNewMetaData: caught exception: {exc.Message}");
+                throw exc;
             }
         }
 
         // Updating the pointcloud every time a new buffer is received from the network
         private void OnNewPCLData(object sender, EventArgs<byte[]> e) {
-            lock (e) {
-                if (e.Value == null) {
-                    Debug.LogWarning("CerthReader: OnNewPCLData: received null data");
-                    return;
-                }
-
-                if (!metaDataReceived) {
-                    Debug.Log("CerthReader: OnNewPCLData: received data, but no metadata yet");
-                    return;
-
-                }
-                if (outQueue.Count < 2) { // FPA_TODO: Fix this using queue.Size
-                    // Flaging that a new buffer is received
-                    var buffer = e.Value; // Buffer 's data
-                    GCHandle rgbdHandle = GCHandle.Alloc(buffer, GCHandleType.Pinned); // GCHandler for the buffer
-                    System.IntPtr rgbdPtr = rgbdHandle.AddrOfPinnedObject(); // Buffer 's address
-                    System.IntPtr pclPtr = native_pointcloud_receiver_pinvoke.callColorizedPCloudFrameDLL(rgbdPtr, buffer.Length, pcl_id); // Pointer of the returned structure
-                    if (pclPtr == System.IntPtr.Zero)
-                    {
-                        Debug.LogWarning("CerthReader: callColorizedPCloudFrameDLL returned NULL");
+            try {
+                lock (e) {
+                    if (e.Value == null) {
+                        Debug.LogWarning("CerthReader: OnNewPCLData: received null data");
                         return;
                     }
-                    float[] bbox = { -1f, 1f, -1f, 0.5f, -3f, 1f };
-                    System.UInt64 timestamp = 0;
-                    cwipc.pointcloud pc = cwipc.from_certh(pclPtr, bbox, timestamp);
-                    if (pc == null)
-                    {
-                        Debug.LogWarning("CerthReader: cwipc.from_certh did not produce a pointcloud");
-                    }
-                    else
-                    {
-                        outQueue.Enqueue(pc);
-                        out2Queue.Enqueue(pc);
+
+                    if (!metaDataReceived) {
+                        Debug.Log("CerthReader: OnNewPCLData: received data, but no metadata yet");
+                        return;
 
                     }
-                    // Freeing the GCHandler
-                    rgbdHandle.Free();
+                    if (outQueue.Count < 2) { // FPA_TODO: Fix this using queue.Size
+                        // Flaging that a new buffer is received
+                        var buffer = e.Value; // Buffer 's data
+                        GCHandle rgbdHandle = GCHandle.Alloc(buffer, GCHandleType.Pinned); // GCHandler for the buffer
+                        System.IntPtr rgbdPtr = rgbdHandle.AddrOfPinnedObject(); // Buffer 's address
+                        System.IntPtr pclPtr = native_pointcloud_receiver_pinvoke.callColorizedPCloudFrameDLL(rgbdPtr, buffer.Length, pcl_id); // Pointer of the returned structure
+                        if (pclPtr == System.IntPtr.Zero)
+                        {
+                            Debug.LogWarning("CerthReader: callColorizedPCloudFrameDLL returned NULL");
+                            return;
+                        }
+                        float[] bbox = { -1f, 1f, -1f, 0.5f, -3f, 1f };
+                        System.UInt64 timestamp = 0;
+                        cwipc.pointcloud pc = cwipc.from_certh(pclPtr, bbox, timestamp);
+                        if (pc == null)
+                        {
+                            Debug.LogWarning("CerthReader: cwipc.from_certh did not produce a pointcloud");
+                        }
+                        else
+                        {
+                            outQueue.Enqueue(pc);
+                            out2Queue.Enqueue(pc);
+
+                        }
+                        // Freeing the GCHandler
+                        rgbdHandle.Free();
+                    }
                 }
             }
+            catch (System.Exception exc)
+            {
+                Debug.LogError($"PCCerthReader: OnNewPCLData: caught exception: {exc.Message}");
+                throw exc;
+            }
+
         }
     }
 }
