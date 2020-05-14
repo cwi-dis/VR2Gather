b4702c8fa5e1948b925b1c2972f0efe1297f69b8
Marc-i2Cat <marc.martos@i2cat.net>
2020-05-14 11:11:10 +0200

    Shader+TextRotating

---
+++ Assets/Tools/Scripts/RotateUp.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RotateUp : MonoBehaviour {
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+}
