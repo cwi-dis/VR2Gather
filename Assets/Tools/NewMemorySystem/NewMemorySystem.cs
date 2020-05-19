dfd6ca8fcd81864c1dc1dfe58d51decd58f19ed4
fernando.perez <fernando.perez@themoderncultural.com>
2020-05-19 11:01:59 +0200

    Changed bin2dash/Sub urls

--- Assets/Tools/NewMemorySystem/NewMemorySystem.cs
+++ Assets/Tools/NewMemorySystem/NewMemorySystem.cs
@@ -33,7 +33,7 @@ public class NewMemorySystem : MonoBehaviour
     // rtmp://127.0.0.1:1935/live/signals
     // Start is called before the first frame update
     void Start() {
-        Config._User cfg = Config.Instance.Users[0];
+        Config config = Config.Instance;
         if (forceMesh) {
             preparer = new Workers.MeshPreparer(preparerQueue);
             render = gameObject.AddComponent<Workers.PointMeshRenderer>();
@@ -44,35 +44,27 @@ public class NewMemorySystem : MonoBehaviour
             ((Workers.PointBufferRenderer)render).preparer = (Workers.BufferPreparer)preparer;
         }
 
-
-        var pcSelfConfig = cfg.PCSelfConfig;
         if (localPCs) {
             if(!useCompression)
-                reader = new Workers.RS2Reader(pcSelfConfig.RS2ReaderConfig.configFilename, pcSelfConfig.voxelSize, preparerQueue);
+                reader = new Workers.RS2Reader("../cameraconfig.xml", 0.01f, preparerQueue);
             else {
-                reader = new Workers.RS2Reader(pcSelfConfig.RS2ReaderConfig.configFilename, pcSelfConfig.voxelSize, encoderQueue);
-                encoder = new Workers.PCEncoder(cfg.PCSelfConfig.Encoders[0].octreeBits, encoderQueue, writerQueue);
+                reader = new Workers.RS2Reader("../cameraconfig.xml", 0.01f, encoderQueue);
+                encoder = new Workers.PCEncoder(10, encoderQueue, writerQueue);
                 decoder = new Workers.PCDecoder(writerQueue, preparerQueue);
             }
         } else {
-            reader = new Workers.RS2Reader(pcSelfConfig.RS2ReaderConfig.configFilename, pcSelfConfig.voxelSize, encoderQueue);
-            encoder = new Workers.PCEncoder(cfg.PCSelfConfig.Encoders[0].octreeBits, encoderQueue, writerQueue);
+            reader = new Workers.RS2Reader("../cameraconfig.xml", 0.01f, encoderQueue);
+            encoder = new Workers.PCEncoder(10, encoderQueue, writerQueue);
             string uuid = System.Guid.NewGuid().ToString();
-            var b2d = cfg.PCSelfConfig.Bin2Dash;
-            dashWriter = new Workers.B2DWriter("https://vrt-evanescent1.viaccess-orca.com/" + uuid + "/pcc/", "pointclouds", b2d.segmentSize, b2d.segmentLife, writerQueue);
-            var SUBConfig = cfg.SUBConfig;
-            dashReader = new Workers.PCSubReader("https://vrt-evanescent1.viaccess-orca.com/" + uuid + "/pcc/", "pointclouds", cfg.SUBConfig.streamNumber, cfg.SUBConfig.initialDelay, decoderQueue, true);
+            dashWriter = new Workers.B2DWriter("https://vrt-evanescent.viaccess-orca.com/" + uuid + "/", "pointclouds", 2000, 10000, writerQueue);
+            dashReader = new Workers.PCSubReader("https://vrt-evanescent.viaccess-orca.com/" + uuid + "/", "pointclouds", 0, 1, decoderQueue, true);
             decoder = new Workers.PCDecoder(decoderQueue, preparerQueue);
         }
 
         if (useVoice) {
             string uuid = System.Guid.NewGuid().ToString();
-            var audioB2D = cfg.PCSelfConfig.AudioBin2Dash;
-
-            gameObject.AddComponent<VoiceDashSender>().Init("https://vrt-evanescent1.viaccess-orca.com/" + uuid + "/", "audio", audioB2D.segmentSize, audioB2D.segmentLife); //Audio Pipeline
-
-            var audioConfig = cfg.AudioSUBConfig;
-            gameObject.AddComponent<VoiceDashReceiver>().Init("https://vrt-evanescent1.viaccess-orca.com/" + uuid + "/", "audio", audioConfig.streamNumber, audioConfig.initialDelay); //Audio Pipeline
+            gameObject.AddComponent<VoiceDashSender>().Init("https://vrt-evanescent.viaccess-orca.com/" + uuid + "/", "audio", 2000, 10000); //Audio Pipeline
+            gameObject.AddComponent<VoiceDashReceiver>().Init("https://vrt-evanescent.viaccess-orca.com/" + uuid + "/", "audio", 0, 1); //Audio Pipeline
 
         }
     }
