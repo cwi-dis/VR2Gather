0e9bcde9dbd34f7d3efad3bbd01b562533609b4e
Jack Jansen <Jack.Jansen@cwi.nl>
2020-06-05 15:32:40 +0200

    Handle queue better, and cater for getting null object back.

--- Assets/UserRepresentation/Audio/Scripts/Workers/Codecs/VoiceDecoder.cs
+++ Assets/UserRepresentation/Audio/Scripts/Workers/Codecs/VoiceDecoder.cs
@@ -30,32 +30,29 @@ namespace Workers {
         NTPTools.NTPTime tempTime;
         protected override void Update() {
             base.Update();
-            if (inQueue._CanDequeue()) { 
-                NativeMemoryChunk mcIn = (NativeMemoryChunk)inQueue.Dequeue();
-                if (outQueue._CanEnqueue()) {
+            NativeMemoryChunk mcIn = (NativeMemoryChunk)inQueue.Dequeue();
+            if (mcIn == null) return;
 #if USE_SPEEX
-                    byte[] buffer = new byte[mcIn.length];
-                    if (temporalBuffer == null) temporalBuffer = new float[mcIn.length * 10]; // mcIn.length*10
-                    System.Runtime.InteropServices.Marshal.Copy(mcIn.pointer, buffer, 0, mcIn.length);
-                    int len = decoder.Decode(buffer, 0, mcIn.length, temporalBuffer, 0);
+            byte[] buffer = new byte[mcIn.length];
+            if (temporalBuffer == null) temporalBuffer = new float[mcIn.length * 10]; // mcIn.length*10
+            System.Runtime.InteropServices.Marshal.Copy(mcIn.pointer, buffer, 0, mcIn.length);
+            int len = decoder.Decode(buffer, 0, mcIn.length, temporalBuffer, 0);
 #else
-                    int len = mcIn.length / 4;
-                    if (temporalBuffer == null) temporalBuffer = new float[len];
-                    System.Runtime.InteropServices.Marshal.Copy(mcIn.pointer, temporalBuffer, 0, len);
+            int len = mcIn.length / 4;
+            if (temporalBuffer == null) temporalBuffer = new float[len];
+            System.Runtime.InteropServices.Marshal.Copy(mcIn.pointer, temporalBuffer, 0, len);
 #endif
-                    FloatMemoryChunk mcOut = new FloatMemoryChunk(len * 6);
-                    for (int i = 0; i < len; ++i) {
-                        mcOut.buffer[i * 6 + 0] =
-                        mcOut.buffer[i * 6 + 1] =
-                        mcOut.buffer[i * 6 + 2] =
-                        mcOut.buffer[i * 6 + 3] =
-                        mcOut.buffer[i * 6 + 4] =
-                        mcOut.buffer[i * 6 + 5] = temporalBuffer[i];
-                    }
-                    outQueue.Enqueue(mcOut);
-                }
-                mcIn.free();
+            FloatMemoryChunk mcOut = new FloatMemoryChunk(len * 6);
+            for (int i = 0; i < len; ++i) {
+                mcOut.buffer[i * 6 + 0] =
+                mcOut.buffer[i * 6 + 1] =
+                mcOut.buffer[i * 6 + 2] =
+                mcOut.buffer[i * 6 + 3] =
+                mcOut.buffer[i * 6 + 4] =
+                mcOut.buffer[i * 6 + 5] = temporalBuffer[i];
             }
+            outQueue.Enqueue(mcOut);
+            mcIn.free();
         }
     }
 }
