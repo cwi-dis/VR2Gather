836410b687676ede9814d0d6a4a7ac817232a161
fernando.perez <fernando.perez@themoderncultural.com>
2020-06-09 11:21:45 +0200

    Add cutout shader for pointclouds.

--- Assets/UserRepresentation/PointClouds/Scripts/Workers/Renderers/PointBufferRenderer.cs
+++ Assets/UserRepresentation/PointClouds/Scripts/Workers/Renderers/PointBufferRenderer.cs
@@ -13,13 +13,7 @@ namespace Workers
 
         // Start is called before the first frame update
         void Start() {
-            if (material == null) {
-                material = new Material(Shader.Find("Entropy/PointCloud"));
-                //material = new Material(Resources.Load<Shader>("PointCloudBuffer"));
-                material.SetFloat("_PointSize", 0.008f );
-                material.SetColor("_Tint", Color.gray);
-                material.hideFlags = HideFlags.DontSave;
-            }
+            if (material == null)  material = Resources.Load<Material>("PointClouds");
         }
 
         private void Update() {
@@ -34,24 +28,7 @@ namespace Workers
             Graphics.DrawProcedural(material, new Bounds(transform.position, Vector3.one*2), MeshTopology.Points, pointCount, 1);
             statsUpdate(pointCount);
         }
-        /*
-        void OnRenderObject() {
-            material.SetFloat("_PointSize", preparer.GetPointSize());
-            pointCount = preparer.GetComputeBuffer(ref pointBuffer);
-            if (pointCount == 0 || pointBuffer == null || !pointBuffer.IsValid()) return;
-            var camera = Camera.current;
-            if ((camera.cullingMask & (1 << gameObject.layer)) == 0) return;
-            if (camera.name == "Preview Scene Camera") return;
-
-            // TODO: Do view frustum culling here.
-            material.SetBuffer("_PointBuffer", pointBuffer);
-            material.SetMatrix("_Transform", transform.localToWorldMatrix);
-            material.SetPass(0);
-
-            Graphics.DrawProceduralNow(MeshTopology.Points, pointCount, 1);
-            statsUpdate(pointCount);
-        }
-        */
+     
         public void OnDestroy() {
             if (pointBuffer != null) { pointBuffer.Release(); pointBuffer = null; }
             if (material != null) { Destroy(material); material = null; }
