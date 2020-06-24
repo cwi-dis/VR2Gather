82ecafdff29ef1dad3557e05e79862fb193710bd
Jack Jansen <Jack.Jansen@cwi.nl>
2020-06-24 21:35:48 +0200

    Added locking to AudioPreparer. Untested.

--- Assets/UserRepresentation/Audio/Scripts/Workers/Preparers/AudioPreparer.cs
+++ Assets/UserRepresentation/Audio/Scripts/Workers/Preparers/AudioPreparer.cs
@@ -33,63 +33,83 @@ namespace Workers {
         protected override void Update() {
             base.Update();
             if (inQueue.IsClosed()) return;
-            FloatMemoryChunk mc = (FloatMemoryChunk)inQueue.TryDequeue(1);
-            if (mc == null) return;
-            // xxxjack attempting to drop audio if there is too much in the buffer already                
-            int bytesInAudioBuffer = (writePosition - readPosition) % bufferSize;
-            if (bytesInAudioBuffer > preferredBufferFill) {
-                Debug.Log($"AudioPreparer: audioBuffer has {bytesInAudioBuffer} already, dropping audio");
-                mc.free();
-                return;
-            }
+            lock(circularBuffer)
+            {
+                FloatMemoryChunk mc = (FloatMemoryChunk)inQueue.TryDequeue(1);
+                if (mc == null) return;
+                // xxxjack attempting to drop audio if there is too much in the buffer already                
+                int bytesInAudioBuffer = (writePosition - readPosition) % bufferSize;
+                if (bytesInAudioBuffer > preferredBufferFill)
+                {
+                    Debug.Log($"AudioPreparer: audioBuffer has {bytesInAudioBuffer} already, dropping audio");
+                    mc.free();
+                    return;
+                }
 
-            int len = mc.elements;
-            statsUpdate(len);
-            if (writePosition + len < bufferSize) {
-                System.Array.Copy(mc.buffer, 0, circularBuffer, writePosition, len);
-                writePosition += len;
-            } else {
-                int partLen = bufferSize - writePosition;
-                System.Array.Copy(mc.buffer, 0, circularBuffer, writePosition, partLen);
-                System.Array.Copy(mc.buffer, partLen, circularBuffer, 0, len - partLen);
-                writePosition = len - partLen;
+                int len = mc.elements;
+                statsUpdate(len);
+                if (writePosition + len < bufferSize)
+                {
+                    System.Array.Copy(mc.buffer, 0, circularBuffer, writePosition, len);
+                    writePosition += len;
+                }
+                else
+                {
+                    int partLen = bufferSize - writePosition;
+                    System.Array.Copy(mc.buffer, 0, circularBuffer, writePosition, partLen);
+                    System.Array.Copy(mc.buffer, partLen, circularBuffer, 0, len - partLen);
+                    writePosition = len - partLen;
+                }
+                mc.free();
             }
-            mc.free();
         }
 
         public int available {
             get {
-                if (writePosition < readPosition)
-                    return (bufferSize - readPosition) + writePosition; // Looped
-                return writePosition - readPosition;
+                lock(circularBuffer)
+                {
+                    if (writePosition < readPosition)
+                        return (bufferSize - readPosition) + writePosition; // Looped
+                    return writePosition - readPosition;
+                }
             }
         }
 
         bool firstTime = true;
         public bool GetAudioBuffer(float[] dst, int len) {
-            if ((firstTime && available >= len) || !firstTime) {
-                firstTime = false;
-                if (available >= len) {
-                    if (writePosition < readPosition){ // Se ha dado la vuelta.
-                        int partLen = bufferSize - readPosition;
-                        if (partLen > len) {
+            lock(circularBuffer)
+            {
+                if ((firstTime && available >= len) || !firstTime)
+                {
+                    firstTime = false;
+                    if (available >= len)
+                    {
+                        if (writePosition < readPosition)
+                        { // Se ha dado la vuelta.
+                            int partLen = bufferSize - readPosition;
+                            if (partLen > len)
+                            {
+                                System.Array.Copy(circularBuffer, readPosition, dst, 0, len);
+                                readPosition += len;
+                            }
+                            else
+                            {
+                                System.Array.Copy(circularBuffer, readPosition, dst, 0, partLen);
+                                System.Array.Copy(circularBuffer, 0, dst, partLen, len - partLen);
+                                readPosition = len - partLen;
+                            }
+                        }
+                        else
+                        {
                             System.Array.Copy(circularBuffer, readPosition, dst, 0, len);
                             readPosition += len;
                         }
-                        else {
-                            System.Array.Copy(circularBuffer, readPosition, dst, 0, partLen);
-                            System.Array.Copy(circularBuffer, 0, dst, partLen, len - partLen);
-                            readPosition = len - partLen;
-                        }
+                        return true;
                     }
-                    else {
-                        System.Array.Copy(circularBuffer, readPosition, dst, 0, len);
-                        readPosition += len;
-                    }
-                    return true;
                 }
+                return false;
             }
-            return false;
+
         }
 
         System.DateTime statsLastTime;
