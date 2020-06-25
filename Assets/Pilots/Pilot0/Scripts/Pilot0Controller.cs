5d6b37e0483aa8be130e8f73e2228f8a13a34e2d
Marc-i2Cat <marc.martos@i2cat.net>
2020-06-25 14:51:12 +0200

    New Voyeur & Ghost Users
    - Voyeur is NONE representation (Only listen)
    - Ghost is SPECTATOR representation (Can Talk)

--- Assets/Pilots/Pilot0/Scripts/Pilot0Controller.cs
+++ Assets/Pilots/Pilot0/Scripts/Pilot0Controller.cs
@@ -3,11 +3,15 @@
 public class Pilot0Controller : PilotController {
 
     public PlayerManager[] players;
+    public PlayerManager[] spectators;
+    public GameObject voyeur;
 
     // Start is called before the first frame update
     public override void Start() { 
         base.Start();
-        LoadPlayers(players);
+        LoadPlayers(players, spectators);
+        if (OrchestratorController.Instance.SelfUser.userData.userRepresentationType == OrchestratorWrapping.UserData.eUserRepresentationType.__NONE__)
+            voyeur.SetActive(true);
     }
 
     public override void MessageActivation(string message) { }
