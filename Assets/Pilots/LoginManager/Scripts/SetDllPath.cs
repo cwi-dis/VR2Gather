01cc1241c8513694e846d906c3a3523cc70f4234
Marc-i2Cat <marc.martos@i2cat.net>
2020-06-26 12:12:15 +0200

    Dll loading quick fix

--- Assets/Pilots/LoginManager/Scripts/SetDllPath.cs
+++ Assets/Pilots/LoginManager/Scripts/SetDllPath.cs
@@ -5,7 +5,7 @@ public class SetDllPath {
     static void OnBeforeSceneLoadRuntimeMethod() {
         string pathvar = System.Environment.GetEnvironmentVariable("PATH");
         //string dllsPath = $"{Application.dataPath}/../dlls/";
-        string dllsPath = $"{System.IO.Directory.GetParent(Application.dataPath).ToString()}/dlls/";
+        string dllsPath = $"{System.IO.Directory.GetParent(Application.dataPath).ToString()}/dll/";
         if (System.IO.Directory.Exists(dllsPath)) { 
             System.Environment.SetEnvironmentVariable("PATH", $"{dllsPath};{pathvar}", System.EnvironmentVariableTarget.Process);
             System.Environment.SetEnvironmentVariable("SIGNALS_SMD_PATH", dllsPath);
