5820c9a4272c5f6b8d93c4a7e3c6891917f3b850
Jack Jansen <Jack.Jansen@cwi.nl>
2020-11-02 00:33:26 +0100

    Allow changing height by using the scroll whell. Note that this should really only be allowed for spectators and no-representation users, probably.

--- Assets/Tools/Scripts/MoveCamera.cs
+++ Assets/Tools/Scripts/MoveCamera.cs
@@ -12,6 +12,9 @@ public class MoveCamera : MonoBehaviour {
 
     void Awake() {
         if (XRDevice.isPresent) {
+            // Note by Jack: there is something to be said for allowing this behaviour also for HMD
+            // users, *for some scenarios*. It may be useful (even though the usual caveats about
+            // motion sickness when forcibly moving HMD users' viewpoint apply).
             enabled = false;
         }
         else {
@@ -33,11 +36,21 @@ public class MoveCamera : MonoBehaviour {
             yRotation += mouseX;
 
             if (spectator)
+            {
                 playerBody.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
-            else {
+            }
+            else
+            {
                 transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
                 playerBody.Rotate(Vector3.up, mouseX);
                 avatarHead.Rotate(Vector3.right, -mouseY);
+            }
+            // Note by Jack: spectators and no-representation users should be able to move their viewpoint up and down.
+            // with the current implementation all users have this ability, which may or may not be a good idea.
+            float deltaHeight = Input.GetAxis("Mouse ScrollWheel");
+            if (deltaHeight != 0)
+            {
+                playerBody.Translate(0, deltaHeight, 0);
             }
         }
     }
