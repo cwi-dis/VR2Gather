3d368369f6e5da73d510f854a535342aa04b8b68
fernando.perez <fernando.perez@themoderncultural.com>
2020-11-11 10:05:59 +0100

    - Removed circular buffers (again). - Microphone uses max frequency.

--- Assets/UserRepresentation/Audio/Scripts/Workers/Preparers/AudioPreparer.cs
+++ Assets/UserRepresentation/Audio/Scripts/Workers/Preparers/AudioPreparer.cs
@@ -4,23 +4,14 @@ using UnityEngine;
 
 namespace Workers {
     public class AudioPreparer : BaseWorker {
-        float[] circularBuffer;
         int bufferSize;
-        int writePosition;
-        int readPosition;
-        int preferredBufferFill;
 
         QueueThreadSafe inQueue;
 
-        public AudioPreparer(QueueThreadSafe _inQueue, int _preferredBufferFill=0) : base(WorkerType.End) {
+        public AudioPreparer(QueueThreadSafe _inQueue) : base(WorkerType.End) {
             inQueue = _inQueue;
             if (inQueue == null) Debug.LogError($"AudioPreparer: Programmer error: ERROR inQueue=NULL");
             bufferSize = 320 * 6 * 100;
-            if (_preferredBufferFill == 0) _preferredBufferFill = bufferSize + 1;
-            preferredBufferFill = _preferredBufferFill;
-            circularBuffer = new float[bufferSize];
-            writePosition = 0;
-            readPosition = 0;
             Debug.Log("AudioPreparer: Started.");
             Start();
         }
@@ -32,86 +23,29 @@ namespace Workers {
 
         protected override void Update() {
             base.Update();
-            if (inQueue.IsClosed()) return;
-            lock(circularBuffer)
-            {
-                FloatMemoryChunk mc = (FloatMemoryChunk)inQueue.TryDequeue(1);
-                if (mc == null) return;
-                // Drop audio if there is too much in the buffer already
-                bool didDrop = false;            
-                int bytesInAudioBuffer = (writePosition - readPosition) % bufferSize;
-                if (bytesInAudioBuffer > preferredBufferFill)
-                {
-                    Debug.Log($"AudioPreparer: audioBuffer has {bytesInAudioBuffer} already, dropping audio");
-                    didDrop = true;
-                    mc.free();
-                    return;
-                }
-
-                int len = mc.elements;
-                statsUpdate(len, didDrop);
-                if (writePosition + len < bufferSize)
-                {
-                    System.Array.Copy(mc.buffer, 0, circularBuffer, writePosition, len);
-                    writePosition += len;
-                }
-                else
-                {
-                    int partLen = bufferSize - writePosition;
-                    System.Array.Copy(mc.buffer, 0, circularBuffer, writePosition, partLen);
-                    System.Array.Copy(mc.buffer, partLen, circularBuffer, 0, len - partLen);
-                    writePosition = len - partLen;
-                }
-                mc.free();
-            }
         }
 
         public int available {
             get {
-                lock(circularBuffer)
-                {
-                    if (writePosition < readPosition)
-                        return (bufferSize - readPosition) + writePosition; // Looped
-                    return writePosition - readPosition;
+                lock (inQueue) {
+                    if (!inQueue.IsClosed())
+                        return inQueue._Count * VoiceReader.wantedOutputBufferSize * 2;
+                    else
+                        return 0;
                 }
             }
         }
 
         bool firstTime = true;
         public bool GetAudioBuffer(float[] dst, int len) {
-            lock(circularBuffer)
-            {
-                if ((firstTime && available >= len) || !firstTime)
-                {
-                    firstTime = false;
-                    if (available >= len)
-                    {
-                        if (writePosition < readPosition)
-                        { // Se ha dado la vuelta.
-                            int partLen = bufferSize - readPosition;
-                            if (partLen > len)
-                            {
-                                System.Array.Copy(circularBuffer, readPosition, dst, 0, len);
-                                readPosition += len;
-                            }
-                            else
-                            {
-                                System.Array.Copy(circularBuffer, readPosition, dst, 0, partLen);
-                                System.Array.Copy(circularBuffer, 0, dst, partLen, len - partLen);
-                                readPosition = len - partLen;
-                            }
-                        }
-                        else
-                        {
-                            System.Array.Copy(circularBuffer, readPosition, dst, 0, len);
-                            readPosition += len;
-                        }
-                        return true;
-                    }
+            lock (inQueue) {
+                if (!inQueue.IsClosed()) {
+                    FloatMemoryChunk mc = (FloatMemoryChunk)inQueue.TryDequeue(1);
+                    if (mc == null) return false;
+                    System.Array.Copy(mc.buffer, 0, dst, 0, len);
                 }
-                return false;
             }
-
+            return true;
         }
 
         System.DateTime statsLastTime;
@@ -132,7 +66,7 @@ namespace Workers {
             if (System.DateTime.Now > statsLastTime + System.TimeSpan.FromSeconds(10))
             {
                 double samplesInBufferAverage = statsTotalSamplesInOutputBuffer / statsTotalUpdates;
-                double timeInBufferAverage = samplesInBufferAverage / VoiceReader.wantedInputSampleRate; // xxxjack or is this outputSampleRate???
+                double timeInBufferAverage = samplesInBufferAverage / VoiceReader.wantedOutputSampleRate;
                 Debug.Log($"stats: ts={(int)System.DateTime.Now.TimeOfDay.TotalSeconds}: {Name()}: {statsTotalUpdates / 10} fps, {(int)samplesInBufferAverage} samples output latency, {(int)(timeInBufferAverage * 1000)} ms output latency, {statsDrops/10} drops per second");
                 statsTotalUpdates = 0;
                 statsTotalSamplesInOutputBuffer = 0;
