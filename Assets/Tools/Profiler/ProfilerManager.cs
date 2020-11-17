d99bfdb6304c08b754130c753a0b0cd17182aab0
Jack Jansen <Jack.Jansen@cwi.nl>
2020-11-17 01:24:19 +0100

    Added stats: logging calls that should enable us to find the profile.csv file (especially if we make the filename session-dependent), and matching up the ideas of timestamps.

--- Assets/Tools/Profiler/ProfilerManager.cs
+++ Assets/Tools/Profiler/ProfilerManager.cs
@@ -5,11 +5,13 @@ using System.Text;
 
 public class ProfilerManager : MonoBehaviour {
     public string fileName = "Profiler";
+    public string csvOutputPathname;
     public float SamplingRate = 1/30f;
     public bool FPSActive = true;
     public bool HMDActive = Config.Instance.pilot3NavigationLogs;
     public bool TVMActive = false;
     public GameObject[] TVMs;
+    private bool printedStatMessage = false;
 
     private float timeToNext = 0.0f;
     private uint lineCount = 0;
@@ -19,10 +21,12 @@ public class ProfilerManager : MonoBehaviour {
 
     private void Awake() {
         Instance = this;
+        csvOutputPathname = string.Format("{0}/../{1}.csv", Application.persistentDataPath, fileName);
+
         //Moved to update for pilot 3
         //HMD = FindObjectOfType<Camera>().gameObject.transform;
-        
-    }       
+
+    }
 
     List<BaseProfiler> profiles = new List<BaseProfiler>();
 
@@ -36,6 +40,7 @@ public class ProfilerManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        // xxxjack I don't like all this comment-edout code.... Why? Why not set the corresponding boolean to false?
         //if (FPSActive) AddProfiler(new FPSProfiler());
         //Moved to update for pilot 3
         //if (HMDActive) AddProfiler(new HMDProfiler(HMD));
@@ -44,6 +49,12 @@ public class ProfilerManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+        // xxxjack should bail out quickly if no profilers active
+        if (!printedStatMessage)
+        {
+            Debug.Log($"stats: ts={(int)System.DateTime.Now.TimeOfDay.TotalSeconds}, component=ProfilerManager, started=1, csv_output={csvOutputPathname}, TimeSinceGameStart={Time.time}, FPSActive={FPSActive}, HMDActive={HMDActive}, TVMActive={TVMActive}");
+            printedStatMessage = true;
+        }
         if (HMDActive == true)
         {
             var cam = FindObjectOfType<Camera>().gameObject;
@@ -67,8 +78,11 @@ public class ProfilerManager : MonoBehaviour {
     }
     
     private void OnApplicationQuit() {
+        // xxxjack should bail out quickly if no profilers active
+        // xxxjack seems pretty scary to not write before OnApplicationQuit....
         //UnityEngine.Debug.Log("<color=red>XXXShishir: </color> Writing nav logs to " + string.Format("{0}/../{1}.csv", Application.persistentDataPath, fileName));
         StringBuilder sb = new StringBuilder();
+        Debug.Log($"stats: ts={(int)System.DateTime.Now.TimeOfDay.TotalSeconds}, component=ProfilerManager, finished=1, csv_output={csvOutputPathname}, TimeSinceGameStart={Time.time}");
         if (profiles.Count > 0) {
             foreach (var profile in profiles)
                 profile.GetHeaders(sb);
@@ -81,7 +95,7 @@ public class ProfilerManager : MonoBehaviour {
                 sb.AppendLine();
             }
             //string time = System.DateTime.Now.ToString("yyyyMMddHmm");
-            System.IO.File.WriteAllText(string.Format("{0}/../{1}.csv", Application.persistentDataPath, fileName), sb.ToString());
+            System.IO.File.WriteAllText(csvOutputPathname, sb.ToString());
         }
     }
 }
