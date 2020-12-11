20f117d97c11a462da86ec2759f454b83e1b986f
Jack Jansen <Jack.Jansen@cwi.nl>
2020-12-11 15:32:26 +0100

    Print warning of pipline register function cannot be found.

--- Assets/VRTCore/Player/PlayerManager.cs
+++ Assets/VRTCore/Player/PlayerManager.cs
@@ -20,16 +20,19 @@ public class PlayerManager : MonoBehaviour {
         {
             System.Type pcPipelineType = System.Type.GetType("PointCloudPipeline");
             MethodInfo pcRegister = pcPipelineType?.GetMethod("Register", BindingFlags.Static | BindingFlags.Public);
+            if (pcRegister == null) Debug.LogWarning("PlayerManager: No support for Pointclouds, PointCloudPipeline type or register-function not found");
             pcRegister?.Invoke(null, null);
         }
         {
             System.Type wcPipelineType = System.Type.GetType("WebCamPipeline");
             MethodInfo wcRegister = wcPipelineType?.GetMethod("Register", BindingFlags.Static | BindingFlags.Public);
+            if (wcRegister == null) Debug.LogWarning("PlayerManager: No support for WebCam, WebCamPipeline type or register-function not found");
             wcRegister?.Invoke(null, null);
         }
         {
             System.Type tvmPipelineType = System.Type.GetType("TVMPipeline");
             MethodInfo tvmRegister = tvmPipelineType?.GetMethod("Register", BindingFlags.Static | BindingFlags.Public);
+            if (tvmRegister == null) Debug.LogWarning("PlayerManager: No support for TVMs, TVMPipeline type or register-function not found");
             tvmRegister?.Invoke(null, null);
         }
     }
