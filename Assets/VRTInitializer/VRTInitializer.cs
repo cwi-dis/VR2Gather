ca2db0e80f523fbcb6b58946147330ad8c9b4f41
Jack Jansen <Jack.Jansen@cwi.nl>
2020-12-11 16:07:40 +0100

    Different way to register pipeline-type constructors: created VRTInitializer object and added it to LoginManager graph.

---
+++ Assets/VRTInitializer/VRTInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRTInitializer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("Initializer: Registering pipelines");
+        PointCloudPipeline.Register();
+        WebCamPipeline.Register();
+    }
+
+    private void Start()
+    {
+        Debug.Log("Initializer: Start");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
