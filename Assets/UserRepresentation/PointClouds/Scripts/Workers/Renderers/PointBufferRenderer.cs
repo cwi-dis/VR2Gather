04f9186bc7dab9f00f48b16a3475c58c530724e8
Jack Jansen <Jack.Jansen@cwi.nl>
2021-01-08 18:10:05 +0100

    Print average rendered pointsize in stats.

--- Assets/UserRepresentation/PointClouds/Scripts/Workers/Renderers/PointBufferRenderer.cs
+++ Assets/UserRepresentation/PointClouds/Scripts/Workers/Renderers/PointBufferRenderer.cs
@@ -37,13 +37,14 @@ namespace VRT.UserRepresentation.PointCloud
         private void Update()
         {
             pointCount = preparer.GetComputeBuffer(ref pointBuffer);
+            float pointSize = preparer.GetPointSize();
             if (pointCount == 0 || pointBuffer == null || !pointBuffer.IsValid()) return;
             block.SetBuffer("_PointBuffer", pointBuffer);
-            block.SetFloat("_PointSize", preparer.GetPointSize());
+            block.SetFloat("_PointSize", pointSize);
             block.SetMatrix("_Transform", transform.localToWorldMatrix);
 
             Graphics.DrawProcedural(material, new Bounds(transform.position, Vector3.one * 2), MeshTopology.Points, pointCount, 1, null, block);
-            statsUpdate(pointCount, preparer.currentTimestamp);
+            statsUpdate(pointCount, pointSize, preparer.currentTimestamp);
         }
 
         public void OnDestroy()
@@ -57,9 +58,10 @@ namespace VRT.UserRepresentation.PointCloud
         System.DateTime statsLastTime;
         double statsTotalPointcloudCount = 0;
         double statsTotalPointCount = 0;
+        double statsTotalPointSize = 0;
         const int statsInterval = 10;
 
-        public void statsUpdate(int pointCount, ulong timestamp)
+        public void statsUpdate(int pointCount, float pointSize, ulong timestamp)
         {
             System.TimeSpan sinceEpoch = System.DateTime.UtcNow - new System.DateTime(1970, 1, 1);
             if (statsLastTime == null)
@@ -67,16 +69,19 @@ namespace VRT.UserRepresentation.PointCloud
                 statsLastTime = System.DateTime.Now;
                 statsTotalPointcloudCount = 0;
                 statsTotalPointCount = 0;
+                statsTotalPointSize = 0;
             }
             if (System.DateTime.Now > statsLastTime + System.TimeSpan.FromSeconds(statsInterval))
             {
-                Debug.Log($"stats: ts={System.DateTime.Now.TimeOfDay.TotalSeconds:F3}, component={Name()}, fps={statsTotalPointcloudCount / statsInterval}, points_per_cloud={(int)(statsTotalPointCount / (statsTotalPointcloudCount == 0 ? 1 : statsTotalPointcloudCount))}, pc_timestamp={timestamp}, pc_latency_ms={(ulong)sinceEpoch.TotalMilliseconds - timestamp}");
+                Debug.Log($"stats: ts={System.DateTime.Now.TimeOfDay.TotalSeconds:F3}, component={Name()}, fps={statsTotalPointcloudCount / statsInterval}, points_per_cloud={(int)(statsTotalPointCount / (statsTotalPointcloudCount == 0 ? 1 : statsTotalPointcloudCount))}, avg_pointsize={(statsTotalPointSize / (statsTotalPointcloudCount == 0 ? 1 : statsTotalPointcloudCount))}, pc_timestamp={timestamp}, pc_latency_ms={(ulong)sinceEpoch.TotalMilliseconds - timestamp}");
                 statsTotalPointcloudCount = 0;
                 statsTotalPointCount = 0;
+                statsTotalPointSize = 0;
                 statsLastTime = System.DateTime.Now;
             }
             statsTotalPointCount += pointCount;
             statsTotalPointcloudCount += 1;
+            statsTotalPointSize += pointSize;
         }
     }
 }
