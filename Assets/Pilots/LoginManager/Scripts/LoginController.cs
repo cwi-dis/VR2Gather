93db80338a7c83be570936bf306422740df09bfa
Jack Jansen <Jack.Jansen@cwi.nl>
2021-01-29 13:52:37 +0100

    Created PilotRegsitry class to manage mapping pilot names to scenario names.

--- Assets/Pilots/LoginManager/Scripts/LoginController.cs
+++ Assets/Pilots/LoginManager/Scripts/LoginController.cs
@@ -17,50 +17,6 @@ public class LoginController : PilotController {
     //AsyncOperation async;
     Coroutine loadCoroutine = null;
 
-    public static string GetSceneNameForPilotName(string pilotName, string pilotVariant)
-    {
-        // Note: Pilot scenes need to be registered here, but also added to the "scenes in build"
-        // through Unity Editor File->Build Settings dialog.
-        switch (pilotName)
-        {
-            case "Pilot 0": 
-                return "Pilot0";
-            case "Pilot 1":
-                return "Pilot1";
-            case "Pilot 2":
-                switch (pilotVariant)
-                {
-                    case "0": // NONE
-                        Config.Instance.presenter = Config.Presenter.None;
-                        break;
-                    case "1": // LOCAL
-                        Config.Instance.presenter = Config.Presenter.Local;
-                        break;
-                    case "2": // LIVE
-                        Config.Instance.presenter = Config.Presenter.Live;
-                        break;
-                    default:
-                        break;
-                }
-                if (OrchestratorController.Instance.UserIsMaster && Config.Instance.presenter == Config.Presenter.Live)
-                {
-                    return "Pilot2_Presenter";
-                }
-                return "Pilot2_Player";
-            case "Pilot 3":
-                return "Pilot3";
-            case "Museum":
-                return "Museum";
-            case "HoloConference":
-                return "HoloMeet";
-            case "MedicalExamination":
-                return "MedicalExamination";
-            default:
-                Debug.LogError($"Selected scenario \"{pilotName}\" not implemented in this player");
-                return null;
-        }
-    }
-
     void Awake() {
         if (!XRDevice.isPresent) {
             Resolution[] resolutions = Screen.resolutions;
@@ -109,7 +65,7 @@ public class LoginController : PilotController {
             string pilotName = msg[1];
             string pilotVariant = null;
             if (msg.Length > 3) pilotVariant = msg[3];
-            string sceneName = GetSceneNameForPilotName(pilotName, pilotVariant);
+            string sceneName = PilotRegistry.GetSceneNameForPilotName(pilotName, pilotVariant);
             if (loadCoroutine == null) loadCoroutine = StartCoroutine(RefreshAndLoad(sceneName));
         }
         else if (msg[0] == MessageType.READY) {
