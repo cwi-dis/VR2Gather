6c2848cd31d67ea41fca7826d2129cb40d251b3b
Shishir Subramanyam <s.subramanyam@cwi.nl>
2021-02-12 10:41:34 +0100

    Updated rating onclick event, needed to set canvas world camera to register UI events correctly

--- Assets/Pilots/QualityAssesment/Scripts/RatingSceneController.cs
+++ Assets/Pilots/QualityAssesment/Scripts/RatingSceneController.cs
@@ -10,14 +10,28 @@ namespace Assets.Pilots.QualityAssesment.Scripts
 {
     public class RatingSceneController : MonoBehaviour
     {
-
+        private bool ratingRegistered = false;
+        private bool camFound = false;
+        private int ratingValue;
         //xxxshishir add rating variables and logging info here
-
+        public void registerScore(int rating)
+        {
+            ratingValue = rating;
+            Debug.Log("<color=green> Rating Registered:  </color>" + ratingValue);
+            ratingRegistered = true;
+        }
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Escape))
+            if (Input.GetKeyDown(KeyCode.Escape) || ratingRegistered==true)
             {
                 SceneManager.LoadScene("QualityAssesment");
+            }
+            if (camFound==false)
+            {
+                var cam = FindObjectOfType<Camera>();
+                Canvas ratingCanvas = FindObjectOfType<Canvas>();
+                ratingCanvas.worldCamera = cam;
+                camFound = true;
             }
         }
     }
