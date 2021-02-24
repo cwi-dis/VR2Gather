a00ec89113431f23286c576389ce62b5b183e327
Jack Jansen <Jack.Jansen@cwi.nl>
2021-02-25 00:58:24 +0100

    Implemented saving stats to file.

--- Assets/VRTCore/BaseStats.cs
+++ Assets/VRTCore/BaseStats.cs
@@ -9,16 +9,37 @@ namespace VRT.Core
         protected System.DateTime statsLastTime;
         private static bool initialized = false;
         private static double statsInterval = 10;
+        private static System.IO.StreamWriter statsStream;
 
         private static void Init()
         {
             statsInterval = Config.Instance.statsInterval;
+            if (Config.Instance.statsOutputFile != "")
+            {
+                string statsFilename = $"{Application.persistentDataPath}/{Config.Instance.statsOutputFile}";
+                statsStream = new System.IO.StreamWriter(statsFilename, true);
+                //
+                // Write an identifying line to both the statsfile (so we can split runs) and the console (so we can find the stats file)
+                //
+                string statsLine = $"stats: ts={System.DateTime.Now.TimeOfDay.TotalSeconds:F3}, component=stats, starting=1, statsFilename={statsFilename}";
+                statsStream.WriteLine(statsLine);
+                statsStream.Flush();
+                Debug.Log(statsLine);
+            }
             initialized = true;
         }
 
+        static void Flush()
+        {
+            // xxxjack: need to check performance penalty of this.
+            // If it turns out to be too much we call at most every second or something like that.
+            if (statsStream != null) statsStream.Flush();
+
+        }
         private static void DeInit()
         {
             Debug.Log("xxxjack BaseStats DeInit called");
+            if (statsStream != null) statsStream.Flush();
         }
         protected BaseStats(string _name)
         {
@@ -53,7 +74,16 @@ namespace VRT.Core
 
         protected void Output(string s)
         {
-            Debug.Log($"stats: ts={System.DateTime.Now.TimeOfDay.TotalSeconds:F3}, component={name}, {s}");
+            string statsLine = $"stats: ts={System.DateTime.Now.TimeOfDay.TotalSeconds:F3}, component={name}, {s}";
+            if (statsStream == null)
+            {
+                Debug.Log(statsLine);
+            }
+            else
+            {
+                statsStream.WriteLine(statsLine);
+                Flush();
+            }
         }
     }
 }
