1afa38d4928d64b9449ae90ab17dac295b1aa2de
ARUGULA\VRTogether <cwidisgroup@gmail.com>
2021-03-03 19:38:33 +0100

    Improved aesthetics in the scenes

--- Assets/Pilots/QualityAssesment/Scripts/QualityAssesmentController.cs
+++ Assets/Pilots/QualityAssesment/Scripts/QualityAssesmentController.cs
@@ -1,21 +1,31 @@
 ﻿using UnityEngine;
 using VRT.Pilots.Common;
-
+using VRTCore;
+using VRT.Core;
+using VRT.Orchestrator.Wrapping;
 //XXXShishir switched back to original pilot0controller, ToDo: Cherry pick the rating scale scene transitions, ToDo: Reimplement scene controller to use modified entity pipeline later
 //Note: Use scenemanager.loadsceneasync for the rating scale scene once remote user prerecorded view is reimplemented
-public class QualityAssesmentController : PilotController {
-    public static QualityAssesmentController Instance { get; private set; }
+namespace VRT.UserRepresentation.PointCloud
+{
+    public class QualityAssesmentController : MonoBehaviour
+    {
+        public static QualityAssesmentController Instance { get; private set; }
 
-    public void Awake() {
-        if (Instance == null) {
-            Instance = this;
+
+        public void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
         }
-    }
 
-    // Start is called before the first frame update
-    public override void Start() { 
-        base.Start();
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            //xxxshishir load the stimuli list and set the target folder for the prerecorded pointcloud
+            
+        }
 
-    public override void MessageActivation(string message) { }
+    }
 }
