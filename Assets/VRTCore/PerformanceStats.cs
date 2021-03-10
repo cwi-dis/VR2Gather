ed41f24235a4ece8d6b5b0c1bcf70c109b41c8e9
Jack Jansen <Jack.Jansen@cwi.nl>
2021-03-10 14:06:30 +0100

    Implemented printing cpu/memory statistics in stats: form. Unfortunately it doesn't work: values are always 0. Google suggests this is a Unity issue, so shelving the code.

---
+++ Assets/VRTCore/PerformanceStats.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+namespace VRT.Core
+{
+    public class PerformanceStats : MonoBehaviour
+    {
+        System.Threading.Thread thread;
+        public bool isRunning { get; private set; }
+
+        [Tooltip("Interval in ms for performance measurements")]
+        public int interval = 1000;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            stats = new Stats("PerformanceStats");
+            thread = new System.Threading.Thread(new System.Threading.ThreadStart(_Update));
+            thread.Name = "PerformanceStats";
+            isRunning = true;
+            thread.Start();
+        }
+        public virtual void Stop()
+        {
+            isRunning = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            stats.statsUpdate();
+        }
+
+        void _Update()
+        {
+            while(isRunning)
+            {
+                stats.statsUpdate();
+                System.Threading.Thread.Sleep(interval);
+            }
+        }
+
+        protected class Stats : BaseStats
+        {
+            public Stats(string name) : base(name)
+            {
+                totalCpuCounter = new PerformanceCounter("Process", "% Processor Time", "_Total");
+                cpuCounter = new PerformanceCounter("Process", "% Processor Time", Process.GetCurrentProcess().ProcessName);
+                ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            }
+
+            PerformanceCounter totalCpuCounter;
+            PerformanceCounter cpuCounter;
+            PerformanceCounter ramCounter;
+
+            public void statsUpdate()
+            {
+
+                if (ShouldOutput())
+                {
+                    float totalCpu = totalCpuCounter.NextValue();
+                    float cpu = cpuCounter.NextValue();
+                    float memory = ramCounter.NextValue();
+                    double received = 0;
+                    double sent = 0;
+                    Output($"total_cpu={totalCpu}, cpu={cpu}, memory={memory}, received={received}, sent={sent}");
+                }
+                if (ShouldClear())
+                {
+                    Clear();
+                }
+            }
+        }
+
+        protected Stats stats;
+    }
+}
