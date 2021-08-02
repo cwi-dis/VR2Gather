fac96fbc81c2458f3aa62cba0587f9fddd70b3c0
Jack Jansen <Jack.Jansen@cwi.nl>
2021-08-02 22:26:21 +0200

    Put code specific to quality assessment experiment inside #if WITH_QUALITY_ASSESSMENT, for now. Should move to a subclass eventually.

--- Assets/UserRepresentation/PointClouds/Scripts/PrerecordedTileSelector.cs
+++ Assets/UserRepresentation/PointClouds/Scripts/PrerecordedTileSelector.cs
@@ -11,7 +11,9 @@ using VRT.Orchestrator.Wrapping;
 using System;
 using System.Linq;
 using VRT.Pilots.Common;
+#if WITH_QUALITY_ASSESSMENT
 using QualityAssesment;
+#endif
 
 namespace VRT.UserRepresentation.PointCloud
 {
@@ -125,6 +127,7 @@ namespace VRT.UserRepresentation.PointCloud
             prerecordedTileAdaptationSets = new List<AdaptationSet>[nTiles];
             prerecordedTileGeometrySets = new List<TileGeometry>[nTiles];
             Config._User realUser = Config.Instance.LocalUser;
+#if WITH_QUALITY_ASSESSMENT
             currentStimuli = StimuliController.getCurrentStimulus();
             bitRatebudget = StimuliController.getBitrateBudget();
             int codec = StimuliController.getCodec();
@@ -153,6 +156,7 @@ namespace VRT.UserRepresentation.PointCloud
                     algorithm = SelectionAlgorithm.alwaysBest;
                     break;
             }
+
             //xxxshishir load the tile description csv files
             string rootFolder = Config.Instance.LocalUser.PCSelfConfig.PrerecordedReaderConfig.folder;
             string[] tileFolder = Config.Instance.LocalUser.PCSelfConfig.PrerecordedReaderConfig.tiles;
@@ -224,6 +228,7 @@ namespace VRT.UserRepresentation.PointCloud
                     gFrame = new TileGeometry();
                 }
             }
+#endif
 
         }
 
@@ -341,93 +346,5 @@ namespace VRT.UserRepresentation.PointCloud
             Array.Reverse(tileOrder);
             return tileOrder;
         }
-    }
-    public class LiveTileSelector : BaseTileSelector
-    {
-
-        //
-        // Temporary variable (should be measured from internet connection): total available bitrate for this run.
-        //
-        public double bitRatebudget = 1000000;
-       
-        TilingConfig tilingConfig;
-
-        string Name()
-        {
-            return "LiveTileSelector";
-        }
-
-
-        public void Init(PointCloudPipeline _pipeline, int _nQualities, int _nTiles, TilingConfig? _tilingConfig)
-        {
-            if (_tilingConfig == null)
-            {
-                throw new System.Exception($"{Name()}: Must have tilingConfig argument");
-            }
-            tilingConfig = (TilingConfig)_tilingConfig;
-            pipeline = _pipeline;
-            nQualities = _nQualities;
-            Debug.Log($"{Name()}: PrerecordedTileSelector nQualities={nQualities}, nTiles={nTiles}");
-            nTiles = _nTiles;
-            TileOrientation = new Vector3[_nTiles];
-            for (int i = 0; i < _nTiles; i++)
-            {
-                TileOrientation[i] = tilingConfig.tiles[i].orientation;
-            }
-        }
-
-        protected override double[][] getBandwidthUsageMatrix(long currentFrameNumber)
-        {
-            if (currentFrameNumber != 0)
-            {
-                Debug.LogError($"{Name()}: Programmer error: currentFrameNumber={currentFrameNumber}");
-            }
-            int nTiles = tilingConfig.tiles.Length;
-            if (nTiles == 0) return null; // Not yet initialized
-            int nQualities = tilingConfig.tiles[0].qualities.Length;
-            double[][] bandwidthUsageMatrix = new double[nTiles][];
-            for (int ti = 0; ti < nTiles; ti++)
-            {
-                var thisTile = tilingConfig.tiles[ti];
-                var thisBandwidth = new double[nQualities];
-                for (int qi = 0; qi < nQualities; qi++)
-                {
-                    var thisQuality = thisTile.qualities[qi];
-                    thisBandwidth[qi] = thisQuality.bandwidthRequirement;
-                }
-
-                bandwidthUsageMatrix[ti] = thisBandwidth;
-            }
-            return bandwidthUsageMatrix;
-        }
-        protected override double getBitrateBudget()
-        {
-            return bitRatebudget;
-        }
-        protected override long getCurrentFrameIndex()
-        {
-            return 0;
-        }
-
-
-        protected override Vector3 getCameraForward()
-        {
-            // xxxjack currently returns camera viedw angle (as the name implies)
-            // but maybe camera position is better. Or both.
-            var cam = FindObjectOfType<Camera>().gameObject;
-            if (cam == null)
-                Debug.LogError("Camera not found!");
-            //Debug.Log("<color=red> Camera Transform </color>" + cameraForward.x + " " + cameraForward.y + " " + cameraForward.z);
-            Transform cameraTransform = cameraTransform = cam.transform;
-            return cameraTransform.forward;
-
-        }
-
-        protected override Vector3 getPointcloudPosition(long currentFrameNumber)
-        {
-            return new Vector3(0, 0, 0);
-        }
-
-
     }
 }
