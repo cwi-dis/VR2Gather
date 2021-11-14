b21994427d4a4e1e3e1516c73d2ae58d230fc448
Jack Jansen <Jack.Jansen@cwi.nl>
2021-11-14 23:45:47 +0100

    Test script to debug gamepads. It will print all KeyPress names of keys pressed, and for a single axis (set in the inspector) it will print the value of the axis whenever it changes.

---
+++ Assets/DevelopmentTests/GamePadDiscover/GamePadDiscover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class GamePadDiscover : MonoBehaviour
+{
+    public string axisName = "";
+    public float axisValue = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var names = Input.GetJoystickNames();
+        foreach(var name in names)
+        {
+            Debug.Log($"Joystick name: {name}");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axisName != "")
+        {
+            float newValue = Input.GetAxis(axisName);
+            if (newValue != axisValue)
+            {
+                Debug.Log($"Axis {axisName} = {newValue}");
+                axisValue = newValue;
+            }
+        }
+        foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(kcode))
+                Debug.Log("KeyCode down: " + kcode);
+        }
+    }
+}
