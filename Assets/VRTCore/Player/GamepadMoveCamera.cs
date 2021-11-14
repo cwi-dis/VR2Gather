b6ebcf106df47f271bcedb72067a42df13e9e16d
Jack Jansen <Jack.Jansen@cwi.nl>
2021-11-14 02:08:32 +0100

    Different approach. Renamed axes to follow unity numbers, all non-inverted. Adapt in prefab and code.

--- Assets/VRTCore/Player/GamepadMoveCamera.cs
+++ Assets/VRTCore/Player/GamepadMoveCamera.cs
@@ -5,22 +5,22 @@ using VRT.Core;
 
 public class GamepadMoveCamera : MoveCamera
 {
-    public string xAxisName = "Mouse X";
-    public string yAxisName = "Mouse Y";
+    public string xAxisName = "Gamepad_Axis_1";
+    public string yAxisName = "Gamepad_Axis_2";
     public string heightAxisName = "Mouse ScrollWheel";
     
     // Update is called once per frame
     void Update()
     {
         // If axis names are specified we are (probably) using a gamepad
-        float mouseX = Input.GetAxis(xAxisName) * xySensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis(yAxisName) * xySensitivity * Time.deltaTime;
-        xRotation -= mouseY;
+        float x = Input.GetAxis(xAxisName) * xySensitivity * Time.deltaTime;
+        float y = -1 * Input.GetAxis(yAxisName) * xySensitivity * Time.deltaTime;
+        xRotation += y;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         cameraToControl.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
-        adjustBodyHead(mouseX, -mouseY);
+        adjustBodyHead(x, y);
         
     }
 }
