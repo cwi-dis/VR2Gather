485e0ac8b333c0115db674c5b242bb15035a52b0
Jack Jansen <Jack.Jansen@cwi.nl>
2021-11-15 00:25:05 +0100

    Fixed gamepad scripts and prefab to work reasonably well.

--- Assets/VRTCore/Player/GamepadMoveCamera.cs
+++ Assets/VRTCore/Player/GamepadMoveCamera.cs
@@ -5,17 +5,28 @@ using VRT.Core;
 
 public class GamepadMoveCamera : MoveCamera
 {
-    public string xAxisName = "Gamepad_Axis_1";
-    public string yAxisName = "Gamepad_Axis_2";
-    public string heightAxisName = "Mouse ScrollWheel";
+    public string leftRightAxisName = "Gamepad_Axis_1";
+    public string upDownAxisName = "Gamepad_Axis_2";
+    public string heightAxisName = "Gamepad_Axis_6";
     public new const bool allowHJKLforMouse = false;
 
     // Update is called once per frame
     void Update()
     {
-        // If axis names are specified we are (probably) using a gamepad
-        float x = Input.GetAxis(xAxisName) * xySensitivity * Time.deltaTime;
-        float y = -1 * Input.GetAxis(yAxisName) * xySensitivity * Time.deltaTime;
+        if (heightAxisName != "")
+        {
+            float deltaHeight = Input.GetAxis(heightAxisName);
+            if (deltaHeight != 0)
+            {
+                // Do Camera movement for up/down.
+                cameraToControl.transform.localPosition = new Vector3(
+                    cameraToControl.transform.localPosition.x,
+                    cameraToControl.transform.localPosition.y - deltaHeight * heightSensitivity,
+                    cameraToControl.transform.localPosition.z);
+            }
+        }
+        float x = Input.GetAxis(leftRightAxisName) * xySensitivity * Time.deltaTime;
+        float y = -1 * Input.GetAxis(upDownAxisName) * xySensitivity * Time.deltaTime;
         xRotation += y;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
