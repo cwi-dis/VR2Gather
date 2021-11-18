21da2028a10dd5442ea16c3a57d5dfd4a752f4c8
Jack Jansen <Jack.Jansen@cwi.nl>
2021-11-18 22:18:01 +0100

    Moved into VRT.Pilots.Common namespace

--- Assets/Pilots/Common/Interaction/HandControllerEmulation.cs
+++ Assets/Pilots/Common/Interaction/HandControllerEmulation.cs
@@ -3,96 +3,100 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRT.Core;
 
-//
-// This script emulates HandController for use when you have no HMD or controllers,
-// only keyboard and mouse.
-// When you press shift you will see an indication whether the object under the mouse is
-// touchable. If so you can press left-click and touch it.
-//
-// Grabbing not implemented, because it doesn't seem to useful (without hands). But doable
-// if we want to.
-//
-public class HandControllerEmulation : MonoBehaviour
+namespace VRT.Pilots.Common
 {
-    [Tooltip("Mouse cursor to use while looking for touchable items")]
-    public Texture2D gropingCursorTexture;
-    [Tooltip("Mouse cursor to use when over a touchable item")]
-    public Texture2D touchingCursorTexture;
-    [Tooltip("Maximum distance of touchable objects")]
-    public float maxDistance = Mathf.Infinity;
-    [Tooltip("Key to press to start looking for touchable items")]
-    public KeyCode gropeKey = KeyCode.LeftShift;
-    [Tooltip("Key to press to touch an item")]
-    public KeyCode touchKey = KeyCode.Mouse0;
-    [Tooltip("Auto-center mouse, to allow use with gamepads. Not implemented.")]
-    public bool autoCenterMouse = false;
-    [Tooltip("Collider that actually presses the button")]
-    public Collider touchCollider = new SphereCollider();
-    protected bool isGroping;
-    protected bool isTouching;
-
-    // Update is called once per frame
-    void Update()
+    //
+    // This script emulates HandController for use when you have no HMD or controllers,
+    // only keyboard and mouse.
+    // When you press shift you will see an indication whether the object under the mouse is
+    // touchable. If so you can press left-click and touch it.
+    //
+    // Grabbing not implemented, because it doesn't seem to useful (without hands). But doable
+    // if we want to.
+    //
+    public class HandControllerEmulation : MonoBehaviour
     {
-        bool isGropingingNow = Input.GetKey(gropeKey);
-        if (isGroping != isGropingingNow)
-        {
-            isGroping = isGropingingNow;
-            if (isGroping)
-            {
-                 Cursor.SetCursor(gropingCursorTexture, Vector2.zero, CursorMode.Auto);
-                touchCollider.enabled = false;
-                isTouching = false;
-            } else
-            {
-                Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
-                touchCollider.enabled = false;
-            }
-        }
-        if (!isGroping) return;
+        [Tooltip("Mouse cursor to use while looking for touchable items")]
+        public Texture2D gropingCursorTexture;
+        [Tooltip("Mouse cursor to use when over a touchable item")]
+        public Texture2D touchingCursorTexture;
+        [Tooltip("Maximum distance of touchable objects")]
+        public float maxDistance = Mathf.Infinity;
+        [Tooltip("Key to press to start looking for touchable items")]
+        public KeyCode gropeKey = KeyCode.LeftShift;
+        [Tooltip("Key to press to touch an item")]
+        public KeyCode touchKey = KeyCode.Mouse0;
+        [Tooltip("Auto-center mouse, to allow use with gamepads. Not implemented.")]
+        public bool autoCenterMouse = false;
+        [Tooltip("Collider that actually presses the button")]
+        public Collider touchCollider = new SphereCollider();
+        protected bool isGroping;
+        protected bool isTouching;
 
-        //
-        // Check whether we are hitting any elegible object
-        //
-        bool isHittingNow = false;
-        int layerMask = LayerMask.GetMask("TouchableObject");
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit = new RaycastHit();
-        if (Physics.Raycast(ray, out hit, maxDistance, layerMask))
+        // Update is called once per frame
+        void Update()
         {
-            //Debug.Log($"xxxjack mouse-hit {hit.collider.gameObject.name}");
-            isHittingNow = true;
-        }
-        if (isTouching != isHittingNow)
-        {
-            isTouching = isHittingNow;
-            if (isTouching)
+            bool isGropingingNow = Input.GetKey(gropeKey);
+            if (isGroping != isGropingingNow)
             {
-                Cursor.SetCursor(touchingCursorTexture, Vector2.zero, CursorMode.Auto);
+                isGroping = isGropingingNow;
+                if (isGroping)
+                {
+                    Cursor.SetCursor(gropingCursorTexture, Vector2.zero, CursorMode.Auto);
+                    touchCollider.enabled = false;
+                    isTouching = false;
+                }
+                else
+                {
+                    Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+                    touchCollider.enabled = false;
+                }
             }
-            else
+            if (!isGroping) return;
+
+            //
+            // Check whether we are hitting any elegible object
+            //
+            bool isHittingNow = false;
+            int layerMask = LayerMask.GetMask("TouchableObject");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit, maxDistance, layerMask))
             {
-                Cursor.SetCursor(gropingCursorTexture, Vector2.zero, CursorMode.Auto);
+                //Debug.Log($"xxxjack mouse-hit {hit.collider.gameObject.name}");
+                isHittingNow = true;
             }
-        }
-        if (!isTouching)
-        {
-            touchCollider.enabled = false;
-            return;
-        }
-        //
-        // Now check whether the left mouse is clicked and perform the action.
-        //
-        if (Input.GetKey(touchKey))
-        {
-            GameObject objHit = hit.collider.gameObject;
-            Debug.Log($"xxxjack Moving touchCollider to {objHit.name} at {objHit.transform.position}");
-            touchCollider.enabled = true;
-            touchCollider.transform.position = hit.collider.transform.position;
-        }
-        if (Input.GetKeyUp(touchKey))
-        {
-            touchCollider.enabled = false;
+            if (isTouching != isHittingNow)
+            {
+                isTouching = isHittingNow;
+                if (isTouching)
+                {
+                    Cursor.SetCursor(touchingCursorTexture, Vector2.zero, CursorMode.Auto);
+                }
+                else
+                {
+                    Cursor.SetCursor(gropingCursorTexture, Vector2.zero, CursorMode.Auto);
+                }
+            }
+            if (!isTouching)
+            {
+                touchCollider.enabled = false;
+                return;
+            }
+            //
+            // Now check whether the left mouse is clicked and perform the action.
+            //
+            if (Input.GetKey(touchKey))
+            {
+                GameObject objHit = hit.collider.gameObject;
+                Debug.Log($"xxxjack Moving touchCollider to {objHit.name} at {objHit.transform.position}");
+                touchCollider.enabled = true;
+                touchCollider.transform.position = hit.collider.transform.position;
+            }
+            if (Input.GetKeyUp(touchKey))
+            {
+                touchCollider.enabled = false;
+            }
         }
     }
 }
