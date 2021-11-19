6945fdb72a571ae96b7bf3789f15d16a5e1b1c3e
Jack Jansen <Jack.Jansen@cwi.nl>
2021-11-19 17:33:08 +0100

    Use the hand animator to do pointing.

--- Assets/Pilots/Common/Interaction/HandControllerGamepad.cs
+++ Assets/Pilots/Common/Interaction/HandControllerGamepad.cs
@@ -20,34 +20,49 @@ namespace VRT.Pilots.Common
         new const Texture2D touchingCursorTexture = null;
         public GameObject hand;
         protected override bool alwaysShowGrope { get { return true; } }
+        private Animator _Animator = null;
 
+        void Start()
+        {
+            _Animator = GetComponentInChildren<Animator>();
+        }
 
         protected override void showGropeNotTouching(Ray ray, float distance)
         {
-            Debug.Log($"showGropeNotTouching {ray.origin} to {ray.direction}");
-            Debug.DrawRay(ray.origin, ray.direction, Color.cyan, 1f);
-            var point = ray.GetPoint(distance);
+            //Debug.Log($"showGropeNotTouching {ray.origin} to {ray.direction}");
+            //Debug.DrawRay(ray.origin, ray.direction, Color.cyan, 1f);
+            var point = ray.GetPoint(distance-0.1f);
             hand.transform.position = point;
             hand.SetActive(true);
+            UpdateAnimation("");
         }
 
         protected override void showGropeTouching(Ray ray, float distance)
         {
-            Debug.Log($"showGropeTouching {ray.origin} to {ray.direction}");
-            Debug.DrawRay(ray.origin, ray.direction, Color.magenta, 1f);
-            var point = ray.GetPoint(distance);
+            //Debug.Log($"showGropeTouching {ray.origin} to {ray.direction}");
+            //Debug.DrawRay(ray.origin, ray.direction, Color.magenta, 1f);
+            var point = ray.GetPoint(distance-0.1f);
             hand.transform.position = point;
             hand.SetActive(true);
+            UpdateAnimation("IsPointing");
         }
 
         protected override void showGropeNone()
         {
             hand.SetActive(false);
+            UpdateAnimation("");
         }
 
         protected override Vector3 getRayDestination()
         {
             return new Vector3(Camera.main.pixelWidth/2, Camera.main.pixelHeight/2, 0);
+        }
+
+        private void UpdateAnimation(string state)
+        {
+            if (_Animator == null) return;
+            _Animator.SetBool("IsGrabbing", state == "IsGrabbing");
+            _Animator.SetBool("IsPointing", state == "IsPointing");
         }
     }
 }
