09b8313ac1756b951e7f1713a65d91d6f9c7cf73
Jack Jansen <Jack.Jansen@cwi.nl>
2021-11-19 16:20:00 +0100

    Moved into VRT.Pilots.Common namespace.

--- Assets/Pilots/Common/Interaction/MovePlayer.cs
+++ Assets/Pilots/Common/Interaction/MovePlayer.cs
@@ -2,20 +2,25 @@
 using UnityEngine.XR;
 using VRT.Core;
 
-public class MovePlayer : MonoBehaviour {
-    public string leftRightAxis = "Horizontal";
-    public string forwardAxis = "Vertical";
-    public bool invertForwardAxis = false;
-    public float speed = 5f;
-    public CharacterController controller;
+namespace VRT.Pilots.Common
+{
+    public class MovePlayer : MonoBehaviour
+    {
+        public string leftRightAxis = "Horizontal";
+        public string forwardAxis = "Vertical";
+        public bool invertForwardAxis = false;
+        public float speed = 5f;
+        public CharacterController controller;
 
-    // Update is called once per frame
-    void Update() {
-        float x = Input.GetAxis(leftRightAxis);
-        float z = Input.GetAxis(forwardAxis);
-        if (invertForwardAxis) z = -z;
+        // Update is called once per frame
+        void Update()
+        {
+            float x = Input.GetAxis(leftRightAxis);
+            float z = Input.GetAxis(forwardAxis);
+            if (invertForwardAxis) z = -z;
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
+            Vector3 move = transform.right * x + transform.forward * z;
+            controller.Move(move * speed * Time.deltaTime);
+        }
     }
 }
