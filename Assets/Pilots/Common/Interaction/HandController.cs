e7869dd98f78da2f3b841858584a2a75d87def1a
Jack Jansen <Jack.Jansen@cwi.nl>
2021-11-22 01:02:03 +0100

    ControllerInput functionality has been moved to the various Controllers (parameterized through attributes on the object, so the correct buttons are used for each hand).

--- Assets/Pilots/Common/Interaction/HandController.cs
+++ Assets/Pilots/Common/Interaction/HandController.cs
@@ -26,7 +26,19 @@ namespace VRT.Pilots.Common
 			Right
 		}
 
+		[Tooltip("When this key is pressed we are in teleporting mode")]
 		public KeyCode teleportModeKey = KeyCode.None;
+
+		[Tooltip("When this axis is active (or inactive depending on invert) we are in pointing mode")]
+		public string pointingModeAxis = "";
+		[Tooltip("Invert meaning of PointingModeAxis")]
+		public bool pointingModeAxisInvert = false;
+
+		[Tooltip("When this axis is active (or inactive depending on invert) we are in grabbing mode")]
+		public string grabbingModeAxis = "";
+		[Tooltip("Invert meaning of grabbingModeAxis")]
+		public bool grabbingModeAxisInvert = false;
+		
 		public XRNode XRNode;
 		public State HandState;
 		public Handedness HandHandedness;
@@ -42,7 +54,9 @@ namespace VRT.Pilots.Common
 		private Animator _Animator;
 		private NetworkPlayer _Player;
 
+#if xxxjack_removed
 		private List<XRNodeState> _NodeStates = new List<XRNodeState>();
+#endif
 
 		private bool inTeleportMode = false;
 		private PlayerLocation _SelectedLocation;
@@ -72,7 +86,9 @@ namespace VRT.Pilots.Common
 		{
 			if (_Player.IsLocalPlayer)
 			{
+#if xxxjack_removed
 				InputTracking.GetNodeStates(_NodeStates);
+#endif
 
 				//Prevent floor clipping when input tracking provides glitched results
 				//This could on occasion cause released grabbables to go throught he floor
@@ -81,9 +97,13 @@ namespace VRT.Pilots.Common
 					transform.position = new Vector3(transform.position.x, 0.05f, transform.position.z);
 				}
 
-
-				bool index_trigger_pressed = ControllerInput.Instance.PrimaryTrigger(XRNode);
-				bool hand_trigger_pressed = ControllerInput.Instance.SecondaryTrigger(XRNode);
+				//
+				// See whether we are pointing, grabbing, teleporting or idle
+				//
+				bool pointingModeAxisIsPressed = Input.GetAxis(pointingModeAxis) >= 0.01f;
+				if (pointingModeAxisInvert) pointingModeAxisIsPressed = !pointingModeAxisIsPressed;
+				bool grabbingModeAxisIsPressed = Input.GetAxis(grabbingModeAxis) >= 0.01f;
+				if (grabbingModeAxisInvert) grabbingModeAxisIsPressed = !grabbingModeAxisIsPressed;
 				bool teleportModeKeyIsPressed = teleportModeKey != KeyCode.None && Input.GetKey(teleportModeKey);
 
 				if (teleportModeKeyIsPressed)
@@ -132,6 +152,9 @@ namespace VRT.Pilots.Common
 				}
 				else if (inTeleportMode)
 				{
+					//
+					// Teleport key was released. See if we should teleport.
+					//
 					if (_SelectedLocation != null)
 					{
 						SessionPlayersManager.Instance.RequestLocationChange(_SelectedLocation.NetworkId);
@@ -141,16 +164,14 @@ namespace VRT.Pilots.Common
 					TeleportLineRenderer.enabled = false;
 					inTeleportMode = false;
 				}
-
-
-
-				if (index_trigger_pressed && hand_trigger_pressed)
+				
+				if (grabbingModeAxisIsPressed)
 				{
 					SetHandState(State.Grabbing);
 					GrabCollider.SetActive(true);
 					TouchCollider.SetActive(false);
 				}
-				else if (hand_trigger_pressed)
+				else if (pointingModeAxisIsPressed)
 				{
 					SetHandState(State.Pointing);
 					GrabCollider.SetActive(false);
@@ -167,6 +188,10 @@ namespace VRT.Pilots.Common
 
 		void OnHandControllerData(HandControllerData data)
 		{
+			//
+			// For incoming hand data, see if this is for a remote player hand and we
+			// are that player and that hand. If so: Update our visual representation/animation.
+			//
 			if (!_Player.IsLocalPlayer && _Player.UserId == data.SenderId)
 			{
 				if (OrchestratorController.Instance.UserIsMaster)
@@ -187,7 +212,10 @@ namespace VRT.Pilots.Common
 			{
 				HandState = handState;
 				UpdateAnimation();
-
+				//
+				// If we are a hand of the local player we forward the state change,
+				// so other players can see it too.
+				//
 				if (_Player.IsLocalPlayer)
 				{
 					var data = new HandControllerData
