cc3679c121429bb31e3b02a11ca64a9baa95015b
ARUGULA\VRTogether <cwidisgroup@gmail.com>
2021-12-08 14:34:43 +0100

    Modified to accomodate three key presses and annotation messages

--- Assets/VRTCore/ManualStatsAnnotator.cs
+++ Assets/VRTCore/ManualStatsAnnotator.cs
@@ -6,7 +6,13 @@ namespace VRT.Core
 {
     public class ManualStatsAnnotator : MonoBehaviour
     {
-        private string msg = "Something interesting happened here";
+        public string msg1 = "Something interesting happened here";
+        public string msg2 = "Something interesting happened here";
+        public string msg3 = "Something interesting happened here";
+
+        public KeyCode key1;
+        public KeyCode key2;
+        public KeyCode key3;
         // Start is called before the first frame update
         void Start()
         {
@@ -16,10 +22,12 @@ namespace VRT.Core
         // Update is called once per frame
         void Update()
         {
-            if(Input.GetKeyDown(KeyCode.Space))
-            {
-                BaseStats.Output("ManualAnnotator", msg);
-            }
+            if(Input.GetKeyDown(key1))
+                BaseStats.Output("ManualAnnotator", msg1);
+            if(Input.GetKeyDown(key2))
+                BaseStats.Output("ManualAnnotator", msg2);
+            if(Input.GetKeyDown(key3))
+                BaseStats.Output("ManualAnnotator", msg3);
         }
     }
 }
