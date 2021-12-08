17ac0b69f10ba2cd86e1a10b41dfe062e1cb9bdd
ARUGULA\VRTogether <cwidisgroup@gmail.com>
2021-12-08 13:39:38 +0100

    Implemented manual annotator

---
+++ Assets/VRTCore/ManualStatsAnnotator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRT.Core
+{
+    public class ManualStatsAnnotator : MonoBehaviour
+    {
+        private string msg = "Something interesting happened here";
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                BaseStats.Output("ManualAnnotator", msg);
+            }
+        }
+    }
+}
