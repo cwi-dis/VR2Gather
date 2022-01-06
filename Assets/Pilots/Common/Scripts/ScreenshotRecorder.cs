50b130f3ba53a9c67e87898275edc9c4be22919b
BALTHAZAR\Shishir <s.subramanyam@cwi.nl>
2022-01-06 18:48:05 +0100

    First implementation of screenshot tool

--- Assets/Pilots/Common/Scripts/ScreenshotRecorder.cs
+++ Assets/Pilots/Common/Scripts/ScreenshotRecorder.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRT.Core;
+using System.IO;
 
-public class ScreenshotRecorder : MonoBehaviour
+namespace VRT.Pilots.Common
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ScreenshotRecorder : MonoBehaviour
     {
-        
-    }
+        private bool takeScreenshot = false;
+        private string screenshotTargetDirectory = "";
+        private int width;
+        private int height;
+        // Start is called before the first frame update
+        // xxxshishir ToDo: modify to disable if takeScreenshot is nul;
+        void Start()
+        {
+            takeScreenshot = Config.Instance.ScreenshotTool.takeScreenshot;
+            screenshotTargetDirectory = Config.Instance.ScreenshotTool.screenshotTargetDirectory;
+            width = Screen.width;
+            height = Screen.height;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        IEnumerator captureScreenshot()
+        {
+            yield return new WaitForEndOfFrame();
+
+            Texture2D screenshot = new Texture2D(width, height, TextureFormat.RGB24, false); //xxxshishir the false is to disable mipmap texture (default behaviour)
+            screenshot.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+            screenshot.Apply();
+            byte[] screenshotBytes = screenshot.EncodeToPNG();
+            Destroy(screenshot);
+
+            if(!Directory.Exists(screenshotTargetDirectory))
+            {
+                Directory.CreateDirectory(screenshotTargetDirectory);
+            }
+
+            File.WriteAllBytes(screenshotTargetDirectory + "/Frame" + Time.frameCount + ".png",screenshotBytes);
+
+            yield return null;
+        }
+
     }
 }
