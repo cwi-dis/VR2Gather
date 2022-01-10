6c9eb24c4fd5782f2eb12e90bc98f238ad2c62f0
Jack Jansen <Jack.Jansen@cwi.nl>
2022-01-10 16:29:03 +0100

    PCencoder queuesize stat is average over all streams. Make that clear.

--- Assets/UserRepresentation/PointClouds/Scripts/Workers/Codecs/PCEncoder.cs
+++ Assets/UserRepresentation/PointClouds/Scripts/Workers/Codecs/PCEncoder.cs
@@ -192,7 +192,7 @@ namespace VRT.UserRepresentation.PointCloud
                 if (dropped) statsTotalDropped++;
 
                 if (ShouldOutput()) {
-                    Output($"fps={statsTotalPointclouds / Interval():F2}, fps_dropped={statsTotalDropped / Interval():F2}, encoder_ms={(int)(statsTotalEncodeDuration / statsTotalPointclouds)}, transmitter_queue_ms={(int)(statsTotalQueuedDuration / statsTotalPointclouds)}");
+                    Output($"fps={statsTotalPointclouds / Interval():F2}, fps_dropped={statsTotalDropped / Interval():F2}, encoder_ms={(int)(statsTotalEncodeDuration / statsTotalPointclouds)}, avg_transmitter_queue_ms={(int)(statsTotalQueuedDuration / statsTotalPointclouds)}");
                 }
                 if (ShouldClear()) {
                     Clear();
