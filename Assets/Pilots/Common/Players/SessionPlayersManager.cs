9e1506e0f9b302c8efc9e10e704c53354e724825
Jack Jansen <Jack.Jansen@cwi.nl>
2022-07-19 16:33:36 +0200

    Brought up-to-date with current state of develop.

--- Assets/Pilots/Common/Players/SessionPlayersManager.cs
+++ Assets/Pilots/Common/Players/SessionPlayersManager.cs
@@ -166,7 +166,7 @@ namespace VRT.Pilots.Common
 #if UNITY_EDITOR
 						if (representationType == UserRepresentationType.__CAMERAMAN__ && me.userId == user.userId) {
 							Debug.Log($"-----------------------> {player.name} representationType {representationType}");
-							playerManager.getCameraTransform().GetComponent<VRTCore.UnityRecorderController>().enabled = true;
+							playerManager.getCameraTransform().GetComponent<UnityRecorderController>().enabled = true;
 						}
 #endif
 						Voyeurs.Add(networkPlayer.UserId, networkPlayer);
