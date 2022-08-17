64cd09b84cb49476553327bf948e44cf0fcdf0d6
Jack Jansen <Jack.Jansen@cwi.nl>
2022-08-17 23:57:51 +0200

    Getting started on script for testing new input system. Currently only shows key presses and mouse movement.

---
+++ Assets/DevelopmentTests/TestNewInputSystem/TestNewInputSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class TestNewInputSystem : MonoBehaviour
+{
+    Vector2 oldMousePosition;
+
+     void Start()
+    {
+ 
+    }
+
+    public void OnMove()
+    {
+        
+    }
+
+    public void OnTeleport()
+    {
+
+    }
+
+    public void OnFoobar()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        if (mousePosition != oldMousePosition)
+        {
+            Debug.Log($"Mouse was moved from {oldMousePosition} to {mousePosition}");
+            oldMousePosition = mousePosition;
+        }
+        if (Keyboard.current.anyKey.wasPressedThisFrame)
+        {
+            Debug.Log("A key was pressed");
+        }
+    }
+}
