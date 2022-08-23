197c3566b977b43b6666386b8614df4a0d2c9962
Jack Jansen <Jack.Jansen@cwi.nl>
2022-08-24 00:06:44 +0200

    Implemented calibration. Camera height is still incorrect for non-hmd.

--- Assets/UserRepresentation/SelfCalibration/Scripts/Calibration.cs
+++ Assets/UserRepresentation/SelfCalibration/Scripts/Calibration.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 using VRT.Core;
 
 public class Calibration : MonoBehaviour {
     private bool initialized = false;
-    private enum State { Comfort, Mode, Translation, Rotation }
-    private State       state = State.Comfort;
+    private enum State { CheckWithUser, SelectTranslationRotation, Translation, Rotation }
+    private State       state = State.CheckWithUser;
 
+    [Tooltip("The camera to control, for preview")]
     public GameObject   cameraReference;
+    [Tooltip("How fast to rotate")]
     public float        _rotationSlightStep = 1f;
+    [Tooltip("How fast to translate")]
     public float        _translationSlightStep = 0.01f;
-    public string        prefix = "pcs";
 
-    [Header("Input controller support")]
-    public GameObject emulation;
-    public GameObject gamepad;
-    public GameObject oculus;
-    public GameObject openvr;
-    private GameObject controls = null;
+    const string        prefix = "pcs";
+
     [Header("UI Panel references")]
     public GameObject   ComfortUI;
     public GameObject   CalibrationModeUI;
@@ -39,6 +39,7 @@ public class Calibration : MonoBehaviour {
 
     private void Start() {
         _Initialize();
+        ChangeModeUI();
     }
 
     private void _Initialize()
@@ -46,15 +47,6 @@ public class Calibration : MonoBehaviour {
         if (initialized) return;
         if (!VRConfig.Instance.isInitialized()) return;
         initialized = true;
-        // Enable the correct set of controls (and only the correct set)
-        if (VRConfig.Instance.useControllerEmulation()) controls = emulation;
-        if (VRConfig.Instance.useControllerGamepad()) controls = gamepad;
-        if (VRConfig.Instance.useControllerOculus()) controls = oculus;
-        if (VRConfig.Instance.useControllerOpenVR()) controls = openvr;
-        emulation.SetActive(emulation == controls);
-        gamepad.SetActive(gamepad == controls);
-        oculus.SetActive(oculus == controls);
-        openvr.SetActive(openvr == controls);
         // Get initial position/orientation from the preferences
         Vector3 pos = new Vector3(PlayerPrefs.GetFloat(prefix + "_pos_x", 0), PlayerPrefs.GetFloat(prefix + "_pos_y", 0), PlayerPrefs.GetFloat(prefix + "_pos_z", 0));
         Vector3 rot = new Vector3(PlayerPrefs.GetFloat(prefix + "_rot_x", 0), PlayerPrefs.GetFloat(prefix + "_rot_y", 0), PlayerPrefs.GetFloat(prefix + "_rot_z", 0));
@@ -63,130 +55,193 @@ public class Calibration : MonoBehaviour {
         cameraReference.transform.localRotation = Quaternion.Euler(rot);
     }
 
+    public void OnYes()
+    {
+        Debug.Log($"CalibrationControls: OnYes");
+        if (state == State.CheckWithUser)
+        {
+            Debug.Log("Calibration: CheckWithUser: User is happy, return to LoginManager");
+            SceneManager.LoadScene("LoginManager");
+        } else
+        if (state == State.SelectTranslationRotation)
+        {
+            Debug.Log("Calibration: Mode: User is done");
+            state = State.CheckWithUser;
+            ChangeModeUI();
+        }
+        else
+        if (state == State.Translation)
+        {
+            var pos = cameraReference.transform.localPosition;
+            PlayerPrefs.SetFloat(prefix + "_pos_x", pos.x);
+            PlayerPrefs.SetFloat(prefix + "_pos_y", pos.y);
+            PlayerPrefs.SetFloat(prefix + "_pos_z", pos.z);
+            Debug.Log($"Calibration: Translation: Saved: {pos.x}, {pos.y}, {pos.z}");
+            state = State.SelectTranslationRotation;
+            ChangeModeUI();
+        }
+        else
+        if (state == State.Rotation)
+        {
+            var rot = cameraReference.transform.localRotation.eulerAngles;
+            PlayerPrefs.SetFloat(prefix + "_rot_x", rot.x);
+            PlayerPrefs.SetFloat(prefix + "_rot_y", rot.y);
+            PlayerPrefs.SetFloat(prefix + "_rot_z", rot.z);
+
+            Debug.Log($"Calibration: Rotation: Saved: {rot.x}, {rot.y}, {rot.z}");
+            state = State.SelectTranslationRotation;
+            ChangeModeUI();
+        }
+    }
+
+    public void OnNo()
+    {
+        Debug.Log($"CalibrationControls: OnNo");
+        if (state == State.CheckWithUser)
+        {
+            Debug.Log("Calibration: Comfort: Starting calibration process");
+            state = State.SelectTranslationRotation;
+            ChangeModeUI();
+        }
+        else
+        if (state == State.SelectTranslationRotation)
+        {
+            Debug.Log("Calibration: Mode: User is done");
+            state = State.CheckWithUser;
+            ChangeModeUI();
+        }
+        else
+        if (state == State.Translation)
+        {
+            cameraReference.transform.localPosition = new Vector3(
+                        PlayerPrefs.GetFloat(prefix + "_pos_x", 0),
+                        PlayerPrefs.GetFloat(prefix + "_pos_y", 0),
+                        PlayerPrefs.GetFloat(prefix + "_pos_z", 0)
+                    );
+            var pos = cameraReference.transform.localPosition;
+            Debug.Log($"Calibration: Translation: Reloaded to: {pos.x}, {pos.y}, {pos.z}");
+            state = State.SelectTranslationRotation;
+            ChangeModeUI();
+        }
+        else
+        if (state == State.Rotation)
+        {
+            cameraReference.transform.localRotation = Quaternion.Euler(
+                 PlayerPrefs.GetFloat(prefix + "_rot_x", 0),
+                 PlayerPrefs.GetFloat(prefix + "_rot_y", 0),
+                 PlayerPrefs.GetFloat(prefix + "_rot_z", 0)
+             );
+            var rot = cameraReference.transform.localRotation;
+            Debug.Log($"Calibration: Rotation: Reloaded to: {rot.x}, {rot.y}, {rot.z}");
+            state = State.SelectTranslationRotation;
+            ChangeModeUI();
+        }
+    }
+
+    public void OnTranslate()
+    {
+        Debug.Log($"CalibrationControls: OnTranslate");
+        if (state == State.SelectTranslationRotation)
+        {
+            Debug.Log("Calibration: Mode: Selected Translation Mode");
+            state = State.Translation;
+            ChangeModeUI();
+        }
+    }
+
+    public void OnRotate()
+    {
+        Debug.Log($"CalibrationControls: OnRotate");
+        if (state == State.SelectTranslationRotation)
+        {
+            Debug.Log("Calibration: Mode: Selected Rotation Mode");
+            state = State.Rotation;
+            ChangeModeUI();
+        }
+
+    }
+
+    public void OnDone()
+    {
+        Debug.Log($"CalibrationControls: OnDone");
+
+        if (state == State.SelectTranslationRotation)
+        {
+            Debug.Log("Calibration: Mode: User is done");
+            state = State.CheckWithUser;
+            ChangeModeUI();
+        }
+    }
+
+    public void OnReset()
+    {
+        Debug.Log($"CalibrationControls: OnReset");
+        if (state == State.SelectTranslationRotation)
+        {
+            Debug.Log("Calibration: Mode: Reset factory settings");
+            ResetFactorySettings();
+            cameraReference.transform.localPosition = Vector3.zero;
+            cameraReference.transform.localRotation = Quaternion.Euler(Vector3.zero);
+
+        } else
+        if (state == State.Translation)
+        {
+            cameraReference.transform.localPosition = new Vector3(0, 0, 0);
+            Debug.Log($"Calibration: Translation: reset to 0, 0, 0");
+        } else
+        if (state == State.Rotation)
+        {
+            Debug.Log("Calibration: Rotation: Reset to 0,0,0");
+            cameraReference.transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+
+    public void OnBackwardForward(InputValue value)
+    {
+        var delta = value.Get<float>();
+        Debug.Log($"CalibrationControls: OnBackwardForward: {delta}");
+
+        if (state == State.Translation)
+        {
+            cameraReference.transform.localPosition += new Vector3(0, 0, delta) * _translationSlightStep;
+        }
+    }
+
+    public void OnLeftRight(InputValue value)
+    {
+        var delta = value.Get<float>();
+        Debug.Log($"CalibrationControls: OnLeftRight: {delta}");
+        if (state == State.Translation)
+        {
+            cameraReference.transform.localPosition += new Vector3(delta, 0, 0) * _translationSlightStep;
+
+        } else
+        if (state == State.Rotation)
+        {
+            cameraReference.transform.localRotation = Quaternion.Euler(cameraReference.transform.localRotation.eulerAngles + Vector3.up * -_rotationSlightStep * delta);
+        }
+    }
+
+    public void OnUpDown(InputValue value)
+    {
+        var delta = value.Get<float>();
+        Debug.Log($"CalibrationControls: OnUpDown: {delta}");
+        if (state == State.Translation)
+        {
+            cameraReference.transform.localPosition += new Vector3(0, delta, 0) * _translationSlightStep;
+        } 
+    }
+
     // Update is called once per frame
     void Update() {
         _Initialize();
-#if !ENABLE_INPUT_SYSTEM
-        //
-        // Enable current UI
-        //
-        ComfortUI.SetActive(state == State.Comfort);
-        CalibrationModeUI.SetActive(state == State.Mode);
+    }
+
+    void ChangeModeUI()
+    {
+        ComfortUI.SetActive(state == State.CheckWithUser);
+        CalibrationModeUI.SetActive(state == State.SelectTranslationRotation);
         TransalationUI.SetActive(state == State.Translation);
         RotationUI.SetActive(state == State.Rotation);
-
-        switch (state) {
-            case State.Comfort:
-                // I'm Comfortable
-                if (controls.yes.get())
-                {
-                    Debug.Log("Calibration: Comfort: User is happy, return to LoginManager");
-                    //Application.Quit();
-                    SceneManager.LoadScene("LoginManager");
-                }
-                // I'm not comfortable
-                if (controls.no.get())
-                {
-                    Debug.Log("Calibration: Comfort: Starting calibration process");
-                    state = State.Mode;
-                }
-                break;
-            case State.Mode:
-                //Activate Translation
-                if (controls.translate.get())
-                {
-                    Debug.Log("Calibration: Mode: Selected Translation Mode");
-                    state = State.Translation;
-                }
-                //Activate Rotation (UpAxis)
-                if (controls.rotate.get())
-                {
-                    Debug.Log("Calibration: Mode: Selected Rotation Mode");
-                    state = State.Rotation;
-                }
-                // Reset everything to factory settings
-                if (controls.reset.get())
-                {
-                    Debug.Log("Calibration: Mode: Reset factory settings");
-                    ResetFactorySettings();
-                    cameraReference.transform.localPosition = Vector3.zero;
-                    cameraReference.transform.localRotation = Quaternion.Euler(Vector3.zero);
-                }
-                if (controls.yes.get() || controls.no.get() || controls.done.get()) {
-                    Debug.Log("Calibration: Mode: User is done");
-                    state = State.Comfort;
-                }
-                break;
-            case State.Translation:
-                // Movement
-                float zAxis = controls.backwardForward.get();
-                float xAxis = controls.leftRight.get();
-                float yAxis = controls.downUp.get();
-                if (zAxis != 0) Debug.Log($"xxxjack translation z={zAxis}");
-                if (xAxis != 0) Debug.Log($"xxxjack translation x={xAxis}");
-                if (yAxis != 0) Debug.Log($"xxxjack translation y={yAxis}");
-                if (controls.reset.get())
-                {
-                    cameraReference.transform.localPosition = new Vector3(0, 0, 0);
-                    Debug.Log($"Calibration: Translation: reset to 0, 0, 0");
-                }
-                cameraReference.transform.localPosition += new Vector3(xAxis, yAxis, zAxis) * _translationSlightStep;
-                // Save Translation
-                if (controls.yes.get()) {
-                    var pos = cameraReference.transform.localPosition;
-                    PlayerPrefs.SetFloat(prefix + "_pos_x", pos.x);
-                    PlayerPrefs.SetFloat(prefix + "_pos_y", pos.y);
-                    PlayerPrefs.SetFloat(prefix + "_pos_z", pos.z);
-                    Debug.Log($"Calibration: Translation: Saved: {pos.x}, {pos.y}, {pos.z}");
-                    state = State.Mode;
-                }
-                // Back
-                if (controls.no.get()) {
-                    cameraReference.transform.localPosition = new Vector3 ( 
-                        PlayerPrefs.GetFloat(prefix+"_pos_x", 0), 
-                        PlayerPrefs.GetFloat(prefix+"_pos_y", 0), 
-                        PlayerPrefs.GetFloat(prefix+"_pos_z", 0)
-                    );
-                    var pos = cameraReference.transform.localPosition;
-                    Debug.Log($"Calibration: Translation: Reloaded to: {pos.x}, {pos.y}, {pos.z}");
-                    state = State.Mode;
-                }
-                break;
-            case State.Rotation:
-                // Rotation
-                float yAxisR = controls.leftRight.get();
-                if (yAxisR != 0) Debug.Log($"xxxjack rotation y={yAxisR}");
-                if (controls.reset.get())
-                {
-                    Debug.Log("Calibration: Rotation: Reset to 0,0,0");
-                    cameraReference.transform.localEulerAngles = new Vector3(0, 0, 0);
-                }
-                cameraReference.transform.localRotation = Quaternion.Euler(cameraReference.transform.localRotation.eulerAngles + Vector3.up * -_rotationSlightStep* yAxisR);
-                // Save Translation
-                if (controls.yes.get()) {
-                    var rot = cameraReference.transform.localRotation.eulerAngles;
-                    PlayerPrefs.SetFloat(prefix + "_rot_x", rot.x);
-                    PlayerPrefs.SetFloat(prefix + "_rot_y", rot.y);
-                    PlayerPrefs.SetFloat(prefix + "_rot_z", rot.z);
-
-                    Debug.Log($"Calibration: Rotation: Saved: {rot.x}, {rot.y}, {rot.z}");
-                    state = State.Mode;
-                }
-                // Back
-                if (controls.no.get()) {
-                    cameraReference.transform.localRotation = Quaternion.Euler( 
-                        PlayerPrefs.GetFloat(prefix + "_rot_x", 0),
-                        PlayerPrefs.GetFloat(prefix + "_rot_y", 0),
-                        PlayerPrefs.GetFloat(prefix + "_rot_z", 0)
-                    );
-                    var rot = cameraReference.transform.localRotation;
-                    Debug.Log($"Calibration: Rotation: Reloaded to: {rot.x}, {rot.y}, {rot.z}");
-                    state = State.Mode;
-                }
-                break;
-            default:
-                break;
-        }
-#endif
     }
 }
