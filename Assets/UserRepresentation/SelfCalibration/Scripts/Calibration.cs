a66e66a640615666095ce283481fa5c785013c12
Jack Jansen <Jack.Jansen@cwi.nl>
2022-09-26 15:53:50 +0200

    Calibration working without callbacks, by meticulously porting old code from InputManager and adapting to InputSystem.

--- Assets/UserRepresentation/SelfCalibration/Scripts/Calibration.cs
+++ Assets/UserRepresentation/SelfCalibration/Scripts/Calibration.cs
@@ -99,6 +99,131 @@ public class Calibration : MonoBehaviour {
         if (curMove != Vector2.zero) Debug.Log($"xxxjack MoveAction {curMove}");
         var curHeight = HeightAction.ReadValue<float>();
         if (curHeight != 0) Debug.Log($"xxxjack HeightAction {curHeight}");
+
+        ComfortUI.SetActive(state == State.CheckWithUser);
+        CalibrationModeUI.SetActive(state == State.SelectTranslationRotation);
+        TransalationUI.SetActive(state == State.Translation);
+        RotationUI.SetActive(state == State.Rotation);
+
+        switch (state)
+        {
+            case State.CheckWithUser:
+                // I'm Comfortable
+                if (YesAction.triggered)
+                {
+                    Debug.Log("Calibration: Comfort: User is happy, return to LoginManager");
+                    //Application.Quit();
+                    SceneManager.LoadScene("LoginManager");
+                }
+                // I'm not comfortable
+                if (NoAction.triggered)
+                {
+                    Debug.Log("Calibration: Comfort: Starting calibration process");
+                    state = State.SelectTranslationRotation;
+                }
+                break;
+            case State.SelectTranslationRotation:
+                //Activate Translation
+                if (TranslateAction.triggered)
+                {
+                    Debug.Log("Calibration: Mode: Selected Translation Mode");
+                    state = State.Translation;
+                }
+                //Activate Rotation (UpAxis)
+                if (RotateAction.triggered)
+                {
+                    Debug.Log("Calibration: Mode: Selected Rotation Mode");
+                    state = State.Rotation;
+                }
+                // Reset everything to factory settings
+                if (ResetAction.triggered)
+                {
+                    Debug.Log("Calibration: Mode: Reset factory settings");
+                    ResetFactorySettings();
+                    cameraReference.transform.localPosition = Vector3.zero;
+                    cameraReference.transform.localRotation = Quaternion.Euler(Vector3.zero);
+                }
+                if (YesAction.triggered || NoAction.triggered || DoneAction.triggered)
+                {
+                    Debug.Log("Calibration: Mode: User is done");
+                    state = State.CheckWithUser;
+                }
+                break;
+            case State.Translation:
+                // Movement
+                float zAxis = curMove.y;
+                float xAxis = curMove.x;
+                float yAxis = curHeight;
+                if (zAxis != 0) Debug.Log($"xxxjack translation z={zAxis}");
+                if (xAxis != 0) Debug.Log($"xxxjack translation x={xAxis}");
+                if (yAxis != 0) Debug.Log($"xxxjack translation y={yAxis}");
+                if (ResetAction.triggered)
+                {
+                    cameraReference.transform.localPosition = new Vector3(0, 0, 0);
+                    Debug.Log($"Calibration: Translation: reset to 0, 0, 0");
+                }
+                cameraReference.transform.localPosition += new Vector3(xAxis, yAxis, zAxis) * _translationSlightStep;
+                // Save Translation
+                if (YesAction.triggered)
+                {
+                    var pos = cameraReference.transform.localPosition;
+                    PlayerPrefs.SetFloat(prefix + "_pos_x", pos.x);
+                    PlayerPrefs.SetFloat(prefix + "_pos_y", pos.y);
+                    PlayerPrefs.SetFloat(prefix + "_pos_z", pos.z);
+                    Debug.Log($"Calibration: Translation: Saved: {pos.x}, {pos.y}, {pos.z}");
+                    state = State.CheckWithUser;
+                }
+                // Back
+                if (NoAction.triggered)
+                {
+                    cameraReference.transform.localPosition = new Vector3(
+                        PlayerPrefs.GetFloat(prefix + "_pos_x", 0),
+                        PlayerPrefs.GetFloat(prefix + "_pos_y", 0),
+                        PlayerPrefs.GetFloat(prefix + "_pos_z", 0)
+                    );
+                    var pos = cameraReference.transform.localPosition;
+                    Debug.Log($"Calibration: Translation: Reloaded to: {pos.x}, {pos.y}, {pos.z}");
+                    state = State.CheckWithUser;
+                }
+                break;
+            case State.Rotation:
+                // Rotation
+                float yAxisR = curMove.x;
+                if (yAxisR != 0) Debug.Log($"xxxjack rotation y={yAxisR}");
+                if (ResetAction.triggered)
+                {
+                    Debug.Log("Calibration: Rotation: Reset to 0,0,0");
+                    cameraReference.transform.localEulerAngles = new Vector3(0, 0, 0);
+                }
+                cameraReference.transform.localRotation = Quaternion.Euler(cameraReference.transform.localRotation.eulerAngles + Vector3.up * -_rotationSlightStep * yAxisR);
+                // Save Translation
+                if (YesAction.triggered)
+                {
+                    var rot = cameraReference.transform.localRotation.eulerAngles;
+                    PlayerPrefs.SetFloat(prefix + "_rot_x", rot.x);
+                    PlayerPrefs.SetFloat(prefix + "_rot_y", rot.y);
+                    PlayerPrefs.SetFloat(prefix + "_rot_z", rot.z);
+
+                    Debug.Log($"Calibration: Rotation: Saved: {rot.x}, {rot.y}, {rot.z}");
+                    state = State.CheckWithUser;
+                }
+                // Back
+                if (NoAction.triggered)
+                {
+                    cameraReference.transform.localRotation = Quaternion.Euler(
+                        PlayerPrefs.GetFloat(prefix + "_rot_x", 0),
+                        PlayerPrefs.GetFloat(prefix + "_rot_y", 0),
+                        PlayerPrefs.GetFloat(prefix + "_rot_z", 0)
+                    );
+                    var rot = cameraReference.transform.localRotation;
+                    Debug.Log($"Calibration: Rotation: Reloaded to: {rot.x}, {rot.y}, {rot.z}");
+                    state = State.CheckWithUser;
+                }
+                break;
+            default:
+                Debug.LogError($"Calibration: unexpected state {state}");
+                break;
+        }
     }
 
     private void InitializePosition()
@@ -115,6 +240,7 @@ public class Calibration : MonoBehaviour {
         cameraReference.transform.localRotation = Quaternion.Euler(rot);
     }
 
+#if xxxjack_outdated
     public void OnYesDone(InputValue value)
     {
         if (value.Get<float>() < 0.5) return;
@@ -276,6 +402,7 @@ public class Calibration : MonoBehaviour {
             cameraReference.transform.localPosition += new Vector3(0, delta, 0) * _translationSlightStep;
         } 
     }
+#endif
 
     void ChangeModeUI()
     {
