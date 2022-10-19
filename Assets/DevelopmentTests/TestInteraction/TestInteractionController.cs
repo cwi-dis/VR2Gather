95b3141a0075c73e3c0202f65d01a444b0676ffb
Jack Jansen <Jack.Jansen@cwi.nl>
2022-10-19 12:28:05 +0200

    Enable islocalplayer in TestInteraction.

---
+++ Assets/DevelopmentTests/TestInteraction/TestInteractionController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using VRT.Pilots.Common;
+
+namespace VRT.DevelopmentTests
+{
+    public class TestInteractionController : MonoBehaviour
+    {
+        [Tooltip("Fade in at start of scene")]
+        public bool enableFade;
+
+        [Tooltip("The user (for enabling local)")]
+        public VRT.Pilots.Common.NetworkPlayer player;
+        public static TestInteractionController Instance { get; private set; }
+
+        public void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            if (enableFade && CameraFader.Instance != null)
+            {
+                CameraFader.Instance.StartFadedOut = true;
+            }
+        }
+
+        // Start is called before the first frame update
+        public void Start()
+        {
+            player.SetIsLocalPlayer(true);
+            if (enableFade)
+            {
+                CameraFader.Instance.StartFadedOut = true;
+                StartCoroutine(CameraFader.Instance.FadeIn());
+            }
+        }
+    }
+}
