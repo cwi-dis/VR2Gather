d2ff3c1849e15d8ae081afc34150eeef2720fa1c
Jack Jansen <Jack.Jansen@cwi.nl>
2022-12-02 00:14:34 +0100

    Added namespace VRT.Pilots.LoginManager

--- Assets/Pilots/LoginManager/Scripts/LoginController.cs
+++ Assets/Pilots/LoginManager/Scripts/LoginController.cs
@@ -7,78 +7,91 @@ using VRT.Orchestrator.Wrapping;
 using VRT.Pilots.Common;
 using VRT.Core;
 
-public class LoginController : PilotController {
+namespace VRT.Pilots.LoginManager
+{
 
-    public class MessageType
+    public class LoginController : PilotController
     {
-        // Bad magic. These are prefixes generated (I think) by the orchestrator)
-        public const string START = "START";
-        public const string READY = "READY";
-    }
-    private static LoginController instance;
 
-    public static LoginController Instance { get { return instance; } }
+        public class MessageType
+        {
+            // Bad magic. These are prefixes generated (I think) by the orchestrator)
+            public const string START = "START";
+            public const string READY = "READY";
+        }
+        private static LoginController instance;
 
-    //AsyncOperation async;
-    Coroutine loadCoroutine = null;
+        public static LoginController Instance { get { return instance; } }
 
-   
-    public override void Start() {
-        base.Start();
-        if (instance == null) {
-            instance = this;
+        //AsyncOperation async;
+        Coroutine loadCoroutine = null;
+
+
+        public override void Start()
+        {
+            base.Start();
+            if (instance == null)
+            {
+                instance = this;
+            }
+        }
+
+        IEnumerator RefreshAndLoad(string scenary)
+        {
+            yield return null;
+            OrchestratorController.Instance.GetUsers();
+            yield return new WaitForSeconds(0.5f);
+            SceneManager.LoadScene(scenary);
+        }
+
+        public override void MessageActivation(string message)
+        {
+            Debug.Log($"[FPA] MessageActivation {message}");
+            string[] msg = message.Split(new char[] { '_' });
+            if (msg[0] == MessageType.START)
+            {
+                // Check Audio
+                switch (msg[2])
+                {
+                    case "0": // No Audio
+                        Config.Instance.protocolType = Config.ProtocolType.None;
+                        break;
+                    case "1": // Socket Audio
+                        Config.Instance.protocolType = Config.ProtocolType.SocketIO;
+                        break;
+                    case "2": // Dash Audio
+                        Config.Instance.protocolType = Config.ProtocolType.Dash;
+                        break;
+                    case "3": // Raw TCP
+                        Config.Instance.protocolType = Config.ProtocolType.TCP;
+                        break;
+                    default:
+                        Debug.LogError($"LoginController: received unknown START audio type {msg[2]}");
+                        break;
+                }
+                string pilotName = msg[1];
+                string pilotVariant = null;
+                if (msg.Length > 3 && msg[3] != "") pilotVariant = msg[3];
+                if (msg.Length > 4 && msg[4] != "")
+                {
+                    Config.Instance.PCs.Codec = msg[4];
+                }
+                if (msg.Length > 5 && msg[5] != "")
+                {
+                    Config.Instance.Voice.Codec = msg[5];
+                }
+                string sceneName = PilotRegistry.GetSceneNameForPilotName(pilotName, pilotVariant);
+                if (sceneName == null)
+                {
+                    throw new System.Exception($"Selected scenario \"{sceneName}\" not implemented in this player");
+                }
+                if (loadCoroutine == null) loadCoroutine = StartCoroutine(RefreshAndLoad(sceneName));
+            }
+            else if (msg[0] == MessageType.READY)
+            {
+                // Do something to check if all the users are ready (future implementation)
+            }
         }
     }
 
-    IEnumerator RefreshAndLoad(string scenary) {
-        yield return null;
-        OrchestratorController.Instance.GetUsers();
-        yield return new WaitForSeconds(0.5f);
-        SceneManager.LoadScene(scenary);
-    }
-
-    public override void MessageActivation(string message) {
-        Debug.Log($"[FPA] MessageActivation {message}");
-        string[] msg = message.Split(new char[] { '_' });
-        if (msg[0] == MessageType.START) {
-            // Check Audio
-            switch (msg[2]) {
-                case "0": // No Audio
-                    Config.Instance.protocolType = Config.ProtocolType.None;
-                    break;
-                case "1": // Socket Audio
-                    Config.Instance.protocolType = Config.ProtocolType.SocketIO;
-                    break;
-                case "2": // Dash Audio
-                    Config.Instance.protocolType = Config.ProtocolType.Dash;
-                    break;
-                case "3": // Raw TCP
-                    Config.Instance.protocolType = Config.ProtocolType.TCP;
-                    break;
-                default:
-                    Debug.LogError($"LoginController: received unknown START audio type {msg[2]}");
-                    break;
-            }
-            string pilotName = msg[1];
-            string pilotVariant = null;
-            if (msg.Length > 3 && msg[3] != "") pilotVariant = msg[3];
-            if (msg.Length > 4 && msg[4] != "")
-            {
-                Config.Instance.PCs.Codec = msg[4];
-            }
-            if (msg.Length > 5 && msg[5] != "")
-            {
-                Config.Instance.Voice.Codec = msg[5];
-            }
-            string sceneName = PilotRegistry.GetSceneNameForPilotName(pilotName, pilotVariant);
-            if (sceneName == null)
-            {
-                throw new System.Exception($"Selected scenario \"{sceneName}\" not implemented in this player");
-            }
-            if (loadCoroutine == null) loadCoroutine = StartCoroutine(RefreshAndLoad(sceneName));
-        }
-        else if (msg[0] == MessageType.READY) {
-            // Do something to check if all the users are ready (future implementation)
-        }
-    }
 }
--- Assets/Pilots/LoginManager/Scripts/SelfRepresentationPreview.cs
+++ Assets/Pilots/LoginManager/Scripts/SelfRepresentationPreview.cs
@@ -11,111 +11,129 @@ using VRT.UserRepresentation.PointCloud;
 using VRT.Pilots.Common;
 
 using PointCloudRenderer = Cwipc.PointCloudRenderer;
+namespace VRT.Pilots.LoginManager
+{
 
-public class SelfRepresentationPreview : MonoBehaviour{
-    public static SelfRepresentationPreview Instance { get; private set; }
-    public float MicrophoneLevel { get; private set; }
+    public class SelfRepresentationPreview : MonoBehaviour
+    {
+        public static SelfRepresentationPreview Instance { get; private set; }
+        public float MicrophoneLevel { get; private set; }
 
-    public PlayerManager player;
-    string currentMicrophoneName = "None";
-    AudioClip recorder;
-    float[] buffer = new float[320 * 3];
-    int readPosition = 0;
-    int samples = 16000;
+        public PlayerManager player;
+        string currentMicrophoneName = "None";
+        AudioClip recorder;
+        float[] buffer = new float[320 * 3];
+        int readPosition = 0;
+        int samples = 16000;
 
-    // Start is called before the first frame update
-    void Start() {
-        if (Instance == null) {
-            Instance = this;
-        }
-    }
-
-    void Update() {
-        if (currentMicrophoneName != "None") {
-            int writePosition = Microphone.GetPosition(currentMicrophoneName);
-            int available;
-            if (writePosition < readPosition) available = (samples - readPosition) + writePosition;
-            else available = writePosition - readPosition;
-
-            if (available >= buffer.Length) {
-                float total = 0;
-                if (recorder.GetData(buffer, readPosition)) {
-                    readPosition = (readPosition + buffer.Length) % samples;
-                    for (int i = 0; i < buffer.Length; ++i)
-                        total += Mathf.Abs(buffer[i]*4);
-                }
-                MicrophoneLevel = total / (float)buffer.Length;
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
             }
         }
-    }
 
-    public void Stop() {
-        player.avatar.SetActive(false);
-        if (player.webcam.TryGetComponent(out WebCamPipeline web))
-            Destroy(web);
-        player.webcam.SetActive(false);
-        if (player.pc.TryGetComponent(out PointCloudPipeline pointcloud))
-            Destroy(pointcloud);
-        if (player.pc.TryGetComponent(out PointCloudRenderer renderer))
-            Destroy(renderer);
-        player.pc.SetActive(false);
-    }
+        void Update()
+        {
+            if (currentMicrophoneName != "None")
+            {
+                int writePosition = Microphone.GetPosition(currentMicrophoneName);
+                int available;
+                if (writePosition < readPosition) available = (samples - readPosition) + writePosition;
+                else available = writePosition - readPosition;
 
-    public void ChangeMicrophone(string microphoneName) {
-        StopMicrophone();
-        currentMicrophoneName = microphoneName;
-        if (currentMicrophoneName != "None") {
-            AsyncVoiceReader.PrepareDSP(Config.Instance.audioSampleRate, 0);
-            recorder = Microphone.Start(currentMicrophoneName, true, 1, samples);
-            readPosition = 0;
-        }
-    }
-
-    public void StopMicrophone() {
-        if (currentMicrophoneName != "None") {
-            Microphone.End(currentMicrophoneName);
-            currentMicrophoneName = "None";
-        }
-    }
-
-
-    public void ChangeRepresentation(UserRepresentationType representation, string webcamName) {
-        if (OrchestratorController.Instance == null || OrchestratorController.Instance.SelfUser == null) return;
-        player.userName.text = OrchestratorController.Instance.SelfUser.userName;
-        player.gameObject.SetActive(true);
-        //player.setupInputOutput(true); // xxxjack needed for preview?
-        Stop();
-
-        switch (representation) {
-            case UserRepresentationType.__NONE__:
-                player.gameObject.SetActive(false);
-                break;
-            case UserRepresentationType.__2D__:
-                player.webcam.SetActive(true);
-                if (webcamName != "None") {
-                    BasePipeline wcPipeline = BasePipeline.AddPipelineComponent(player.webcam, representation);
-                    wcPipeline.Init(new User() { userData = new UserData() { webcamName = webcamName, microphoneName = "None" } }, Config.Instance.LocalUser, true);
+                if (available >= buffer.Length)
+                {
+                    float total = 0;
+                    if (recorder.GetData(buffer, readPosition))
+                    {
+                        readPosition = (readPosition + buffer.Length) % samples;
+                        for (int i = 0; i < buffer.Length; ++i)
+                            total += Mathf.Abs(buffer[i] * 4);
+                    }
+                    MicrophoneLevel = total / (float)buffer.Length;
                 }
+            }
+        }
 
-                break;
-            case UserRepresentationType.__AVATAR__:
-                player.avatar.SetActive(true);
-                break;
-            case UserRepresentationType.__PCC_CWI_:
-            case UserRepresentationType.__PCC_CWIK4A_:
-            case UserRepresentationType.__PCC_PROXY__:
-            case UserRepresentationType.__PCC_SYNTH__:
-            case UserRepresentationType.__PCC_PRERECORDED__:
-                player.pc.SetActive(true);
-                BasePipeline pcPipeline = BasePipeline.AddPipelineComponent(player.pc, representation);
-                pcPipeline.Init(new User() { userData = new UserData() { userRepresentationType = representation } }, Config.Instance.LocalUser, true);
-                break;
-            case UserRepresentationType.__SPECTATOR__:
-            case UserRepresentationType.__CAMERAMAN__:
-                player.gameObject.SetActive(false);
-                break;
-            default:
-                break;
+        public void Stop()
+        {
+            player.avatar.SetActive(false);
+            if (player.webcam.TryGetComponent(out WebCamPipeline web))
+                Destroy(web);
+            player.webcam.SetActive(false);
+            if (player.pc.TryGetComponent(out PointCloudPipeline pointcloud))
+                Destroy(pointcloud);
+            if (player.pc.TryGetComponent(out PointCloudRenderer renderer))
+                Destroy(renderer);
+            player.pc.SetActive(false);
+        }
+
+        public void ChangeMicrophone(string microphoneName)
+        {
+            StopMicrophone();
+            currentMicrophoneName = microphoneName;
+            if (currentMicrophoneName != "None")
+            {
+                AsyncVoiceReader.PrepareDSP(Config.Instance.audioSampleRate, 0);
+                recorder = Microphone.Start(currentMicrophoneName, true, 1, samples);
+                readPosition = 0;
+            }
+        }
+
+        public void StopMicrophone()
+        {
+            if (currentMicrophoneName != "None")
+            {
+                Microphone.End(currentMicrophoneName);
+                currentMicrophoneName = "None";
+            }
+        }
+
+
+        public void ChangeRepresentation(UserRepresentationType representation, string webcamName)
+        {
+            if (OrchestratorController.Instance == null || OrchestratorController.Instance.SelfUser == null) return;
+            player.userName.text = OrchestratorController.Instance.SelfUser.userName;
+            player.gameObject.SetActive(true);
+            //player.setupInputOutput(true); // xxxjack needed for preview?
+            Stop();
+
+            switch (representation)
+            {
+                case UserRepresentationType.__NONE__:
+                    player.gameObject.SetActive(false);
+                    break;
+                case UserRepresentationType.__2D__:
+                    player.webcam.SetActive(true);
+                    if (webcamName != "None")
+                    {
+                        BasePipeline wcPipeline = BasePipeline.AddPipelineComponent(player.webcam, representation);
+                        wcPipeline.Init(new User() { userData = new UserData() { webcamName = webcamName, microphoneName = "None" } }, Config.Instance.LocalUser, true);
+                    }
+
+                    break;
+                case UserRepresentationType.__AVATAR__:
+                    player.avatar.SetActive(true);
+                    break;
+                case UserRepresentationType.__PCC_CWI_:
+                case UserRepresentationType.__PCC_CWIK4A_:
+                case UserRepresentationType.__PCC_PROXY__:
+                case UserRepresentationType.__PCC_SYNTH__:
+                case UserRepresentationType.__PCC_PRERECORDED__:
+                    player.pc.SetActive(true);
+                    BasePipeline pcPipeline = BasePipeline.AddPipelineComponent(player.pc, representation);
+                    pcPipeline.Init(new User() { userData = new UserData() { userRepresentationType = representation } }, Config.Instance.LocalUser, true);
+                    break;
+                case UserRepresentationType.__SPECTATOR__:
+                case UserRepresentationType.__CAMERAMAN__:
+                    player.gameObject.SetActive(false);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
