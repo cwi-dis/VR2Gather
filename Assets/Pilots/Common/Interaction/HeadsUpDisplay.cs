ebe102295b268dd9b74a16bbd03d2055afea24f0
Jack Jansen <Jack.Jansen@cwi.nl>
2022-12-22 16:21:36 +0100

    HUD can be shown/hidden. HUD tracks camera.

--- Assets/Pilots/Common/Interaction/HeadsUpDisplay.cs
+++ Assets/Pilots/Common/Interaction/HeadsUpDisplay.cs
@@ -14,6 +14,12 @@ namespace VRT.Pilots.Common
         [SerializeField] InputActionProperty m_ShowHideAction;
         [Tooltip("The canvas to show/hide")]
         [SerializeField] GameObject canvas;
+        [Tooltip("How far away is the HMD from the users eyes?")]
+        public float distance = 1;
+        [Tooltip("How far up/down from the center is the HMD?")]
+        public float height = 0;
+        [Tooltip("How fast should it move when the user changes position/orientation?")]
+        public float velocity = 0.01f;
 
         // Start is called before the first frame update
         void Start()
@@ -29,6 +35,18 @@ namespace VRT.Pilots.Common
                 canvas.SetActive(!canvas.activeSelf);
                 Debug.Log($"xxxjack toggle HUD, now {canvas.activeSelf}");
             }
+        }
+        void LateUpdate()
+        {
+            Vector3 forward = Camera.main.transform.forward;
+            forward.y = height;
+            forward = forward.normalized;
+
+            Vector3 position = Camera.main.transform.position + forward * distance;
+            Quaternion rotation = Quaternion.LookRotation(forward);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, velocity);
+            transform.position = Vector3.Lerp(transform.position, position, velocity);
+            //transform.localScale = Vector3.Lerp(transform.localScale, scale, scaleVel);
         }
     }
 
