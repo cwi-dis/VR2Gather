7caa7e8e0e0429e1968be527d9b6f294055d873c
Jack Jansen <Jack.Jansen@cwi.nl>
2022-12-22 17:27:23 +0100

    Added commands and Messages tabs, and toggle buttons to switch between them.

--- Assets/Pilots/Common/Interaction/HeadsUpDisplay.cs
+++ Assets/Pilots/Common/Interaction/HeadsUpDisplay.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using UnityEngine.InputSystem.Controls;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
@@ -12,8 +13,16 @@ namespace VRT.Pilots.Common
     {
         [Tooltip("The Input System Action that will show/hide the HUD")]
         [SerializeField] InputActionProperty m_ShowHideAction;
-        [Tooltip("The canvas to show/hide")]
+        [Tooltip("The canvas containing the HUD")]
         [SerializeField] GameObject canvas;
+        [Tooltip("Canvas element: user interface")]
+        [SerializeField] GameObject UserInterfaceGO;
+        [Tooltip("Button that toggles User Interface")]
+        [SerializeField] Toggle UserInterfaceToggle;
+        [Tooltip("Canvas element: error messages")]
+        [SerializeField] GameObject MessagesGO;
+        [Tooltip("Button that toggles error messages")]
+        [SerializeField] Toggle MessagesToggle;
         [Tooltip("How far away is the HMD from the users eyes?")]
         public float distance = 1;
         [Tooltip("How far up/down from the center is the HMD?")]
@@ -24,7 +33,7 @@ namespace VRT.Pilots.Common
         // Start is called before the first frame update
         void Start()
         {
-
+            Hide();
         }
 
         // Update is called once per frame
@@ -36,6 +45,7 @@ namespace VRT.Pilots.Common
                 Debug.Log($"xxxjack toggle HUD, now {canvas.activeSelf}");
             }
         }
+
         void LateUpdate()
         {
             Vector3 forward = Camera.main.transform.forward;
@@ -47,6 +57,45 @@ namespace VRT.Pilots.Common
             transform.rotation = Quaternion.Lerp(transform.rotation, rotation, velocity);
             transform.position = Vector3.Lerp(transform.position, position, velocity);
             //transform.localScale = Vector3.Lerp(transform.localScale, scale, scaleVel);
+        }
+
+        public void Hide()
+        {
+            canvas.SetActive(false);
+            UserInterfaceGO.SetActive(true);
+            MessagesGO.SetActive(false);
+            UserInterfaceToggle.isOn = true;
+            MessagesToggle.isOn = false;
+        }
+
+        public void ToggleChanged()
+        {
+            if (UserInterfaceToggle.isOn)
+            {
+                ShowCommands();
+            }
+            else
+            {
+                ShowMessages();
+            }
+        }
+
+        public void ShowCommands()
+        {
+            UserInterfaceGO.SetActive(true);
+            MessagesGO.SetActive(false);
+            UserInterfaceToggle.isOn = true;
+            MessagesToggle.isOn = false;
+            canvas.SetActive(true);
+        }
+
+        public void ShowMessages()
+        {
+            UserInterfaceGO.SetActive(false);
+            MessagesGO.SetActive(true);
+            UserInterfaceToggle.isOn = false;
+            MessagesToggle.isOn = true;
+            canvas.SetActive(true);
         }
     }
 
