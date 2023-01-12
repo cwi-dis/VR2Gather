6d6136456616287d31b0416fd56c40d89bbc841c
BALTHAZAR\Shishir <s.subramanyam@cwi.nl>
2023-01-12 18:11:15 +0100

    Added persistence manager

--- Assets/Pilots/Common/Persistence/PersistenceManager.cs
+++ Assets/Pilots/Common/Persistence/PersistenceManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System.IO;
+using System;
 
 
 namespace VRT.Pilots.Common
@@ -13,6 +15,7 @@ namespace VRT.Pilots.Common
     {
         public static PersistenceManager instance { get; private set; }
         public IDictionary<string, PersistenceData> persistenceDataDictionary;
+        private List<IDataPersistence> persistableSceneObjects;
         //xxxshishir ToDo: Add config variables to enable and disable this feature
         private bool loadPersistenceData = true;
         private bool savePersistenceData = true;
@@ -21,11 +24,44 @@ namespace VRT.Pilots.Common
         // Start is called before the first frame update
         private void Awake()
         {
+            //xxxshishir ToDo: Replace with config.json setting later
+            loadPersistenceData = true;
             if (instance != null)
                 Debug.LogError("Found more than one Persistence Manager in the scene");
             instance = this;
-            PersistenceData pData;
+            if (!loadPersistenceData)
+                return;
+            persistableSceneObjects = FindAllPersistableObjects();
+            string saveFile = Path.Combine(Application.persistentDataPath, fileName);
+            if (!File.Exists(saveFile))
+            {
+                Debug.LogError("No save data found!");
+                return;
+            }
+            string saveFileData = "";
+            try
+            {
+                using (FileStream stream = new FileStream(saveFile, FileMode.Open))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        saveFileData = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Couldn't load persistence data from file :" + saveFile + "\n" + e);
+                return;
+            }
 
+            PersistenceData pDataArray[] = JsonUtility.FromJson<PersistenceData[]>(saveFileData);
+
+        }
+        private List<IDataPersistence> FindAllPersistableObjects()
+        {
+            IEnumerable<IDataPersistence> persistableObjects = FindObjectsOfType<MonoBehaviour>().OfType<IDataPersistence>();
+            return new List<IDataPersistence>(persistableObjects);
         }
     }
 }
