c368134685a82a11856082c144e044adaf50fd14
BALTHAZAR\Shishir <s.subramanyam@cwi.nl>
2023-01-24 16:48:35 +0100

    Added separate save/load functions that can later be controlled by other interactions in the scene

--- Assets/Pilots/Common/Persistence/PersistenceManager.cs
+++ Assets/Pilots/Common/Persistence/PersistenceManager.cs
@@ -13,30 +13,80 @@ namespace VRT.Pilots.Common
     //The manager maintains a dictionary of objects identified by their network ID 
     public class PersistenceManager : MonoBehaviour
     {
-        public static PersistenceManager instance { get; private set; }
-        public IDictionary<string, PersistenceData> persistenceDataDictionary;
-        private List<IDataPersistence> persistableSceneObjects;
+        public static PersistenceManager Instance { get; private set; }
+       private List<IDataPersistence> persistableSceneObjects;
         //xxxshishir ToDo: Add config variables to enable and disable this feature
-        private bool loadPersistenceData = true;
-        private bool savePersistenceData = true;
+        [Tooltip("Enable loading of previously saved persistence data ")]
+        [SerializeField] private bool loadPersistenceData = true;
+        [Tooltip("Enable saving of persistence data in this session")]
+        [SerializeField] private bool savePersistenceData = true;
         //xxxshishir maybe we want to make this user specific and use the current username to name the file
-        private string fileName = "PersistenceData.json";
+        [Tooltip("Filename where persistent data is saved and loaded from")]
+        [SerializeField] private string fileName = "PersistenceData.json";
         // Start is called before the first frame update
-        void Awake()
+        void Start()
         {
             //xxxshishir ToDo: Replace this with config.json setting later
             loadPersistenceData = true;
-            if (instance != null)
+            if (Instance != null)
                 Debug.LogError("Found more than one Persistence Manager in the scene");
-            instance = this;
+            Instance = this;
             //xxxshishir disable persistence for all users that are not the session host
             if (!Orchestrator.Wrapping.OrchestratorController.Instance.UserIsMaster)
                 loadPersistenceData = false;
 
             if (!loadPersistenceData)
                 return;
+            loadAllPaersistenceData();
+        }
+        private void OnApplicationQuit()
+        {
+            if(Orchestrator.Wrapping.OrchestratorController.Instance.UserIsMaster)
+                saveAllPersistenceData();
+        }
+        private List<IDataPersistence> FindAllPersistableObjects()
+        {
+            IEnumerable<IDataPersistence> persistableObjects = FindObjectsOfType<MonoBehaviour>().OfType<IDataPersistence>();
+            return new List<IDataPersistence>(persistableObjects);
+        }
+        public void saveAllPersistenceData()
+        {
+            // xxxshishir We find persistable objects again on quit to account for dynamically created objects
+            Debug.Log($"{name}: saveAllPersistenceData called");
             persistableSceneObjects = FindAllPersistableObjects();
-            persistenceDataDictionary = new Dictionary<string, PersistenceData>();
+            string NetworkID;
+            PersistenceData pData;
+            var persistenceDataDictionary = new Dictionary<string, PersistenceData>();
+            foreach (IDataPersistence pObjects in persistableSceneObjects)
+            {
+                pData = pObjects.savePersistenceData();
+                NetworkID = pObjects.getNetworkID();
+                Debug.Log($"{name}: save data for {NetworkID}");
+                persistenceDataDictionary.Add(NetworkID, pData);
+            }
+            PersistenceDataCollection pDataCollection = new PersistenceDataCollection();
+            pDataCollection.fromDictionary(persistenceDataDictionary);
+            string saveFile = Path.Combine(Application.persistentDataPath, fileName);
+            string saveData = JsonUtility.ToJson(pDataCollection, true);
+            //xxxshishir for now we always overwrite the existing savefile
+            try
+            {
+                using (FileStream stream = new FileStream(saveFile, FileMode.Create))
+                {
+                    using (StreamWriter writer = new StreamWriter(stream))
+                    {
+                        writer.Write(saveData);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Couldn't save persistence data to file :" + saveFile + "\n" + e);
+            }
+        }
+        public void loadAllPaersistenceData()
+        {
+            persistableSceneObjects = FindAllPersistableObjects();
             string saveFile = Path.Combine(Application.persistentDataPath, fileName);
             if (!File.Exists(saveFile))
             {
@@ -60,7 +110,7 @@ namespace VRT.Pilots.Common
                 return;
             }
             PersistenceDataCollection pDataCollection = JsonUtility.FromJson<PersistenceDataCollection>(saveFileData);
-            persistenceDataDictionary = pDataCollection.toDictionary();
+            var persistenceDataDictionary = pDataCollection.toDictionary();
 
             //xxxshishir Loading persistence data to all persistable objects (with controllers) in the scene
             string NetworkID;
@@ -73,49 +123,7 @@ namespace VRT.Pilots.Common
                 persistenceDataDictionary.Remove(NetworkID);
             }
             if (persistenceDataDictionary.Count != 0)
-                Debug.LogWarning("Could not initialize " + persistenceDataDictionary.Count + " persistable objects in the save file");
-        }
-        private void OnApplicationQuit()
-        {
-            if(Orchestrator.Wrapping.OrchestratorController.Instance.UserIsMaster)
-                saveAllPersistenceData();
-        }
-        private List<IDataPersistence> FindAllPersistableObjects()
-        {
-            IEnumerable<IDataPersistence> persistableObjects = FindObjectsOfType<MonoBehaviour>().OfType<IDataPersistence>();
-            return new List<IDataPersistence>(persistableObjects);
-        }
-        private void saveAllPersistenceData()
-        {
-            // xxxshishir We find persistable objects again on quit to account for dynamically created objects
-            persistableSceneObjects = FindAllPersistableObjects();
-            string NetworkID;
-            PersistenceData pData;
-            foreach (IDataPersistence pObjects in persistableSceneObjects)
-            {
-                pData = pObjects.savePersistenceData();
-                NetworkID = pObjects.getNetworkID();
-                persistenceDataDictionary.Add(NetworkID, pData);
-            }
-            PersistenceDataCollection pDataCollection = new PersistenceDataCollection();
-            pDataCollection.fromDictionary(persistenceDataDictionary);
-            string saveFile = Path.Combine(Application.persistentDataPath, fileName);
-            string saveData = JsonUtility.ToJson(pDataCollection, true);
-            //xxxshishir for now we always overwrite the existing savefile
-            try
-            {
-                using (FileStream stream = new FileStream(saveFile, FileMode.Create))
-                {
-                    using (StreamWriter writer = new StreamWriter(stream))
-                    {
-                        writer.Write(saveData);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.LogError("Couldn't save persistence data to file :" + saveFile + "\n" + e);
-            }
+                Debug.LogWarning($"{name}: Could not initialize {persistenceDataDictionary.Count} persistable objects in the save file");
         }
     }
     [System.Serializable]
