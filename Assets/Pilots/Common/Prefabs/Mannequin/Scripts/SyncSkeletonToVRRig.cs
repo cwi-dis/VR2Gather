e911641b005f6b6314ed29329b6bd66147f67622
Fiddlehead\vrtogether <cwidisgroup@gmail.com>
2023-02-08 17:28:49 +0100

    With track position the movement speed seems to be correct but the movement is in the wrong direction

--- Assets/Pilots/Common/Prefabs/Mannequin/Scripts/SyncSkeletonToVRRig.cs
+++ Assets/Pilots/Common/Prefabs/Mannequin/Scripts/SyncSkeletonToVRRig.cs
@@ -17,6 +17,28 @@ public class SyncSkeletonToVRRig : MonoBehaviour
             rigTarget.position = vrTarget.position;
             rigTarget.rotation = vrTarget.rotation;
         }
+        //xxxshishir added new temporary functions to test mannequin movement - will remove these functions one the mannequin behaves correctly
+        public void MapPositionOnly()
+        {
+            rigTarget.position = vrTarget.position;
+        }
+        public void MapRotationOnly()
+        {
+            rigTarget.rotation = vrTarget.rotation;
+        }
+        public void ManualMap(Transform newTarget)
+        {
+            rigTarget.position = newTarget.position;
+            rigTarget.rotation = newTarget.rotation;
+        }
+        public void ManualMapPosition(Vector3 newTargetPosition)
+        {
+            rigTarget.position = newTargetPosition;
+        }
+        public void ManualMapRotation(Quaternion newTargetRotation)
+        {
+            rigTarget.rotation = newTargetRotation;
+        }
     }
     [Tooltip("VRRig to Skeleton mapping for player head")]
     public VRMap head;
@@ -36,6 +58,9 @@ public class SyncSkeletonToVRRig : MonoBehaviour
     public bool followPosition;
     [Tooltip("If set: body follows head rotation")]
     public bool followRotation;
+    //xxxshishir addition debug variable to track camera movement
+    [Tooltip("Camera transform")]
+    public Transform cameraTransform;
     // Start is called before the first frame update
     void Start()
     {
@@ -51,19 +76,41 @@ public class SyncSkeletonToVRRig : MonoBehaviour
         // - compute skeleton head position - player position
         // - update player position so skeleton head position matches camera position.
         // and think about other users.
-        head.Map();
+
+        //xxxshishir initial approach - movement is too fast if position is tracked and rotation is in the opposite direction if rotation is tracked
+        //head.Map();
+        //leftHand.Map();
+        //rightHand.Map();
+
+        //if (followRotation)
+        //{
+        //    //transform.forward = Vector3.ProjectOnPlane(headConstraint.forward, Vector3.up).normalized;
+        //    playerTransform.Rotate(0, -headRotationY, 0);
+        //    headRotationY = headConstraint.eulerAngles.y;
+        //}
+        //if (followPosition)
+        //{
+        //    playerTransform.position = headConstraint.position + headBodyOffset;
+        //    headBodyOffset = playerTransform.position - headConstraint.position;
+        //}
+
+        //xxxshishir trying out new approach based on jack's previous comment - this still doesn't work properly - movement is at the right speed but inverted
+        head.ManualMapRotation(cameraTransform.rotation);
+
+        headBodyOffset = playerTransform.position - headConstraint.position;
+        var newPlayerPosition = headConstraint.position - cameraTransform.position;
+        if(followPosition)
+        {
+            playerTransform.position = playerTransform.position + newPlayerPosition;
+        }
+        head.MapPositionOnly();
+        if(followRotation)
+        {
+            playerTransform.rotation = cameraTransform.rotation;
+        }
+        //head.MapPositionOnly();
         leftHand.Map();
         rightHand.Map();
-        if (followPosition)
-        {
-            playerTransform.position = headConstraint.position + headBodyOffset;
-            headBodyOffset = playerTransform.position - headConstraint.position;
-        }
-        if (followRotation)
-        {
-            //transform.forward = Vector3.ProjectOnPlane(headConstraint.forward, Vector3.up).normalized;
-            playerTransform.Rotate(0, -headRotationY, 0);
-            headRotationY = headConstraint.eulerAngles.y;
-        }
+
     }
 }
