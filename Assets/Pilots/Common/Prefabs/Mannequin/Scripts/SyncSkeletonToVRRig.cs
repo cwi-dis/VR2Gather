4c6e9036f3a372852767b3aa611a951c55a07883
Fiddlehead\vrtogether <cwidisgroup@gmail.com>
2023-02-10 15:31:32 +0100

    Code cleanup

--- Assets/Pilots/Common/Prefabs/Mannequin/Scripts/SyncSkeletonToVRRig.cs
+++ Assets/Pilots/Common/Prefabs/Mannequin/Scripts/SyncSkeletonToVRRig.cs
@@ -17,28 +17,6 @@ public class SyncSkeletonToVRRig : MonoBehaviour
             rigTarget.position = vrTarget.position;
             rigTarget.rotation = vrTarget.rotation;
         }
-        //xxxshishir added new temporary functions to test mannequin movement - will remove these functions one the mannequin behaves correctly
-        public void MapPositionOnly()
-        {
-            rigTarget.position = vrTarget.position;
-        }
-        public void MapRotationOnly()
-        {
-            rigTarget.rotation = vrTarget.rotation;
-        }
-        public void ManualMap(Transform newTarget)
-        {
-            rigTarget.position = newTarget.position;
-            rigTarget.rotation = newTarget.rotation;
-        }
-        public void ManualMapPosition(Vector3 newTargetPosition)
-        {
-            rigTarget.position = newTargetPosition;
-        }
-        public void ManualMapRotation(Quaternion newTargetRotation)
-        {
-            rigTarget.rotation = newTargetRotation;
-        }
     }
     [Tooltip("VRRig to Skeleton mapping for player head")]
     public VRMap head;
@@ -52,17 +30,9 @@ public class SyncSkeletonToVRRig : MonoBehaviour
     public Transform headConstraint;
     [Tooltip("Computed offset between skeleton head and main object")]
     public Vector3 headBodyOffset;
-    [Tooltip("Computed rotation angle difference")]
-    public float headRotationY;
-    [Tooltip("If set: body follows head position")]
-    public bool followPosition;
-    [Tooltip("If set: body follows head rotation")]
-    public bool followRotation;
-    //xxxshishir addition debug variable to track camera movement
-    [Tooltip("Camera transform")]
-    public Transform cameraTransform;
+    //xxxshishir added transform variable to track the mannequin directly rather than the player
     [Tooltip("Mannequin body turn speed")]
-    public float turnSmoothness = 0.1f;
+    public float turnSmoothness = 5;
     [Tooltip("Mannequin transform")]
     public Transform mannequinTransform;
 
@@ -70,63 +40,16 @@ public class SyncSkeletonToVRRig : MonoBehaviour
     void Start()
     {
         headBodyOffset = playerTransform.position - headConstraint.position;
-        headRotationY = headConstraint.eulerAngles.y;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        // xxxjack this is wrong. What we think we should do:
-        // - Map camera rotation to skeleton head
-        // - compute skeleton head position - player position
-        // - update player position so skeleton head position matches camera position.
-        // and think about other users.
-
-        //xxxshishir initial approach - movement is too fast if position is tracked and rotation is in the opposite direction if rotation is tracked
-        //head.Map();
-        //leftHand.Map();
-        //rightHand.Map();
-
-        //if (followRotation)
-        //{
-        //    //transform.forward = Vector3.ProjectOnPlane(headConstraint.forward, Vector3.up).normalized;
-        //    playerTransform.Rotate(0, -headRotationY, 0);
-        //    headRotationY = headConstraint.eulerAngles.y;
-        //}
-        //if (followPosition)
-        //{
-        //    playerTransform.position = headConstraint.position + headBodyOffset;
-        //    headBodyOffset = playerTransform.position - headConstraint.position;
-        //}
-
-        //xxxshishir trying out new approach based on jack's previous comment - this still doesn't work properly - movement is at the right speed but inverted
-        //head.ManualMapRotation(cameraTransform.rotation);
-
-        //headBodyOffset = playerTransform.position - headConstraint.position;
-        //var newPlayerPosition = headConstraint.position - cameraTransform.position;
-        //if(followPosition)
-        //{
-        //    playerTransform.position = playerTransform.position + newPlayerPosition;
-        //}
-        //head.MapPositionOnly();
-        //if(followRotation)
-        //{
-        //    playerTransform.rotation = cameraTransform.rotation;
-        //}
-        ////head.MapPositionOnly();
-        //leftHand.Map();
-        //rightHand.Map();
-
-        //xxxshishir trying out the new method from: https://blog.immersive-insiders.com/animate-avatar-for-vr-in-unity/
-        //playerTransform.position = cameraTransform.position + headBodyOffset;
-        //playerTransform.forward = Vector3.Lerp(playerTransform.forward, Vector3.ProjectOnPlane(cameraTransform.forward, Vector3.up).normalized, Time.deltaTime * turnSmoothness);
-        //playerTransform.position = headConstraint.position + headBodyOffset;
-        //playerTransform.forward = Vector3.Lerp(playerTransform.forward, Vector3.ProjectOnPlane(headConstraint.forward, Vector3.up).normalized, Time.deltaTime * turnSmoothness);
+        //xxxshishir trying out the new method from: https://blog.immersive-insiders.com/animate-avatar-for-vr-in-unity/, seems to work well
         mannequinTransform.position = headConstraint.position + headBodyOffset;
         mannequinTransform.forward = Vector3.Lerp(mannequinTransform.forward, Vector3.ProjectOnPlane(headConstraint.forward, Vector3.up).normalized, Time.deltaTime * turnSmoothness);
         head.Map();
         leftHand.Map();
         rightHand.Map();
-        //headBodyOffset = playerTransform.position - headConstraint.position;
     }
 }
