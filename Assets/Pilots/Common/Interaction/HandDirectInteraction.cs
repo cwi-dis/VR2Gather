63de1c1e12ca3fe1d641da4736fc6d22c9ca4a8c
BEELZEBUB\DIS <cwidisgroup@gmail.com>
2023-03-12 22:05:43 +0100

    NegateProcessor gets its own source file.

--- Assets/Pilots/Common/Interaction/HandDirectInteraction.cs
+++ Assets/Pilots/Common/Interaction/HandDirectInteraction.cs
@@ -3,36 +3,6 @@ using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEditor;
 
-// This InputProcessor should negate values, i.e. a range 0..1 will be mapped to 1..0.
-// This is different from inverting (which maps -1..1 to 1..-1).
-// But it doesn't work...
-#if UNITY_EDITOR
-[InitializeOnLoad]
-#endif
-public class NegateProcessor : InputProcessor<float>
-{
-
-#if UNITY_EDITOR
-	static NegateProcessor()
-	{
-		Initialize();
-	}
-#endif
-
-	[RuntimeInitializeOnLoadMethod]
-	static void Initialize()
-	{
-		InputSystem.RegisterProcessor<NegateProcessor>();
-	}
-
-	public override float Process(float value, InputControl control)
-	{
-		string name = "no control";
-		if (control != null) name = control.name;
-		return 1 - value;
-	}
-}
-
 namespace VRT.Pilots.Common
 {
 	using HandState = HandDirectAppearance.HandState;
