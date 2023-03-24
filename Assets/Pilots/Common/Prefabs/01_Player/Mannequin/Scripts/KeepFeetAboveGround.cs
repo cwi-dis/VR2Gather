18a990d28faa999e6fe5ce65174e4fc1eb79089f
Jack Jansen <Jack.Jansen@cwi.nl>
2023-03-24 10:38:07 +0100

    Experiment with a script to keep the feet above the ground. Not very successful.

---
+++ Assets/Pilots/Common/Prefabs/01_Player/Mannequin/Scripts/KeepFeetAboveGround.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepFeetAboveGround : MonoBehaviour
+{
+    public GameObject RootObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float height = transform.position.y - RootObject.transform.position.y;
+        if (height < 0)
+        {
+            Debug.Log($"KeepFeetAboveGround: {Time.frameCount}: height={height}, fixing.");
+            transform.Translate(0, height, 0);
+        } else
+        {
+            //transform.Translate(0, 0, 0);
+        }
+    }
+}
