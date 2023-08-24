5061ed7bec04950734df61d13e7b963d83b52aab
Jack Jansen <Jack.Jansen@cwi.nl>
2023-08-24 17:41:06 +0200

    Keeping the scenario descriptions in the scenes didn't work: you cannot inspect a scene without loading it. In stead store the scenario descriptions on the object in LoginManager.

--- Assets/Pilots/LoginManager/Scripts/ScenarioRegistry.cs
+++ Assets/Pilots/LoginManager/Scripts/ScenarioRegistry.cs
@@ -1,16 +1,42 @@
 using VRT.Orchestrator.Wrapping;
 using UnityEngine;
 using VRT.Core;
+using System.Collections.Generic;
+using System;
 
 namespace VRT.Pilots.LoginManager
 {
     public class ScenarioRegistry : MonoBehaviour
     {
+        [System.Serializable]
+        public class ScenarioInfo
+        {
+            [Tooltip("Short name for scenario")]
+            public string scenarioName;
+            [Tooltip("Unique ID of scenario. Create only once.")]
+            public string scenarioId;
+            [Tooltip("Short description of the scenario")]
+            public string scenarioDescription;
+            [Tooltip("The name of the scene that implements this scenario")]
+            public string scenarioSceneName;
+            [Tooltip("Higher numbered scenarios will appear higher in the list")]
+            public int scenarioPriority;
+        }
+
+        [Tooltip("Scenarios supported by this VR2Gather player")]
+        public List<ScenarioInfo> scenarios = new List<ScenarioInfo>();
+
+      
         public static ScenarioRegistry Instance;
 
         private void Awake()
         {
             Instance = this;
+        }
+
+        public void Start()
+        {
+            
         }
 
         public string GetSceneNameForPilotName(string pilotName, string pilotVariant)
