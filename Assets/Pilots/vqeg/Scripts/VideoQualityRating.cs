c6600040967a835ab8776151f16bb2456b632702
ITFCWI-A2LMTC0T\Ashutosh <ashutosh.singla@cwi.nl>
2023-11-29 18:05:59 +0100

    Sorting out the video quality rating issue....this function is called 5 times before it gets really called. Need to fix this.

--- Assets/Pilots/vqeg/Scripts/VideoQualityRating.cs
+++ Assets/Pilots/vqeg/Scripts/VideoQualityRating.cs
@@ -1,46 +1,79 @@
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using Cwipc;
 
 public class VideoQualityRating : MonoBehaviour
 {
     public Button nextButton;      // The next button
-    private int currentRating = -1; // Invalid default value to ensure selection
-    private string fileName;
+    public int currentRating = -1; // Invalid default value to ensure selection
+    private static string fileName;
+    public TMP_Text canvasText;
+    // Mainly for debug messages:
+    static int instanceCounter = 0;
+    int instanceNumber = instanceCounter++;
+
+    public string Name()
+    {
+        return $"{GetType().Name}#{instanceNumber}";
+    }
+
+    void Awake()
+    {
+        Debug.Log($"{Name()}: Awake()");
+        // Set the file name only once when the first instance of the script is loaded
+        if (string.IsNullOrEmpty(fileName))
+        {
+            fileName = "Rating_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
+        }
+    }
 
     void Start()
     {
+        
+        if (canvasText != null)
+        {
+            string currentText = canvasText.text;
+            Debug.Log($"{Name()}: Current Canvas Text: {currentText}");
+        }
+        else
+        {
+            Debug.LogError($"{Name()}: CanvasText is not assigned!");
+        }
         InitializeRating();
         nextButton.onClick.AddListener(SaveRatingAndProceed);
     }
 
     void InitializeRating()
     {
-        fileName = "Rating_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
+        // I dont want to have a seperate file for each rating. 
+        //fileName = "Rating_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
         nextButton.interactable = false; // Disable next button initially
     }
 
     public void SetRating(int rating) // setrating int 
     {
         currentRating = rating; // int.Parse(clickedButton.name); // Assuming button names are set to their respective rating values
-        Debug.Log($"VideoQualityRating:rating={rating}");
-        Debug.Log($"VideoQualityRating:rating={currentRating}");
+        Debug.Log($"{Name()}: rating={rating}");
         nextButton.interactable = true; // Enable next button when a rating is selected
     }
-
+        
     public void SaveRatingAndProceed()
     {
-        if (currentRating != -1)
+        if (currentRating == -1)
         {
-            File.AppendAllText(fileName, currentRating.ToString() + "\n");
-            // Load the next question or handle the end of the questionnaire
-            currentRating = -1; // Reset rating for the next question
-            nextButton.interactable = false; // Disable next button until new rating is chosen
+            Debug.LogError($"{Name()}: Rating = -1"); // need to fix  this.
         }
         else
         {
-           // Debug.LogError("VideoQualityRating: Rating = -1");      
+            string ratingText = $"canvasText.text: {currentRating}\n";
+            Statistics.Output(Name(), $"question={ratingText}, rating={currentRating}");
 
+            File.AppendAllText(fileName, ratingText);
+            // Load the next question or handle the end of the questionnaire
+            currentRating = -1; // Reset rating for the next question
+            nextButton.interactable = false; // Disable next button until new rating is chosen
         }
     }
 }
