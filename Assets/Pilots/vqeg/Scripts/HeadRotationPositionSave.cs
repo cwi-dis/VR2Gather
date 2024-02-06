99b6e2af28190d0fd636c1c6098f54a6fe95aec7
Topinambur\dis <cwidisgroup@gmail.com>
2024-02-06 18:12:21 +0100

    added the script tp save head data and movement data

--- Assets/Pilots/vqeg/Scripts/HeadRotationPositionSave.cs
+++ Assets/Pilots/vqeg/Scripts/HeadRotationPositionSave.cs
@@ -8,9 +8,9 @@ using System.IO;
 public class HeadRotationPositionSave : MonoBehaviour
 {
     private string logUrl;
-    private Camera userCamera;
-    private string state;
     
+    private string state;
+
 
     // Start is called before the first frame update
     void Start()
@@ -36,31 +36,37 @@ public class HeadRotationPositionSave : MonoBehaviour
         using (StreamWriter sw = File.CreateText(logUrl))
         {
             sw.WriteLine("Log started on " + DateTime.Now.ToLongDateString());
-        }        
+        }
     }
 
     void RecordCameraPositionAndOrientation()
     {
-        // Ensure userCamera is found, replace "YourCameraObjectName" with the actual name of your camera object
-        if (userCamera == null) userCamera = GameObject.Find("YourCameraObjectName").GetComponent<Camera>();
-
-        // Record current state (Excercise scene) 
-        state = "ExcerciseScene"; 
-
-        // Get camera position and orientation
-        Vector3 head = userCamera.transform.eulerAngles;
-        Vector3 position = userCamera.transform.position;
-
-        // Append record to the log file
-        using (StreamWriter sw = File.AppendText(logUrl))
+        Camera userCamera = Camera.main;
+        
+        if (userCamera == null)
         {
-            string logEntry = $"{DateTime.Now.TimeOfDay.TotalMilliseconds * 1000000};{state};LOOK_AT;{head.x};{head.y};{head.z};POSITION;{position.x};{position.y};{position.z}";
-            sw.WriteLine(logEntry);
+            Debug.LogError("No Main Camera is found");
+        }
+
+        else
+        {
+            // Record current state (Excercise scene) 
+            //state = "ExcerciseScene"; 
+
+            // Get camera position and orientation
+            Vector3 head = userCamera.transform.eulerAngles;
+            Vector3 position = userCamera.transform.position;
+
+            // Append record to the log file
+            using (StreamWriter sw = File.AppendText(logUrl))
+            {
+                string logEntry = $"{DateTime.Now.TimeOfDay.TotalMilliseconds * 1000000};{state};LOOK_AT;{head.x};{head.y};{head.z};POSITION;{position.x};{position.y};{position.z}";
+                sw.WriteLine(logEntry);
+            }
         }
     }
-
-    void OnApplicationQuit()
+    void OnDestroy()
     {
-        Debug.Log("Application has ended after " + Time.time + " seconds");        
-    }
+        Debug.Log("Application has ended after " + Time.time + " seconds");
+    } 
 }
