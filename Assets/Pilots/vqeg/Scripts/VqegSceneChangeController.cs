2ccf5bdb8cb6294cdfda137d4e7d790a7fef8474
Fiddlehead\vrtogether <cwidisgroup@gmail.com>
2024-02-12 18:11:43 +0100

    changed the script in the exercise scene to go to VQEG scene

--- Assets/Pilots/vqeg/Scripts/VqegSceneChangeController.cs
+++ Assets/Pilots/vqeg/Scripts/VqegSceneChangeController.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRT.Pilots.Common;
 
-public class VqegSceneChangeController :  PilotController
+public class VqegSceneChangeController : MonoBehaviour
 {
-    
+    [Tooltip("Scene to go to ")]
+    public string nextSceneName = "Vqeg";
+
+    public void GoToVQEGScene()
+    {
+        PilotController.LoadScene(nextSceneName);
+    }
 }
