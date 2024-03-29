9522c6e11ea208f53b366b3f9464c90cac5d95c9
Jack Jansen <Jack.Jansen@cwi.nl>
2024-03-29 14:28:58 +0100

    Adding second test program, which install VR2Gather package from a git URL in stead of locally

---
+++ VRTApp-TestGitPackage/Assets/Samples/VR2Gather/0.1.0/VRT Essential Assets/Scripts/LoginManager/LoginController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+using VRT.Orchestrator.Wrapping;
+using VRT.Pilots.Common;
+using VRT.Core;
+
+namespace VRT.Pilots.LoginManager
+{
+
+    public class LoginController : PilotController
+    {
+        [Tooltip("The self-player for the login scene")]
+        [SerializeField] public PlayerControllerSelf selfPlayer;
+
+       
+        //AsyncOperation async;
+        Coroutine loadCoroutine = null;
+
+        public override void Start()
+        {
+            // Do not call base.Start(), we don't want to fade in for the login scene.
+            Orchestrator.Wrapping.User user = new Orchestrator.Wrapping.User()
+            {
+                userId = "no-userid",
+                userName = "TestInteractionUser",
+                userData = new Orchestrator.Wrapping.UserData()
+                {
+                    microphoneName = "None",
+                    userRepresentationType = UserRepresentationType.SimpleAvatar // xxxjack need correct one.
+                }
+            };
+            selfPlayer.SetUpPlayerController(true, user);
+        }
+
+        IEnumerator RefreshAndLoad(string scenary)
+        {
+            yield return null;
+            LoadNewScene(scenary);
+        }
+
+        public override void OnUserMessageReceived(string message)
+        {
+            Debug.Log($"{Name()}: OnUserMessageReceived: {message}");
+            if (!message.StartsWith("START_")) {
+                Debug.LogError("LoginController: only expecting START_ messages");
+                return;
+            }
+            message = message.Substring(6);
+            SessionConfig.FromJson(message);
+            string sceneName = ScenarioRegistry.Instance.GetSceneNameForSession(SessionConfig.Instance);
+            if (sceneName == null)
+            {
+                Debug.LogError($"{Name()}: Selected scenario \"{SessionConfig.Instance.scenarioName}\" not implemented in this player (unknown scene)");
+                return;
+            }
+
+            if (loadCoroutine == null) loadCoroutine = StartCoroutine(RefreshAndLoad(sceneName));
+
+        }
+    }
+
+}
