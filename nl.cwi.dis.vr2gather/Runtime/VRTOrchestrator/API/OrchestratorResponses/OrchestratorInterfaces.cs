ff9f3afbc3f082b3e1db872ed23d2d97347ef89d
Thomas Röggla <t.roggla@cwi.nl>
2024-05-27 19:58:22 +0200

    moved OrchestratorInterfaces to OrchestratorResponses namespace

--- nl.cwi.dis.vr2gather/Runtime/VRTOrchestrator/API/OrchestratorResponses/OrchestratorInterfaces.cs
+++ nl.cwi.dis.vr2gather/Runtime/VRTOrchestrator/API/OrchestratorResponses/OrchestratorInterfaces.cs
@@ -1,31 +1,8 @@
-﻿//  © - 2020 – viaccess orca 
-//  
-//  Copyright
-//  This code is strictly confidential and the receiver is obliged to use it 
-//  exclusively for his or her own purposes. No part of Viaccess-Orca code may
-//  be reproduced or transmitted in any form or by any means, electronic or 
-//  mechanical, including photocopying, recording, or by any information 
-//  storage and retrieval system, without permission in writing from 
-//  Viaccess S.A. The information in this code is subject to change without 
-//  notice. Viaccess S.A. does not warrant that this code is error-free. If 
-//  you find any problems with this code or wish to make comments, please 
-//  report them to Viaccess-Orca.
-//  
-//  Trademarks
-//  Viaccess-Orca is a registered trademark of Viaccess S.A in France and/or
-//  other countries. All other product and company names mentioned herein are
-//  the trademarks of their respective owners. Viaccess S.A may hold patents,
-//  patent applications, trademarks, copyrights or other intellectual property
-//  rights over the code hereafter. Unless expressly specified otherwise in a 
-//  written license agreement, the delivery of this code does not imply the 
-//  concession of any license over these patents, trademarks, copyrights or 
-//  other intellectual property.
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using VRT.Orchestrator.Responses;
 
 //Interfaces to be implemented to supervise the orchestrator
-namespace VRT.Orchestrator.Wrapping
+namespace VRT.Orchestrator.Responses
 {
     public interface IOrchestratorConnectionListener
     {
