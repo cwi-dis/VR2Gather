50d7e8bd605ba4fadb8d35f850c62ba99e2aa305
Thomas Röggla <t.roggla@cwi.nl>
2024-05-28 16:45:10 +0200

    removed unused method from OrchestratorController

--- nl.cwi.dis.vr2gather/Runtime/VRTOrchestrator/API/OrchestratorWrapping/OrchestratorController.cs
+++ nl.cwi.dis.vr2gather/Runtime/VRTOrchestrator/API/OrchestratorWrapping/OrchestratorController.cs
@@ -1,37 +1,15 @@
-﻿//  © - 2020 – viaccess orca 
-//  
-//  Copyright
-//  This code is strictly confidential and the receiver is obliged to use it 
-//  exclusively for his or her own purposes. No part of Viaccess-Orca code may
-//  be reproduced or transmitted in any form or by any means, electronic or 
-//  mechanical, including photocopying, recording, or by any information 
-//  storage and retrieval system, without permission in writing from 
-//  Viaccess S.A. The information in this code is subject to change without 
-//  notice. Viaccess S.A. does not warrant that this code is error-free. If 
-//  you find any problems with this code or wish to make comments, please 
-//  report them to Viaccess-Orca.
-//  
-//  Trademarks
-//  Viaccess-Orca is a registered trademark of Viaccess S.A in France and/or
-//  other countries. All other product and company names mentioned herein are
-//  the trademarks of their respective owners. Viaccess S.A may hold patents,
-//  patent applications, trademarks, copyrights or other intellectual property
-//  rights over the code hereafter. Unless expressly specified otherwise in a 
-//  written license agreement, the delivery of this code does not imply the 
-//  concession of any license over these patents, trademarks, copyrights or 
-//  other intellectual property.
-
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 using VRT.Core;
 using VRT.Orchestrator.Responses;
-using static VRT.Core.NTPTools;
+
 #if UNITY_EDITOR
 using UnityEditor.Search;
 #endif
+
 #if VRT_WITH_STATS
 using Statistics = Cwipc.Statistics;
 #endif
@@ -344,9 +322,6 @@ namespace VRT.Orchestrator.Wrapping
             }
 
             OnLogoutEvent?.Invoke(userLoggedOutSucessfully);
-        }
-
-        public void SignIn(string pName, string pPassword) {
         }
 
 #endregion
