cef9f405171a5411ad0f33141a9a0a4b64f0e0fe
Jack Jansen <Jack.Jansen@cwi.nl>
2024-09-18 17:02:28 +0200

    We're recording records. Empty ones, for now.

--- nl.cwi.dis.vr2gather/Runtime/VRTCommon/Players/PositionTracker.cs
+++ nl.cwi.dis.vr2gather/Runtime/VRTCommon/Players/PositionTracker.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using JetBrains.Annotations;
+using Unity.Profiling;
+using UnityEditor.EditorTools;
 using UnityEngine;
 using VRT.Core;
 
@@ -10,19 +13,33 @@ namespace VRT.Pilots.Common
         public class PositionTracker : MonoBehaviour
     {
         [Serializable]
-        public class PositionData {
-            Int64 ts;
-            Vector3 p_pos;
-            Vector3 p_angle;
-            Vector3 c_pos;
-            Vector3 c_angle;
+        public class PositionItem {
+            public int ts;
+            public Vector3 p_pos;
+            public Vector3 p_angle;
+            public Vector3 c_pos;
+            public Vector3 c_angle;
         };
+        public class PositionData {
+            public List<PositionItem> positions;
+        }
+        PositionData positionData;
 
-        PositionData[] positions;
+        public int positionIndex;
+        GameObject playerGO;
+        GameObject playerCameraGO;
+        string inputFile;
+        string outputFile;
 
-        public string inputFile;
-        public string outputFile;
+        System.DateTime epoch = System.DateTime.UnixEpoch;
+
+        [Tooltip("Interval in ms between samples, for recording")]
+        public int timeInterval = 1000;
+        [Tooltip("Introspection: next time in ms we will take a sample")]
+        public int nextSampleTime;
+        [Tooltip("Introspection: true if we are playing back")]
         public bool isPlayingBack = false;
+        [Tooltip("Introspection: true if we are recording")]
         public bool isRecording = false;
         void Awake() 
         {
@@ -45,7 +62,7 @@ namespace VRT.Pilots.Common
             string filename = VRTConfig.ConfigFilename(inputFile);
             try {
                 string json = System.IO.File.ReadAllText(filename);
-                JsonUtility.FromJson<PositionData[]>(json);
+                positionData = JsonUtility.FromJson<PositionData>(json);
             } catch(FileNotFoundException) {
                 Debug.LogError($"{Name()}: File not found: {filename}");
                 isPlayingBack = false;
@@ -55,7 +72,8 @@ namespace VRT.Pilots.Common
         private void SavePositions() {
             Debug.Log($"{Name()}: Saving positions to {outputFile}");
             string filename = VRTConfig.ConfigFilename(outputFile);
-            string json = JsonUtility.ToJson(positions);
+            
+            string json = JsonUtility.ToJson(positionData);
             System.IO.File.WriteAllText(filename, json);
         }
 
@@ -65,13 +83,45 @@ namespace VRT.Pilots.Common
             Debug.Log($"{Name()}: Started");
             if (isPlayingBack) {
                 LoadPositions();
-            }     
+            }
+            if (positionData == null) {
+                positionData = new();
+            }
+            if (positionData.positions == null) {
+                positionData.positions = new();
+            } 
         }
 
+        int currentTime() {
+            System.DateTime now = System.DateTime.Now;
+            if (epoch == System.DateTime.UnixEpoch) {
+                epoch = now;
+            }
+            double tsFloat = (now - epoch).TotalMilliseconds;
+            return (int)tsFloat;
+        }
+
+        void RecordSample() {
+            PositionItem data = new()
+            {
+                ts = currentTime()
+            };
+            positionData.positions.Add(data);
+        }
+        
         // Update is called once per frame
         void Update()
         {
-            
+            if (isRecording) {
+                int now = currentTime();
+                if (now >= nextSampleTime) {
+                    RecordSample();
+                    nextSampleTime = now + timeInterval;
+                }
+            }
+            if (isPlayingBack) {
+
+            }
         }
 
         void OnDestroy() {
