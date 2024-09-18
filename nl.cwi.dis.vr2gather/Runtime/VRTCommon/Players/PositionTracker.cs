68df228137eab9219c7f759eb28c6e2b4731f7aa
Jack Jansen <Jack.Jansen@cwi.nl>
2024-09-18 15:48:27 +0200

    Loading and saving implemented. But no data yet.

--- nl.cwi.dis.vr2gather/Runtime/VRTCommon/Players/PositionTracker.cs
+++ nl.cwi.dis.vr2gather/Runtime/VRTCommon/Players/PositionTracker.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using VRT.Core;
 
@@ -7,6 +9,17 @@ namespace VRT.Pilots.Common
 {
         public class PositionTracker : MonoBehaviour
     {
+        [Serializable]
+        public class PositionData {
+            Int64 ts;
+            Vector3 p_pos;
+            Vector3 p_angle;
+            Vector3 c_pos;
+            Vector3 c_angle;
+        };
+
+        PositionData[] positions;
+
         public string inputFile;
         public string outputFile;
         public bool isPlayingBack = false;
@@ -29,16 +42,30 @@ namespace VRT.Pilots.Common
 
         private void LoadPositions() {
             Debug.Log($"{Name()}: Loading positions from {inputFile}");
+            string filename = VRTConfig.ConfigFilename(inputFile);
+            try {
+                string json = System.IO.File.ReadAllText(filename);
+                JsonUtility.FromJson<PositionData[]>(json);
+            } catch(FileNotFoundException) {
+                Debug.LogError($"{Name()}: File not found: {filename}");
+                isPlayingBack = false;
+            }
         }
 
         private void SavePositions() {
             Debug.Log($"{Name()}: Saving positions to {outputFile}");
+            string filename = VRTConfig.ConfigFilename(outputFile);
+            string json = JsonUtility.ToJson(positions);
+            System.IO.File.WriteAllText(filename, json);
         }
 
         // Start is called before the first frame update
         void Start()
         {
-            Debug.Log($"{Name()}: Started");      
+            Debug.Log($"{Name()}: Started");
+            if (isPlayingBack) {
+                LoadPositions();
+            }     
         }
 
         // Update is called once per frame
