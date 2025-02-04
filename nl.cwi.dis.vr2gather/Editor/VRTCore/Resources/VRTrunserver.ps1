99e0d23956cc595a3bca84951747ce7bf7a98844
BEELZEBUB\DIS <cwidisgroup@gmail.com>
2025-02-04 12:05:36 +0100

    Fixed issue with script.

--- nl.cwi.dis.vr2gather/Editor/VRTCore/Resources/VRTrunserver.ps1
+++ nl.cwi.dis.vr2gather/Editor/VRTCore/Resources/VRTrunserver.ps1
@@ -1,10 +1,13 @@
+$ErrorActionPreference = 'Stop'
 $myPath = $MyInvocation.MyCommand.Path
 $myDir = Split-Path $myPath -Parent
 $venvDir = Join-Path -Path $myDir -ChildPath VR2Gather_Data\venv
-$scriptDir = Join-Path -Path $venvDir -ChildPath VR2Gather_Data\venv\Scripts
+$scriptDir = Join-Path -Path $venvDir -ChildPath Scripts
 $runServer = Join-Path -Path $scriptDir -ChildPath VRTrunserver.exe
 
 if (-not (Test-Path $runServer)) {
+	"Testing whether Python is installed..."
+	python --version
 	"Creating venv..."
 	python -m venv $venvDir
 	"Activating venv..."
