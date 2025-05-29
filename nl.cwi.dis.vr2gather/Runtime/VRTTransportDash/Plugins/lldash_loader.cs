c7947eba9b50553e88ad7157f4434406f4742e5b
Jack Jansen <Jack.Jansen@cwi.nl>
2025-05-29 23:28:26 +0200

    On mac, use dlopen() from libdl.dylib to load the lldash native libraries. Closes #231.

--- nl.cwi.dis.vr2gather/Runtime/VRTTransportDash/Plugins/lldash_loader.cs
+++ nl.cwi.dis.vr2gather/Runtime/VRTTransportDash/Plugins/lldash_loader.cs
@@ -8,20 +8,37 @@ using VRT.NativeLibraries;
 using System.Diagnostics;
 namespace VRT.Transport.Dash
 {
-   
+
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+    public class API_libdl
+    {
+        [DllImport("libdl.dylib", CharSet = CharSet.Auto)]
+        public static extern IntPtr dlopen(string fileName, int flags);
+        [DllImport("libdl.dylib", CharSet = CharSet.Auto)]
+        public static extern IntPtr dlsym(IntPtr handle, string symbol);
+        [DllImport("libdl.dylib", CharSet = CharSet.Auto)]
+        public static extern int dlclose(IntPtr handle);
+    }
+
+#endif   
     public class Loader
     {
         private static bool didSetMSPaths = false;
 
         public static void PreLoadModule(string module_base)
         {
-#if old // UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
             string libPath = Path.Combine(VRT.NativeLibraries.VRTNativeLoader.platformLibrariesPath, module_base);
             UnityEngine.Debug.Log($"VRT.Transport.Dash.Loader: will call LoadLibrary({libPath})");
-            IntPtr rv = API_kernel.LoadLibrary(libPath);
+            IntPtr rv = API_libdl.dlopen(libPath, 0x0002); // RTLD_LAZY
+            if (rv == IntPtr.Zero)
+            {
+                UnityEngine.Debug.LogError($"VRT.Transport.Dash.Loader: API_libdl.dlopen({libPath}) failed");
+                return;
+            }
             UnityEngine.Debug.Log($"VRT.Transport.Dash.Loader: LoadLibrary({libPath}) returned {rv}");
 #endif
-        } 
+        }
 
         public static void PostLoadModule(string module_base)
         {
