eddc3b89ade64cd64e2620ddc6bb79305fe16602
BEELZEBUB\DIS <cwidisgroup@gmail.com>
2025-10-21 12:02:08 +0200

    Initialize in Start() in stead of in Awake(), because VRTConfig may not be initialized yet when Awake() is called.

--- nl.cwi.dis.vr2gather/Runtime/VRTCommon/Players/PositionTracker.cs
+++ nl.cwi.dis.vr2gather/Runtime/VRTCommon/Players/PositionTracker.cs
@@ -49,7 +49,13 @@ namespace VRT.Pilots.Common
         [Tooltip("Enable debug logging")]
         public bool debug;
 
-        void Awake() 
+        private void Awake()
+        {
+            // Calling Initialize() here results in errors sometimes.
+            // But maybe put it back here (in stead of in Start()) if this causes
+            // other problems.
+        }
+        void Initialize() 
         {
             inputFile = VRTConfig.Instance.LocalUser.PositionTracker.inputFile;
             outputFile = VRTConfig.Instance.LocalUser.PositionTracker.outputFile;
@@ -105,6 +111,7 @@ namespace VRT.Pilots.Common
         // Start is called before the first frame update
         void Start()
         {
+            Initialize();
             Debug.Log($"{Name()}: Started");
             if (isPlayingBack) {
                 LoadPositions();
