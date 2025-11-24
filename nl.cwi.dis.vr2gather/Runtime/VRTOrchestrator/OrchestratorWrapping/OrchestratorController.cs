6331c2273c1c98327b41dacdd15d361a38dd74ee
Jack Jansen <Jack.Jansen@cwi.nl>
2025-11-24 16:43:22 +0100

    Trying to fix sessions not terminating in 2024-spirit-lldash experiments.

--- nl.cwi.dis.vr2gather/Runtime/VRTOrchestrator/OrchestratorWrapping/OrchestratorController.cs
+++ nl.cwi.dis.vr2gather/Runtime/VRTOrchestrator/OrchestratorWrapping/OrchestratorController.cs
@@ -463,8 +463,8 @@ namespace VRT.Orchestrator.Wrapping
             mySession = null;
 
             // update the lists of session, anyway the result
+            if (_OptionalStopOnLeave()) return;
             orchestratorWrapper.GetSessions();
-            _OptionalStopOnLeave();
         }
 
         public void JoinSession(string pSessionID) {
@@ -526,7 +526,7 @@ namespace VRT.Orchestrator.Wrapping
             _OptionalStopOnLeave();
         }
 
-        void _OptionalStopOnLeave()
+        bool _OptionalStopOnLeave()
         {
             // If wanted: stop playing (in editor), or quit application
             if (autoStopOnLeave)
@@ -535,7 +535,10 @@ namespace VRT.Orchestrator.Wrapping
 #if UNITY_EDITOR
                 UnityEditor.EditorApplication.isPlaying = false;
 #endif
+                return true;
             }
+
+            return false;
         }
 
         public void OnUserJoinedSession(string userID, User user) {
